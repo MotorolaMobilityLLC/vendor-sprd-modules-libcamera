MotorolaMobilityLLC/vendor-sprd-modules-libcamera

commit 9a71ed22154fe88ebd8b0d958d7238854be09994

Bug #545675 - check resoluton supported by camera videorecording needed by volte ：240*320,320*240,352*288，288x352,640*480,480*640,1280*720
[root cause  ] whale2 dev6.0 bringup,check resoluton supported by camera videorecording needed by volte:240*320,320*240,352*288，
[changes     ] camera hal
[side effects] NA
[self test   ]<board, Test cases summary>:sp9850,camera
[reviewers   ] Bing.Liu,Tony.Peng,Miller.Song
[change_type ] feature_bugfix
[tag_product ] common

Change-Id: I186608ebbe20e03b7d459ef3297dbd646dcd08ff

hal3/SprdCamera3Setting.cpp
@@ -305,8 +305,8 @@ const uint8_t availableIso[] = {
 	CAMERA_ISO_MAX
 };
 
-
-const int32_t kavailable_back_stream_configurations[60][4] = {
+#define CAMERA_SETTINGS_CONFIG_ARRAYSIZE 80
+const int32_t kavailable_back_stream_configurations[CAMERA_SETTINGS_CONFIG_ARRAYSIZE][4] = {
 	/*{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 3264, 2448, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT}, //for CTS
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 2592, 1952, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 2560, 1536, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
@@ -327,6 +327,7 @@ const int32_t kavailable_back_stream_configurations[60][4] = {
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 480, 640, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 352, 288, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 320, 240, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
+	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 288, 352, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 240, 320, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 176, 144, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 
@@ -437,7 +438,7 @@ const int32_t kavailable_back_stream_configurations[60][4] = {
 	{HAL_PIXEL_FORMAT_BLOB, 176, 144, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 #endif
 };
-const int32_t kavailable_front_stream_configurations[60][4] = {
+const int32_t kavailable_front_stream_configurations[CAMERA_SETTINGS_CONFIG_ARRAYSIZE][4] = {
 #ifndef CONFIG_CAMERA_FULL_SCREEN_DISPLAY
 #ifdef CONFIG_FRONT_CAMERA_SUPPORT_5M
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 2592, 1944, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
@@ -463,6 +464,7 @@ const int32_t kavailable_front_stream_configurations[60][4] = {
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 480, 640, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 352, 288, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 320, 240, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
+	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 288, 352, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 240, 320, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 176, 144, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 #ifdef CONFIG_FRONT_CAMERA_SUPPORT_1080P
@@ -542,7 +544,7 @@ const int32_t kavailable_front_stream_configurations[60][4] = {
 	{HAL_PIXEL_FORMAT_BLOB, 176, 144, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 };
 
-const int64_t kavailable_back_min_frame_durations[60][4] = {
+const int64_t kavailable_back_min_frame_durations[CAMERA_SETTINGS_CONFIG_ARRAYSIZE][4] = {
 	/*{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 3264, 2448, 33331760L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 2592, 1952, 33331760L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 2560, 1536, 33331760L},
@@ -561,6 +563,7 @@ const int64_t kavailable_back_min_frame_durations[60][4] = {
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 480, 640, 33331760L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 352, 288, 33331760L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 320, 240, 33331760L},
+	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 288, 352, 33331760L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 240, 320, 33331760L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 176, 144, 33331760L},
 
@@ -651,7 +654,7 @@ const int64_t kavailable_back_min_frame_durations[60][4] = {
 	{HAL_PIXEL_FORMAT_BLOB, 640, 480, 33331760L},
 #endif
 };
-const int64_t kavailable_front_min_frame_durations[60][4] = {
+const int64_t kavailable_front_min_frame_durations[CAMERA_SETTINGS_CONFIG_ARRAYSIZE][4] = {
 #ifndef CONFIG_CAMERA_FULL_SCREEN_DISPLAY
 #ifdef CONFIG_FRONT_CAMERA_SUPPORT_5M
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 2592, 1944, 33331760L},
@@ -671,6 +674,7 @@ const int64_t kavailable_front_min_frame_durations[60][4] = {
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 480, 640, 33331760L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 352, 288, 33331760L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 320, 240, 33331760L},
+	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 288, 352, 33331760L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 240, 320, 33331760L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 176, 144, 33331760L},
 #ifndef CONFIG_CAMERA_FULL_SCREEN_DISPLAY
@@ -732,7 +736,7 @@ const int64_t kavailable_front_min_frame_durations[60][4] = {
 	{HAL_PIXEL_FORMAT_BLOB, 640, 480, 33331760L},
 };
 
-const int64_t kavailable_back_stall_durations[60][4] = {
+const int64_t kavailable_back_stall_durations[CAMERA_SETTINGS_CONFIG_ARRAYSIZE][4] = {
 	/*{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 3264, 2448, 0L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 2592, 1952, 0L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 2560, 1536, 0L},
@@ -751,6 +755,7 @@ const int64_t kavailable_back_stall_durations[60][4] = {
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 480, 640, 0L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 352, 288, 0L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 320, 240, 0L},
+	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 288, 352, 0L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 240, 320, 0L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 176, 144, 0L},
 
@@ -842,7 +847,7 @@ const int64_t kavailable_back_stall_durations[60][4] = {
 	{HAL_PIXEL_FORMAT_BLOB, 640, 480, 33331760L},
 #endif
 };
-const int64_t kavailable_front_stall_durations[60][4] = {
+const int64_t kavailable_front_stall_durations[CAMERA_SETTINGS_CONFIG_ARRAYSIZE][4] = {
 #ifndef CONFIG_CAMERA_FULL_SCREEN_DISPLAY
 #ifdef CONFIG_FRONT_CAMERA_SUPPORT_5M
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 2592, 1944, 0L},
@@ -862,6 +867,7 @@ const int64_t kavailable_front_stall_durations[60][4] = {
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 480, 640, 0L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 352, 288, 0L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 320, 240, 0L},
+	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 288, 352, 0L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 240, 320, 0L},
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 176, 144, 0L},
 #ifndef CONFIG_CAMERA_FULL_SCREEN_DISPLAY
@@ -3040,7 +3046,7 @@ int SprdCamera3Setting::updateWorkParameters(const CameraMetadata &frame_setting
 	if (frame_settings.exists(ANDROID_CONTROL_AE_ANTIBANDING_MODE)) {
 		valueU8 = frame_settings.find(ANDROID_CONTROL_AE_ANTIBANDING_MODE).data.u8[0];
 		GET_VALUE_IF_DIF(s_setting[mCameraId].controlInfo.ae_abtibanding_mode, valueU8, ANDROID_CONTROL_AE_ANTIBANDING_MODE)
-		HAL_LOGD("ANDROID_CONTROL_AE_ANTIBANDING_MODE %d %d",valueU8);
+		HAL_LOGD("ANDROID_CONTROL_AE_ANTIBANDING_MODE %d",valueU8);
 	}
 //SPRD
 	if (frame_settings.exists(ANDROID_SPRD_CAPTURE_MODE)) {

commit e9d28621d8a3dfc3288881d6e57e40fddb585c76

Bug #545675 fix bug of cannot video record caused by commit 9a71ed22154fe88ebd8b0d958d7238854be09994
[root cause  ] cannot video record caused by commit 9a71ed22154fe88ebd8b0d958d7238854be09994
[changes     ] HAL,SprdCamera3Settings
[side effects] Take picture and Video Recording
[self test   ]<board, Test cases summary>:sp9850,camera
[reviewers   ] tony.peng,bing.liu,murphy.xie
[change_type ] feature_bugfix
[tag_product ] common

Change-Id: Ib267439915f0db2033bb0691890fd284afac01ad

hal3/SprdCamera3Setting.cpp
@@ -305,7 +305,6 @@ const uint8_t availableIso[] = {
 	CAMERA_ISO_MAX
 };
 
-#define CAMERA_SETTINGS_CONFIG_ARRAYSIZE 80
 const int32_t kavailable_back_stream_configurations[CAMERA_SETTINGS_CONFIG_ARRAYSIZE][4] = {
 	/*{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 3264, 2448, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT}, //for CTS
 	{HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED, 2592, 1952, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
@@ -388,8 +387,8 @@ const int32_t kavailable_back_stream_configurations[CAMERA_SETTINGS_CONFIG_ARRAY
 	{HAL_PIXEL_FORMAT_BLOB, 1600, 1200, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 	{HAL_PIXEL_FORMAT_BLOB, 1280, 960, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 	{HAL_PIXEL_FORMAT_BLOB, 1280, 720, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
-    {HAL_PIXEL_FORMAT_BLOB, 720, 480, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
-    {HAL_PIXEL_FORMAT_BLOB, 640, 480, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT}, //for CTS
+	{HAL_PIXEL_FORMAT_BLOB, 720, 480, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
+	{HAL_PIXEL_FORMAT_BLOB, 640, 480, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT}, //for CTS
 	{HAL_PIXEL_FORMAT_BLOB, 352, 288, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 	{HAL_PIXEL_FORMAT_BLOB, 176, 144, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS_OUTPUT},
 #elif defined(CONFIG_CAMERA_SUPPORT_8M)
@@ -1932,7 +1931,8 @@ int SprdCamera3Setting::initStaticMetadata(int32_t cameraId, camera_metadata_t *
 	staticInfo.update(ANDROID_SCALER_AVAILABLE_RAW_MIN_DURATIONS,
 			s_setting[cameraId].scalerInfo.raw_min_duration,
 			ARRAY_SIZE(s_setting[cameraId].scalerInfo.raw_min_duration));
-	FILL_CAM_INFO_ARRAY(s_setting[cameraId].scalerInfo.available_stream_configurations, 0, 60, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS)
+	FILL_CAM_INFO_ARRAY(s_setting[cameraId].scalerInfo.available_stream_configurations, 0,
+				CAMERA_SETTINGS_CONFIG_ARRAYSIZE, ANDROID_SCALER_AVAILABLE_STREAM_CONFIGURATIONS)
 	HAL_LOGI("format=%d size=%d", s_setting[cameraId].scalerInfo.available_stream_configurations[0], array_size);
 	staticInfo.update(ANDROID_SCALER_AVAILABLE_PROCESSED_MIN_DURATIONS,
 			s_setting[cameraId].scalerInfo.processed_min_durations,
@@ -1942,8 +1942,10 @@ int SprdCamera3Setting::initStaticMetadata(int32_t cameraId, camera_metadata_t *
 	staticInfo.update(ANDROID_SCALER_AVAILABLE_JPEG_MIN_DURATIONS,
 			s_setting[cameraId].scalerInfo.jpeg_min_durations,
 			ARRAY_SIZE(s_setting[cameraId].scalerInfo.jpeg_min_durations));
-	FILL_CAM_INFO_ARRAY(s_setting[cameraId].scalerInfo.min_frame_durations, 0, 60, ANDROID_SCALER_AVAILABLE_MIN_FRAME_DURATIONS)
-	FILL_CAM_INFO_ARRAY(s_setting[cameraId].scalerInfo.stall_durations, 0, 60, ANDROID_SCALER_AVAILABLE_STALL_DURATIONS)
+	FILL_CAM_INFO_ARRAY(s_setting[cameraId].scalerInfo.min_frame_durations, 0,
+				CAMERA_SETTINGS_CONFIG_ARRAYSIZE, ANDROID_SCALER_AVAILABLE_MIN_FRAME_DURATIONS)
+	FILL_CAM_INFO_ARRAY(s_setting[cameraId].scalerInfo.stall_durations, 0,
+				CAMERA_SETTINGS_CONFIG_ARRAYSIZE, ANDROID_SCALER_AVAILABLE_STALL_DURATIONS)
 	staticInfo.update(ANDROID_SCALER_CROPPING_TYPE,
 			&(s_setting[cameraId].scalerInfo.cropping_type), 1);
 

hal3/SprdCamera3Setting.h
@@ -105,6 +105,7 @@ typedef int64_t nsecs_t;
 #define SPRD_MAX_TONE_CURVE_POINT  64  //>=64
 
 
+#define CAMERA_SETTINGS_CONFIG_ARRAYSIZE 80
 
 typedef struct {
 	uint8_t correction_mode;
@@ -277,9 +278,9 @@ typedef struct {
 	int32_t jpeg_size[20];
 	int64_t jpeg_min_durations[2];
 	int32_t crop_region[4];
-	int32_t available_stream_configurations[240];
-	int64_t min_frame_durations[240];
-	int64_t stall_durations[240];
+	int32_t available_stream_configurations[CAMERA_SETTINGS_CONFIG_ARRAYSIZE*4];
+	int64_t min_frame_durations[CAMERA_SETTINGS_CONFIG_ARRAYSIZE*4];
+	int64_t stall_durations[CAMERA_SETTINGS_CONFIG_ARRAYSIZE*4];
 	uint8_t cropping_type;
 } SCALER_Tag;
 