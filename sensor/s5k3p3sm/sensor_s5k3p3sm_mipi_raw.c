79f05599b9579da4a614a830e404a7e25a99e472
aaron.chen <aaron.chen@spreadtrum.com>
2016-04-12 10:22:04 +0800

    Bug #494343 add 1280*720 sensor setting.
    [bug number ]Bug 494343
    [root cause ]integrate debug info & tuning bin
    [changes ]  none
    [reviewers ] baggio.he
    [self test ] <board, Test cases summary>: sp9850s-1,preview with OK
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I1aec0a355d02d33da44376600573527c3cfef5ee

--- sensor/s5k3p3sm/sensor_s5k3p3sm_mipi_raw.c
+++ sensor/s5k3p3sm/sensor_s5k3p3sm_mipi_raw.c
@@ -369,6 +369,7 @@ static const SENSOR_REG_T s5k3p3sm_1280x720_4lane_setting[] = {
 static SENSOR_REG_TAB_INFO_T s_s5k3p3sm_resolution_Tab_RAW[9] = {
 	{ADDR_AND_LEN_OF_ARRAY(s5k3p3sm_common_init), 0, 0, 24, SENSOR_IMAGE_FORMAT_RAW},
 	{ADDR_AND_LEN_OF_ARRAY(s5k3p3sm_4632x3480_4lane_setting), 4632, 3480, 24, SENSOR_IMAGE_FORMAT_RAW},
+	//{ADDR_AND_LEN_OF_ARRAY(s5k3p3sm_1280x720_4lane_setting), 1280, 720, 24, SENSOR_IMAGE_FORMAT_RAW},
 	//{ADDR_AND_LEN_OF_ARRAY(s5k3p3sm_1920x1080_4lane_setting), 1920, 1080, 24, SENSOR_IMAGE_FORMAT_RAW},
 	//{ADDR_AND_LEN_OF_ARRAY(s5k3p3sm_2320x1748_4lane_setting), 2320, 1748, 24, SENSOR_IMAGE_FORMAT_RAW},
 	//{ADDR_AND_LEN_OF_ARRAY(s5k3p3sm_2304x1740_4lane_setting), 2304, 1740, 24, SENSOR_IMAGE_FORMAT_RAW},
@@ -381,6 +382,7 @@ static SENSOR_REG_TAB_INFO_T s_s5k3p3sm_resolution_Tab_RAW[9] = {
 static SENSOR_TRIM_T s_s5k3p3sm_Resolution_Trim_Tab[SENSOR_MODE_MAX] = {
 	{0, 0, 0, 0, 0, 0, 0, {0, 0, 0, 0}},
 	{0, 0, 4632, 3480, 92, 2784, 3626, {0, 0, 4632, 3480}},
+	//{0, 0, 1280, 720, 92, 1320, 901, {0, 0, 1280, 720}},
 	//{0, 0, 1920, 1080, 94, 1320, 1767, {0, 0, 1920, 1080}},
 	//{0, 0, 2320, 1748, 183, 1440, 1800, {0, 0, 2320, 1748}},
 	//{0, 0, 2304, 1740, 94, 1320, 0x0dc8, {0, 0, 2304, 1740}},
