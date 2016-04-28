3bc0f6aa0bce4491c527f4409d125aeb463dc6a1
chenfj <chenfj@spreadst.com>
2016-04-28 09:51:17 +0800

    Bug #556179 - [Whale2S][mr1][Camera]:"Camera error ,can't connect to the camera" when switch to front camera
    [root cause  ] lack of tuning bin file in pac package
    [changes     ] modify tuning bin items in libcam_device.mk
    [side effects] no
    [self test   ]<board, Test cases summary>:sp9850,test 20 times ok
    [reviewers   ] miller.song
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: If8e2cd39f7ce530f21fba0c30bb6f8b9d7ae96dc

--- libcam_device.mk
+++ libcam_device.mk
@@ -1,6 +1,6 @@
 TUNING_BIN := s5k3l2xx_mipi_raw_3a.bin
 TUNING_BIN += s5k3l2xx_mipi_raw_shading.bin
-TUNING_BIN := s5k5e3yx_mipi_raw_3a.bin
+TUNING_BIN += s5k5e3yx_mipi_raw_3a.bin
 TUNING_BIN += s5k5e3yx_mipi_raw_shading.bin
 TUNING_BIN += imx132_mipi_raw_3a.bin
 TUNING_BIN += imx132_mipi_raw_shading.bin
