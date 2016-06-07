5b49647057b31a00443b151981b9f044bed38fac
aaron.chen <aaron.chen@spreadtrum.com>
2016-06-07 19:55:11 +0800

    Bug #494343 fix ae locked when take raw pic.
    [bug number ]Bug 494343
    [root cause ] did not release afl status buf when stop isp
    [changes ]  none
    [reviewers ] baggio.he
    [self test ] <board, Test cases summary>: sp9850s-1,preview with OK
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Icd8b66500209be05ad819442724d5d5caf3e3050

--- isp3.0/middleware/isp_mw.c
+++ isp3.0/middleware/isp_mw.c
@@ -663,6 +663,7 @@ cmr_int isp_proc_start(cmr_handle isp_handle, struct ips_in_param *input_ptr, st
 	memcpy(&dev_in.dst2_frame, &input_ptr->dst2_frame, sizeof(struct isp_img_frm));
 	memcpy(&dev_in.resolution_info, &input_ptr->resolution_info, sizeof(struct isp_sensor_resolution_info));
 	ret = isp_dev_access_start_postproc(cxt->isp_dev_handle, &dev_in, &dev_out);
+	ret = isp_3a_fw_stop(cxt->isp_3a_handle);
 exit:
 	return ret;
 }
