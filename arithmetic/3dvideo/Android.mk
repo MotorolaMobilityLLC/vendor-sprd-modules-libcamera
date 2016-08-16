9ec4dbb6e832b376ca0a7970d9ff89cd77f47687
chengzd <chengzd@spreadst.com>
2016-08-16 13:37:06 +0800

    Bug #579554 - [研发自提]在whale2手机上实现前摄3D VIDEO功能
    [bug number  ] 579554
    [root cause  ] add new feature 3dvideo
    [changes     ] new feature
    [side effects] none
    [self test   ] <sp9860><ok>
    [reviewers   ] jianping.wang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I943671fb6ac1b40abd96b972167adac2d79b59c8
    (cherry picked from commit a4ba0b686f60d3ba0c531a9af4560fbeddac2763)

---
+++ arithmetic/3dvideo/Android.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2008 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+ifeq ($(strip $(TARGET_BOARD_CAMERA_SUPPORT_3DVIDEO)),true)
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libimagestitcher
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := both
+LOCAL_MODULE_STEM_32 := $(LOCAL_MODULE).so
+LOCAL_MODULE_STEM_64 := $(LOCAL_MODULE).so
+LOCAL_SRC_FILES_32 := lib/$(LOCAL_MODULE).so
+LOCAL_SRC_FILES_64 := lib64/$(LOCAL_MODULE).so
+include $(BUILD_PREBUILT)
+endif
