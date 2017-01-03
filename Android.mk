92cbbf7cca373af8428a9836d3a6f6ff3a7a06f0
kane.li <kane.li@spreadtrum.com>
2017-01-03 17:45:00 +0800

    Bug #562311 [sharkl2]add dummy camera lib for sharkl2 bringup
    [root cause  ] libcamera is not ok on k44_trunk
    [changes     ] add dummy code
    [side effects] no
    [self test   ] k44, build, pass
    [reviewers   ] miller.song
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Ia74eeb578413c40911083ef4439f2b706218d6c4

--- Android.mk
+++ Android.mk
@@ -1,9 +1,17 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+ifeq ($(strip $(TARGET_BOARD)), sp9850ka_1h10)
+
+include $(LOCAL_PATH)/hal3dummy/Android.mk
+
+else
+
 include $(LOCAL_PATH)/Camera.mk
 #include $(LOCAL_PATH)/Camera_Utest.mk
 #include $(LOCAL_PATH)/Utest_jpeg.mk
 
 # include $(call all-subdir-makefiles)
 include $(call first-makefiles-under,$(LOCAL_PATH))
+
+endif
