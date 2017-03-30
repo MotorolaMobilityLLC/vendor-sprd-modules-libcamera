64ddf280acc9b562ccaa8d0315ad1575a42c6105
zhidong.li <zhidong.li@spreadtrum.com>
2017-03-30 17:02:19 +0800

    Bug #657834 add sharkl2 lsc_ctrl log_tag
    [root cause  ] add log_tag
    [changes     ] lsc_ctrl.c
    [side effects] NA
    [self test   ] 9850ka_1h10, build is ok.
    [reviewers   ] jiawei.he
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I5e997c6a7be331fa81fd7e7ffdf290567d66f04c

--- isp2.1/lsc/lsc_ctrl.c
+++ isp2.1/lsc/lsc_ctrl.c
@@ -1,12 +1,25 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#define LOG_TAG "lsc_ctrl"
 #include "lsc_adv.h"
 #include "isp_adpt.h"
 #include "isp_dev_access.h"
-
 #include <dlfcn.h>
 
 #define SMART_LSC_VERSION 1
-
-
 
 /************************************* INTERNAL DATA TYPE ***************************************/
 struct lsc_ctrl_work_lib {
