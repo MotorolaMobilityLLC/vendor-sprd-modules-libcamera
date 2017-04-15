b5cb4c00650bf5f2f20fad2632fcd0096f76df71
wenbin.guo <wenbin.guo@spreadtrum.com>
2017-04-15 16:56:15 +0800

    Bug #663509 remove trigger lib high frequency log for isp3.0
    [root cause  ] high frequency log
    [changes     ] remove log
    [side effects] high speed
    [self test   ]<board, Test cases summary>:preview,capture
    [reviewers   ] aoke.hu andrew.lin
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I2e7d37c302f8df808ce13fe4c23a436f5920ee30

--- isp3.0/af/altek/inc/aft_interface.h
+++ isp3.0/af/altek/inc/aft_interface.h
@@ -1,17 +1,24 @@
 /*
- * Copyright (C) 2012 The Android Open Source Project
+ *******************************************************************************
+ * $Header$
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Copyright (c) 2016-2025 Spreadtrum Inc. All rights reserved.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *  +-----------------------------------------------------------------+
+ *  | THIS SOFTWARE IS FURNISHED UNDER A LICENSE AND MAY ONLY BE USED |
+ *  | AND COPIED IN ACCORDANCE WITH THE TERMS AND CONDITIONS OF SUCH  |
+ *  | A LICENSE AND WITH THE INCLUSION OF THE THIS COPY RIGHT NOTICE. |
+ *  | THIS SOFTWARE OR ANY OTHER COPIES OF THIS SOFTWARE MAY NOT BE   |
+ *  | PROVIDED OR OTHERWISE MADE AVAILABLE TO ANY OTHER PERSON. THE   |
+ *  | OWNERSHIP AND TITLE OF THIS SOFTWARE IS NOT TRANSFERRED.        |
+ *  |                                                                 |
+ *  | THE INFORMATION IN THIS SOFTWARE IS SUBJECT TO CHANGE WITHOUT   |
+ *  | ANY PRIOR NOTICE AND SHOULD NOT BE CONSTRUED AS A COMMITMENT BY |
+ *  | SPREADTRUM INC.                                                 |
+ *  +-----------------------------------------------------------------+
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *
+ *******************************************************************************
  */
 
 #ifndef _AFT_INTERFACE_H_
