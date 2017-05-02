a85916b4fd0fb4947c1fc6cdfa2621288ab385a5
eileen.li <eileen.li@spreadtrum.com>
2017-05-02 18:35:35 +0800

    Bug #671907 Add cmc debug info
    [root cause  ] Add cmc debug info
    [changes     ] Add cmc debug info
    [side effects] no
    [self test   ]<board, Test cases summary>:
    [reviewers   ] james.xiong
    [change_type ] feature
    [tag_product ] common
    
    Change-Id: I3b593d480048838d917e4ca323082c56e832b5d0

--- isp2.1/param_manager/src/isp_blk_cmc10.c
+++ isp2.1/param_manager/src/isp_blk_cmc10.c
@@ -227,6 +227,12 @@ cmr_s32 _pm_cmc10_get_param(void *cmc10_param, cmr_u32 cmd, void *rtn_param0, vo
 		*update_flag = 0;
 		break;
 
+      case ISP_PM_BLK_CMC10:
+		param_data_ptr->data_ptr = cmc10_ptr->cur.matrix.val;
+		param_data_ptr->data_size = 9*sizeof(uint16_t);
+		*update_flag = 0;
+	break;
+
 	default:
 		break;
 	}
