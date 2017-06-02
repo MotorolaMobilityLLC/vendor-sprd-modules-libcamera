ed26a9ca6299c9164e2bf51fe92e5deb8ab910be
wenbin.guo <wenbin.guo@spreadtrum.com>
2017-06-02 18:45:58 +0800

    Bug #671054 init get vcm pos to 0
    [root cause  ] it has default vaule
    [changes     ] init to 0
    [side effects] none
    [self test   ]<board, Test cases summary>:9853i,preview,capture
    [reviewers   ] aoke.hu
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I177304b7511bd3f0dbb635b7d41508b65e9b4b7e

--- isp2.2/af/sprd/af_sprd_adpt_v1.c
+++ isp2.2/af/sprd/af_sprd_adpt_v1.c
@@ -619,6 +619,7 @@ static cmr_u8 if_get_motor_pos(cmr_u16 * motor_pos, void *cookie)
 	struct afctrl_cxt *cxt_ptr = (struct afctrl_cxt *)af->caller;
 	struct isp_alg_fw_context *isp_ctx = (struct isp_alg_fw_context *)cxt_ptr->caller_handle;
 	// read
+	*motor_pos = 0;
 	if (NULL != af->vcm_ops.get_motor_pos) {
 		//af->vcm_ops.get_motor_pos(af->caller, motor_pos);
 		af->vcm_ops.get_motor_pos(isp_ctx->ioctrl_ptr->caller_handler, motor_pos);
