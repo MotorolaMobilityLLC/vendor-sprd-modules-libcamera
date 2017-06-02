c0e812c7b0a5e349e9b9695ae0934f7ac75b1c10
Qin Wang <qin.wang@spreadtrum.com>
2017-06-02 13:37:59 +0800

    Bug #682887 Add dualflash block for PM mode(sync 2.3)
    [root cause  ] PM mode no dualflash block
    [changes     ] add dualflash block for PM mode
    [side effects] NA
    [self test   ]<board, Test cases summary>:Sharkl2 preview/capture/video
    ok
    [reviewers   ] eddy.wei hu.dong
    [change_type ] feature_bugfix
    [tag_product ] common
    
    patch from I5691a6090f1b0615ce1a214cd30bb299055e14a4
    
    Change-Id: I374b5a1463208df9eab4b0afee5d946b8a47b5df

--- camdrv/isp2.3/param_manager/isp_blocks_cfg.h
+++ camdrv/isp2.3/param_manager/isp_blocks_cfg.h
@@ -315,6 +315,10 @@ cmr_s32 _pm_af_new_init(void *dst_af_new_param, void *src_af_new_param, void *pa
 cmr_s32 _pm_af_new_set_param(void *af_new_param, cmr_u32 cmd, void *param_ptr0, void *param_ptr1);
 cmr_s32 _pm_af_new_get_param(void *af_new_param, cmr_u32 cmd, void *rtn_param0, void *rtn_param1);
 
+cmr_s32 _pm_dualflash_init(void *dst_flash_param, void *src_flash_param, void *param1, void *param2);
+cmr_s32 _pm_dualflash_set_param(void *flash_param, cmr_u32 cmd, void *param_ptr0, void *param_ptr1);
+cmr_s32 _pm_dualflash_get_param(void *flash_param, cmr_u32 cmd, void *rtn_param0, void *rtn_param1);
+
 struct isp_block_operations {
 	cmr_s32(*init) (void *blk_ptr, void *param_ptr0, void *param_ptr1, void *param_ptr2);
 	cmr_s32(*set) (void *blk_ptr, cmr_u32 cmd, void *param_ptr0, void *param_ptr1);
