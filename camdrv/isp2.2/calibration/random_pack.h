650d9fbb8947f170ae0284f20172169d38dccef6
james.xiong <james.xiong@spreadtrum.com>
2017-06-07 14:47:35 +0800

    Bug #677085, divide isp2.2 into camdrv and ispalg, move code into camdrv
    [root cause  ]
    [changes     ]
    [side effects]
    [self test   ]<board, Test cases summary>:
    [reviewers   ]
    [change_type ] feature
    [tag_product ] specific
    
    Change-Id: Ied02cd3f329de5c35da6d7099d35195e9be46bca

---
+++ camdrv/isp2.2/calibration/random_pack.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef _RANDOM_PACK_H_
+#define _RANDOM_PACK_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct random_lsc_info {
+	/* 1: tshark algorithm; 2: sharkl/tshark2 algorithm  */
+	cmr_u32 alg_version;
+	/* 0: normal gain (16 bits for one gain); 1: compress gain (14 bit for one gain)  */
+	cmr_u32 compress;
+	cmr_u32 base_gain;
+	/* correction percent: 1-100 */
+	cmr_u32 percent;
+	cmr_u32 grid_width;
+	cmr_u32 grid_height;
+	/*four channel gain placed one by one */
+	cmr_u16 *chn_gain[4];
+	cmr_u16 chn_gain_size;
+	/*0: gr, 1: r, 2: b, 3: gb */
+	cmr_u32 bayer_pattern;
+	cmr_u32 img_width;
+	cmr_u32 img_height;
+	cmr_u32 gain_width;
+	cmr_u32 gain_height;
+	cmr_u32 center_x;
+	cmr_u32 center_y;
+};
+
+struct random_awb_info {
+	/*average value of std image */
+	cmr_u16 avg_r;
+	cmr_u16 avg_g;
+	cmr_u16 avg_b;
+};
+
+//calibration in para
+struct random_pack_param {
+	struct random_lsc_info lsc_info;
+	struct random_awb_info awb_info;
+	void *target_buf;
+	cmr_u32 target_buf_size;
+};
+
+struct random_pack_result {
+	cmr_u32 real_size;
+};
+
+cmr_s32 random_pack(struct random_pack_param *param, struct random_pack_result *result);
+cmr_s32 get_random_pack_size(cmr_u32 chn_gain_size, cmr_u32 * size);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ camdrv/isp2.2/middleware/inc/isp_match.h
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef _ISP_MATCH_H_
+#define _ISP_MATCH_H_
+/*----------------------------------------------------------------------------*
+**				Dependencies					*
+**---------------------------------------------------------------------------*/
+
+#include "isp_type.h"
+#include "cmr_sensor_info.h"
+#include "ae_ctrl.h"
+//#include "ae_com.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+struct awb_match_data {
+	uint32_t ct;
+	uint32_t ct_flash_off;
+	uint32_t ct_capture;
+	uint32_t is_update;
+	uint32_t awb_states;
+	uint16_t light_source;
+	uint32_t awb_decision;
+};
+
+struct ae_otp_param {
+	struct sensor_otp_ae_info otp_info;
+};
+
+struct ae_sync_out {
+	uint32_t updata_flag;
+	uint32_t exposure_time;
+	uint32_t exposure_line;
+	uint32_t sensor_ad_gain;
+	uint32_t exposure_dummy;
+	uint32_t isp_d_gain;
+};
+
+struct sensor_info {
+	cmr_s16 min_exp_line;
+	cmr_s16 max_again;
+	cmr_s16 min_again;
+	cmr_s16 sensor_gain_precision;
+	cmr_s16 line_time;
+};
+
+struct module_sensor_info {
+	struct sensor_info slave_sensor_info;
+	struct sensor_info master_sensor_info;
+};
+
+struct module_otp_info{
+	struct ae_otp_param master_ae_otp;
+	struct ae_otp_param slave_ae_otp;
+};
+
+struct module_info {
+	struct module_sensor_info module_sensor_info;
+	struct module_otp_info module_otp_info;
+};
+
+struct ae_match_data {
+	struct ae_alg_calc_result ae_calc_result;
+	struct ae_sync_out ae_sync_result;
+};
+
+struct match_data_param {
+	struct module_info module_info;
+	struct ae_match_data master_ae_info;
+	struct ae_match_data slave_ae_info;
+	struct awb_match_data master_awb_info;
+	struct awb_match_data slave_awb_info;
+};
+
+cmr_handle isp_br_get_3a_handle(uint8_t is_master);
+int32_t isp_br_init(uint8_t is_master, void* isp_3a_handle);
+int32_t isp_br_deinit(uint8_t is_master);
+int32_t isp_br_ioctrl(uint32_t is_master, enum isp_br_ioctl_cmd cmd, void *in, void *out);
+int32_t isp_br_save_dual_otp(uint32_t camera_id, struct sensor_dual_otp_info *dual_otp);
+int32_t isp_br_get_dual_otp(uint32_t camera_id, struct sensor_dual_otp_info **dual_otp);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
