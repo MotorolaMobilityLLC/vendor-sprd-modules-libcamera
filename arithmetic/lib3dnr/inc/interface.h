e7d919fa143f1523a46da9c6f62e0430d96e1567
Xiaoshuang Guo <xiaoshuang.guo@spreadtrum.com>
2017-08-10 14:24:50 +0800

    Bug #721782 vendor: libcamera: unify 3dnr code format
    [root cause  ] 3dnr code format is not good
    [changes     ] 3dnr code format
    [side effects] NA
    [self test   ]: preview/capture ok
    [download normally]: Yes
    [power on/off normally]:Yes
    [do common repository/branch inspection]:Yes
    [is there dependence]:No
    [confirm dependent commit]:No
    [board]:9850ka
    [test case]:preview/capture ok
    [reviewers   ] wuyi.shen
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Unify 3dnr code format.
    
    Change-Id: Id01e986546f641bc90e8612f6b07521281c47b5c

--- arithmetic/lib3dnr/inc/interface.h
+++ arithmetic/lib3dnr/inc/interface.h
@@ -1,7 +1,6 @@
 #ifndef _3DNR_INTERFACE_H
 #define _3DNR_INTERFACE_H
 
-
 #include <stdint.h>
 
 #ifdef __cplusplus
@@ -10,42 +9,39 @@ extern "C" {
 
 #include "cmr_types.h"
 
-typedef struct c3dnr_buffer
-{
+typedef struct c3dnr_buffer {
 	uint8_t *bufferY;
 	uint8_t *bufferU;
 	uint8_t *bufferV;
-	int32_t fd; //wxz: for phy addr.
-}c3dnr_buffer_t;
+	int32_t fd;		//for phy addr.
+} c3dnr_buffer_t;
 
-typedef struct c3dn_io_info
-{
-        c3dnr_buffer_t image[3];
-        cmr_u32 width;
-        cmr_u32 height;
-        cmr_s8 mv_x;
-        cmr_s8 mv_y;
-        cmr_u8 blending_no;
-}c3dnr_io_info_t;
+typedef struct c3dn_io_info {
+	c3dnr_buffer_t image[3];
+	cmr_u32 width;
+	cmr_u32 height;
+	cmr_s8 mv_x;
+	cmr_s8 mv_y;
+	cmr_u8 blending_no;
+} c3dnr_io_info_t;
 
-typedef cmr_int (* isp_ioctl_fun)(cmr_handle isp_handler, c3dnr_io_info_t *param_ptr);
+typedef cmr_int (*isp_ioctl_fun) (cmr_handle isp_handler, c3dnr_io_info_t *param_ptr);
 
-typedef struct c3dnr_param_info
-{
-        uint16_t orig_width;
-        uint16_t orig_height;
+typedef struct c3dnr_param_info {
+	uint16_t orig_width;
+	uint16_t orig_height;
 	uint16_t small_width;
 	uint16_t small_height;
-        uint16_t total_frame_num;
+	uint16_t total_frame_num;
 	c3dnr_buffer_t *poutimg;
-	cmr_handle isp_handle; //wxz: call the isp_ioctl need the handle.
+	cmr_handle isp_handle;		//call the isp_ioctl need the handle.
 	isp_ioctl_fun isp_ioctrl;
-	c3dnr_buffer_t out_img[2]; //wxz: for preview
-}c3dnr_param_info_t;
+	c3dnr_buffer_t out_img[2];	//for preview
+} c3dnr_param_info_t;
 
 int threednr_init(c3dnr_param_info_t *param);
 int threednr_function(c3dnr_buffer_t *small_image, c3dnr_buffer_t *orig_image);
-int threednr_function_pre(c3dnr_buffer_t *small_image,c3dnr_buffer_t *orig_image);
+int threednr_function_pre(c3dnr_buffer_t *small_image, c3dnr_buffer_t *orig_image);
 int threednr_deinit();
 int threednr_callback();
 int threednr_cancel();
@@ -53,5 +49,4 @@ int threednr_cancel();
 #ifdef __cplusplus
 }
 #endif
-
 #endif
