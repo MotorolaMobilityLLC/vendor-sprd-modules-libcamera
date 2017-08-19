956911d7ea5475d039b7a783ed2f261f69c193c0
zhapplerzhang <zhappler.zhang@spreadtrum.com>
2017-08-19 11:42:42 +0800

    Bug #732444 [ZTE][Blade V8][bu64247]vcm reset when getting vcm pos error;
    [root cause  ] I2C read parameter is error;
    [changes     ] modify I2c read parameters
    [side effects] no
    [self test   ]:af function is ok
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:sp9853i_10c10_vmm_tos_userdebug
    [test case]: Touch AF, caf;
    [reviewers   ] windsor.ma miller.song
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    modify bu64247 driver i2c read parameters
    
    Change-Id: I0453af792a03585be592a9951c4695a4a684874b
    Signed-off-by: zhappler.zhang <zhappler.zhang@spreadtrum.com>

--- sensor/af_drv/bu64297gwz/af_bu64297gwz.c
+++ sensor/af_drv/bu64297gwz/af_bu64297gwz.c
@@ -27,128 +27,130 @@ uint32_t bu64297gwz_get_pose_dis(SENSOR_HW_HANDLE handle, uint32_t *up2h, uint32
 }
 #endif
 
-static int bu64297gwz_drv_create(struct af_drv_init_para *input_ptr, cmr_handle* sns_af_drv_handle)
-{
-	cmr_int ret = AF_SUCCESS;
-	CHECK_PTR(input_ptr);
-	ret = af_drv_create(input_ptr,sns_af_drv_handle);
-	if(ret != AF_SUCCESS) {
-		ret = AF_FAIL;
-	} else {
-		ret = _bu64297gwz_drv_init(*sns_af_drv_handle);
-		if (ret != AF_SUCCESS)
-		ret = AF_FAIL;
-	}
+static int bu64297gwz_drv_create(struct af_drv_init_para *input_ptr,
+                                 cmr_handle *sns_af_drv_handle) {
+    cmr_int ret = AF_SUCCESS;
+    CHECK_PTR(input_ptr);
+    ret = af_drv_create(input_ptr, sns_af_drv_handle);
+    if (ret != AF_SUCCESS) {
+        ret = AF_FAIL;
+    } else {
+        ret = _bu64297gwz_drv_init(*sns_af_drv_handle);
+        if (ret != AF_SUCCESS)
+            ret = AF_FAIL;
+    }
 
-	return ret;
+    return ret;
 }
 
-static int bu64297gwz_drv_delete(cmr_handle sns_af_drv_handle, void* param)
-{
-	cmr_int ret = AF_SUCCESS;
-	CHECK_PTR(sns_af_drv_handle);
-	ret = af_drv_delete(sns_af_drv_handle,param);
-	return ret;
+static int bu64297gwz_drv_delete(cmr_handle sns_af_drv_handle, void *param) {
+    cmr_int ret = AF_SUCCESS;
+    CHECK_PTR(sns_af_drv_handle);
+    ret = af_drv_delete(sns_af_drv_handle, param);
+    return ret;
 }
 
-static int bu64297gwz_drv_set_pos(cmr_handle sns_af_drv_handle, uint16_t pos)
-{
-	uint32_t ret_value = SENSOR_SUCCESS;
-	uint8_t cmd_val[2] = { 0x00 };
-	struct sns_af_drv_cxt *af_drv_cxt = (struct sns_af_drv_cxt*)sns_af_drv_handle;
-	CHECK_PTR(sns_af_drv_handle);
-	int32_t target_code = 0;
+static int bu64297gwz_drv_set_pos(cmr_handle sns_af_drv_handle, uint16_t pos) {
+    uint32_t ret_value = SENSOR_SUCCESS;
+    uint8_t cmd_val[2] = {0x00};
+    struct sns_af_drv_cxt *af_drv_cxt =
+        (struct sns_af_drv_cxt *)sns_af_drv_handle;
+    CHECK_PTR(sns_af_drv_handle);
+    int32_t target_code = 0;
 
-	if ((int32_t)pos < 0)
-		pos = 0;
-	else if ((int32_t)pos > 0x3FF)
-		pos = 0x3FF;
-	target_code = pos& 0x3FF;
-	CMR_LOGI("%d", target_code);
-	cmd_val[0] = ((target_code >> 8) & 0x03) | 0xC4;
-	cmd_val[1] = target_code & 0xff;
-	ret_value = hw_Sensor_WriteI2C(af_drv_cxt->hw_handle,bu64297gwz_VCM_SLAVE_ADDR, (uint8_t *) & cmd_val[0], 2);
-	return ret_value;
+    if ((int32_t)pos < 0)
+        pos = 0;
+    else if ((int32_t)pos > 0x3FF)
+        pos = 0x3FF;
+    target_code = pos & 0x3FF;
+    CMR_LOGI("%d", target_code);
+    cmd_val[0] = ((target_code >> 8) & 0x03) | 0xC4;
+    cmd_val[1] = target_code & 0xff;
+    ret_value =
+        hw_Sensor_WriteI2C(af_drv_cxt->hw_handle, bu64297gwz_VCM_SLAVE_ADDR,
+                           (uint8_t *)&cmd_val[0], 2);
+    return ret_value;
 }
 
-static int bu64297gwz_drv_get_pos(cmr_handle sns_af_drv_handle, uint16_t *pos)
-{
-	uint8_t cmd_val[2] = {0xc4, 0x00};
-	struct sns_af_drv_cxt *af_drv_cxt = (struct sns_af_drv_cxt*)sns_af_drv_handle;
-	CHECK_PTR(sns_af_drv_handle);
-	
-	hw_Sensor_ReadI2C(af_drv_cxt->hw_handle,(0x18 >> 1),(uint8_t*)&cmd_val[0],2);
-	*pos = (cmd_val[0]&0x03)<<8;
-	*pos += cmd_val[1];
-	CMR_LOGI("vcm pos %d",*pos);
-	return 0;
+static int bu64297gwz_drv_get_pos(cmr_handle sns_af_drv_handle, uint16_t *pos) {
+    uint8_t cmd_val[2] = {0xc4, 0x00};
+    struct sns_af_drv_cxt *af_drv_cxt =
+        (struct sns_af_drv_cxt *)sns_af_drv_handle;
+    CHECK_PTR(sns_af_drv_handle);
+
+    hw_Sensor_ReadI2C(af_drv_cxt->hw_handle, (0x18 >> 1),
+                      (uint8_t *)&cmd_val[0],
+                      (2 << 16) | SENSOR_I2C_VAL_16BIT | SENSOR_I2C_VAL_8BIT);
+    *pos = (cmd_val[0] & 0x03) << 8;
+    *pos += cmd_val[1];
+    CMR_LOGI("vcm pos %d", *pos);
+    return 0;
 }
-static int bu64297gwz_drv_ioctl(cmr_handle sns_af_drv_handle, enum sns_cmd cmd, void* param)
-{
-	uint32_t ret_value = AF_SUCCESS;
-	struct sns_af_drv_cxt *af_drv_cxt = (struct sns_af_drv_cxt*)sns_af_drv_handle;
-	CHECK_PTR(sns_af_drv_handle);
-	switch(cmd) {
-		case CMD_SNS_AF_SET_BEST_MODE:
-			break;
-		case CMD_SNS_AF_GET_TEST_MODE:
-			break;
-		case CMD_SNS_AF_SET_TEST_MODE:
-			break;
-		default:
-			break;
-	}
-	return ret_value;
+static int bu64297gwz_drv_ioctl(cmr_handle sns_af_drv_handle, enum sns_cmd cmd,
+                                void *param) {
+    uint32_t ret_value = AF_SUCCESS;
+    struct sns_af_drv_cxt *af_drv_cxt =
+        (struct sns_af_drv_cxt *)sns_af_drv_handle;
+    CHECK_PTR(sns_af_drv_handle);
+    switch (cmd) {
+    case CMD_SNS_AF_SET_BEST_MODE:
+        break;
+    case CMD_SNS_AF_GET_TEST_MODE:
+        break;
+    case CMD_SNS_AF_SET_TEST_MODE:
+        break;
+    default:
+        break;
+    }
+    return ret_value;
 }
 
-struct sns_af_drv_entry bu64297gwz_drv_entry =
-{
-		.motor_avdd_val = SENSOR_AVDD_3000MV,
-		.default_work_mode = 0,
-		.af_ops =
-		{
-			.create  = bu64297gwz_drv_create,
-			.delete  = bu64297gwz_drv_delete,
-			.set_pos = bu64297gwz_drv_set_pos,
-			.get_pos = bu64297gwz_drv_get_pos,
-			.ioctl   = bu64297gwz_drv_ioctl,
-		},
+struct sns_af_drv_entry bu64297gwz_drv_entry = {
+    .motor_avdd_val = SENSOR_AVDD_3000MV,
+    .default_work_mode = 0,
+    .af_ops =
+        {
+            .create = bu64297gwz_drv_create,
+            .delete = bu64297gwz_drv_delete,
+            .set_pos = bu64297gwz_drv_set_pos,
+            .get_pos = bu64297gwz_drv_get_pos,
+            .ioctl = bu64297gwz_drv_ioctl,
+        },
 };
 
-static int _bu64297gwz_drv_init(cmr_handle sns_af_drv_handle)
-{
-	struct sns_af_drv_cxt *af_drv_cxt = (struct sns_af_drv_cxt*)sns_af_drv_handle;
-	CHECK_PTR(sns_af_drv_handle);
+static int _bu64297gwz_drv_init(cmr_handle sns_af_drv_handle) {
+    struct sns_af_drv_cxt *af_drv_cxt =
+        (struct sns_af_drv_cxt *)sns_af_drv_handle;
+    CHECK_PTR(sns_af_drv_handle);
 
-	uint8_t cmd_val[2] = { 0x00 };
-	uint16_t slave_addr = bu64297gwz_VCM_SLAVE_ADDR;
-	uint16_t cmd_len = 0;
-	uint32_t ret_value = SENSOR_SUCCESS;
+    uint8_t cmd_val[2] = {0x00};
+    uint16_t slave_addr = bu64297gwz_VCM_SLAVE_ADDR;
+    uint16_t cmd_len = 0;
+    uint32_t ret_value = SENSOR_SUCCESS;
 
-	cmd_val[0] = 0xcc;
-	cmd_val[1] = 0x02; //0x4b;
-	cmd_len = 2;
-	ret_value =
-	hw_Sensor_WriteI2C(af_drv_cxt->hw_handle, slave_addr, (uint8_t *) & cmd_val[0], cmd_len);
+    cmd_val[0] = 0xcc;
+    cmd_val[1] = 0x02; // 0x4b;
+    cmd_len = 2;
+    ret_value = hw_Sensor_WriteI2C(af_drv_cxt->hw_handle, slave_addr,
+                                   (uint8_t *)&cmd_val[0], cmd_len);
 
-	cmd_val[0] = 0xd4;
-	cmd_val[1] = 0x96;
-	cmd_len = 2;
-	ret_value =
-	hw_Sensor_WriteI2C(af_drv_cxt->hw_handle, slave_addr, (uint8_t *) & cmd_val[0], cmd_len);
+    cmd_val[0] = 0xd4;
+    cmd_val[1] = 0x96;
+    cmd_len = 2;
+    ret_value = hw_Sensor_WriteI2C(af_drv_cxt->hw_handle, slave_addr,
+                                   (uint8_t *)&cmd_val[0], cmd_len);
 
-	cmd_val[0] = 0xdc;
-	cmd_val[1] = 0x03;
-	cmd_len = 2;
-	ret_value =
-	hw_Sensor_WriteI2C(af_drv_cxt->hw_handle, slave_addr, (uint8_t *) & cmd_val[0], cmd_len);
+    cmd_val[0] = 0xdc;
+    cmd_val[1] = 0x03;
+    cmd_len = 2;
+    ret_value = hw_Sensor_WriteI2C(af_drv_cxt->hw_handle, slave_addr,
+                                   (uint8_t *)&cmd_val[0], cmd_len);
 
-	cmd_val[0] = 0xe4;
-	cmd_val[1] = 0x02;
-	cmd_len = 2;
-	ret_value =
-	hw_Sensor_WriteI2C(af_drv_cxt->hw_handle, slave_addr, (uint8_t *) & cmd_val[0], cmd_len);
+    cmd_val[0] = 0xe4;
+    cmd_val[1] = 0x02;
+    cmd_len = 2;
+    ret_value = hw_Sensor_WriteI2C(af_drv_cxt->hw_handle, slave_addr,
+                                   (uint8_t *)&cmd_val[0], cmd_len);
 
-	return ret_value;
+    return ret_value;
 }
-
