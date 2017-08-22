4a18f567b8eaffcefd0c609c8ec89daa918472e7
fengjie.chen <fengjie.chen@spreadtrum.com>
2017-08-22 20:20:39 +0800

    Bug #717228  Camera FOV Calibration failed.
    [root cause  ] the focal length of sensor is wrong.
    [changes     ] modify focal length to right.
    [side effects] no
    [self test   ]: no
    [download normally]: yes.
    [power on/off normally]: yes.
    [do common repository/branch inspection]: no.
    [is there dependence]: no
    [confirm dependent commit]: yes.
    [board]: sharklj1
    [test case]: no.
    [reviewers   ] miller.song
    [change_type ] AOB --> sprd_adaptation
    [tag_product ] common
    
    Change-Id: I26fa196a1edf88c80393485fe3a0f7daee290419

--- sensor/sensor_drv/classic/OmniVision/ov13855/sensor_ov13855_mipi_raw.h
+++ sensor/sensor_drv/classic/OmniVision/ov13855/sensor_ov13855_mipi_raw.h
@@ -485,7 +485,7 @@ static SENSOR_STATIC_INFO_T s_ov13855_static_info[VENDOR_NUM] = {
                      .exp_valid_frame_num = 1,
                      .clamp_level = 64,
                      .adgain_valid_frame_num = 1,
-                     .fov_info = {{4.815f, 3.6783f}, 3.95f}}}
+                     .fov_info = {{4.815f, 3.6783f}, 3.74f}}}
     /*If there are multiple modules,please add here*/
 };
 
