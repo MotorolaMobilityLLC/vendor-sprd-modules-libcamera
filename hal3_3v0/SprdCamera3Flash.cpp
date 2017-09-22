153459b3b41dd4c16c16e92067cdbf11c69b174b
stevenying <steven.ying@spreadtrum.com>
2017-09-22 13:33:41 +0800

    Bug #745752 Add flash torch config the index of led and default value
    [root cause  ] Add flash torch config the index of led and default value
    [changes     ] sensor hal
    [side effects] NA
    [self test   ]:
    [download normally]:Yes
    [power on/off normally]:Yes
    [do common repository/branch inspection]:Yes
    [is there dependence]:No
    [confirm dependent commit]:No
    [board]:sharkle sharklj1
    [test case]:preview catpure
    [reviewers   ] miller.song
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I3b3973d025e20852a54198e2862c897758fa7db1

--- hal3_3v0/SprdCamera3Flash.cpp
+++ hal3_3v0/SprdCamera3Flash.cpp
@@ -109,6 +109,8 @@ int32_t SprdCamera3Flash::registerCallbacks(
 *==========================================================================*/
 int32_t SprdCamera3Flash::setFlashMode(const int camera_id, const bool mode) {
     int32_t retVal = 0;
+    int32_t bytes = 0;
+    char buffer[16];
     const char *const flashInterface =
         "/sys/devices/virtual/misc/sprd_flash/test";
     ssize_t wr_ret;
@@ -119,13 +121,12 @@ int32_t SprdCamera3Flash::setFlashMode(const int camera_id, const bool mode) {
         LOGE("Failed to open: flash_light_interface, %s", flashInterface);
         return -EINVAL;
     }
-    if (mode) {
-        m_flashOn[0] = SPRD_FLASH_STATUS_ON;
-        wr_ret = write(fd, SPRD_FLASH_CMD_ON, 4);
-    } else {
-        m_flashOn[0] = SPRD_FLASH_STATUS_OFF;
-        wr_ret = write(fd, SPRD_FLASH_CMD_OFF, 4);
-    }
+
+    m_flashOn[0] = mode ? SPRD_FLASH_STATUS_ON : SPRD_FLASH_STATUS_OFF;
+
+    bytes = snprintf(buffer, sizeof(buffer), "0x%x", 0x11 ^ mode);
+    wr_ret = write(fd, buffer, bytes);
+
     if (-1 == wr_ret) {
         LOGE("WRITE FAILED \n");
         retVal = -EINVAL;
