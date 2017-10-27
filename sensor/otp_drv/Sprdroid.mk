a19603c5d744ce50e999d1a032e849960b94963a
steven.ying <steven.ying@spreadtrum.com>
2017-10-27 10:32:44 +0800

    Bug #748461 k44_trunk otp makefile search optimize
    [root cause  ] k44_trunk otp makefile search optimize
    [changes     ] sensor hal
    [side effects] NA
    [self test   ]:
    [download normally]:Yes
    [power on/off normally]:Yes
    [do common repository/branch inspection]:Yes
    [is there dependence]:No
    [confirm dependent commit]:No
    [board]:sharkle sharklj1
    [test case]:preview catpure
    [reviewers   ] miller.song
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I86fa461e4bafaee8764cf61a31f129a71464822a

--- sensor/otp_drv/Sprdroid.mk
+++ sensor/otp_drv/Sprdroid.mk
@@ -16,34 +16,6 @@
 CUR_DIR := otp_drv
 
 LOCAL_SRC_DIR := $(LOCAL_PATH)/$(CUR_DIR)
+LOCAL_SRC_FILES += $(shell find $(LOCAL_SRC_DIR) -name '*.c' | sed s:^$(LOCAL_PATH)/::g )
 LOCAL_C_INCLUDES += $(shell find $(LOCAL_SRC_DIR) -name '*.h' | sed -r 's/(.*)\//\1 /' | cut -d" " -f1 )
 
-LOCAL_SRC_FILES += \
-	otp_drv/otp_common.c
-
-ifeq ($(strip $(TARGET_BOARD_CAMERA_ISP_DIR)),3)
-LOCAL_SRC_FILES += \
-	otp_drv/driver/imx258_truly/imx258_truly_otp_drv.c \
-	otp_drv/driver/s5k3p8sm_truly/s5k3p8sm_truly_otp_drv.c
-else
-LOCAL_SRC_FILES += \
-    otp_drv/driver/ov8856_shine/ov8856_shine_otp_drv.c \
-	otp_drv/driver/gc2375_altek/gc2375_altek_otp_drv.c \
-	otp_drv/driver/gc5024_common/gc5024_common_otp_drv.c \
-	otp_drv/driver/imx258/imx258_otp_drv.c \
-	otp_drv/driver/ov5675_sunny/ov5675_sunny_otp_drv.c \
-	otp_drv/driver/ov13855/ov13855_otp_drv.c \
-	otp_drv/driver/ov13855_altek/ov13855_altek_otp_drv.c \
-	otp_drv/driver/ov13855_sunny/ov13855_sunny_otp_drv.c \
-	otp_drv/driver/s5k3l8xxm3_qtech/s5k3l8xxm3_qtech_otp_drv.c \
-	otp_drv/driver/s5k3l8xxm3_reachtech/s5k3l8xxm3_reachtech_otp_drv.c \
-	otp_drv/driver/ov2680_cmk/ov2680_cmk_otp_drv.c \
-	otp_drv/driver/ov8856_cmk/ov8856_cmk_otp_drv.c \
-	otp_drv/driver/ov8858_cmk/ov8858_cmk_otp_drv.c \
-	otp_drv/driver/sp8407/sp8407_otp_drv.c \
-	otp_drv/driver/sp8407_cmk/sp8407_cmk_otp_drv.c \
-	otp_drv/driver/s5k5e8yx_jd/s5k5e8yx_jd_otp_drv.c \
-	otp_drv/driver/dual_master_2e/dual_master_2e_otp_drv_1v0.c \
-	otp_drv/driver/dual_slave_2e/dual_slave_2e_otp_drv_1v0.c \
-	otp_drv/driver/single_1e/single_1e_otp_drv_1v0.c
-endif
