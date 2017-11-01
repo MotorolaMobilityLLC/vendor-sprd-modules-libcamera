2d11ea6d8f629dd3d03e5ca26619b3e47cc7a053
ming.tang <ming.tang@spreadtrum.com>
2017-11-01 17:27:29 +0800

    Bug #645797 disable camera compile
    [root cause  ] gpu compile failed
    [changes     ] disable camera compile first
    [side effects] no
    [self test   ]:camera compile
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:sp77313
    [test case]:camera preview/record
    [reviewers   ] ming.tang, huan_h.huang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I78f5bd34882ffcc85e0332cdd2c35cfc134ddaab

--- Android.mk
+++ Android.mk
@@ -1,9 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+ifneq ($(strip $(TARGET_PRODUCT)), sp7731e_1h10_native)
 include $(LOCAL_PATH)/Camera.mk
+endif
 #include $(LOCAL_PATH)/Camera_Utest.mk
 #include $(LOCAL_PATH)/Utest_jpeg.mk
 
 # include $(call all-subdir-makefiles)
+ifneq ($(strip $(TARGET_PRODUCT)), sp7731e_1h10_native)
 include $(call first-makefiles-under,$(LOCAL_PATH))
+endif
