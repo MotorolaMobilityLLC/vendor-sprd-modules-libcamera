9996a1a1207623945447bb6a7dd43a31c0346159
Qin Wang <qin.wang@spreadtrum.com>
2017-12-09 19:40:55 +0800

    Bug #794201 Deleted ISP AEM buffer
    [root cause  ] unused
    [changes     ] fw
    [side effects] n/a
    [self test   ]: yes
    [Download normally]: yes
    [Power on/off normally]:yes
    [Do common repository/branch inspection]:yes
    [Is there dependence]:no
    [Confirm dependent commit]:no
    [Board]:sp9832e_1h10_native-userdebug
    [Test Case]: opencam
    [reviewers   ] Marvin;
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    We don't use ISP AEM.
    
    Change-Id: Iba45abca7c8b1af249f4407ebcf8c178485db9ec
    Signed-off-by: Qin Wang <qin.wang@spreadtrum.com>

--- camdrv/isp2.3/middleware/isp_dev_access.c
+++ camdrv/isp2.3/middleware/isp_dev_access.c
@@ -62,7 +62,6 @@ cmr_int isp_dev_statis_buf_malloc(cmr_handle isp_dev_handle, struct isp_statis_m
 		statis_mem_info->cb_of_malloc = in_ptr->cb_of_malloc;
 		statis_mem_info->cb_of_free = in_ptr->cb_of_free;
 		statis_mem_info->isp_statis_mem_size = (ISP_AEM_STATIS_BUF_SIZE +
-							ISP_AEM_STATIS_BUF_SIZE +
 							ISP_AFM_STATIS_BUF_SIZE +
 							ISP_AFL_STATIS_BUF_SIZE +
 							ISP_HIST_STATIS_BUF_SIZE +
@@ -83,7 +82,6 @@ cmr_int isp_dev_statis_buf_malloc(cmr_handle isp_dev_handle, struct isp_statis_m
 				  statis_mem_info->buffer_client_data)) {
 					ISP_LOGE("fail to malloc statis_bq buffer");
 					return ISP_ALLOC_ERROR;
-
 				  }
 		} else {
 			ISP_LOGE("fail to malloc statis_bq buffer");
