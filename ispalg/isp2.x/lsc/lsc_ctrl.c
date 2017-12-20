a1e209c530191787d20d27c2d31e19b18011ecb5
charlei.zhang <charlei.zhang@spreadtrum.com>
2017-12-20 19:18:01 +0800

    Bug #806620 - add log show alsc alg calculate time
    [root cause  ] need get alsc alg calculate time
    [changes     ] add log show alsc calculate time
    [side effects] NO
    [self test   ]:YES
    [download normally]:YES
    [power on/off normally]:YES
    [do common repository/branch inspection]:NO
    [is there dependence]:NO
    [confirm dependent commit]:NO
    [board]:sp9832e
    [test case]:camera preview capture recording normal
    [reviewers   ] james.xiong
    [change_type ] debug_log
    [tag_product ] common
    
    Change-Id: I2fb3af87d1b64bf3e4831031d81fc26bbdf25d51

--- ispalg/isp2.x/lsc/lsc_ctrl.c
+++ ispalg/isp2.x/lsc/lsc_ctrl.c
@@ -18,6 +18,7 @@
 #include "isp_adpt.h"
 #include <dlfcn.h>
 #include "isp_mw.h"
+#include <utils/Timers.h>
 
 
 #define SMART_LSC_VERSION 1
@@ -690,9 +691,10 @@ static cmr_s32 lsc_sprd_calculation(void *handle, void *in, void *out)
 	cxt = (struct lsc_ctrl_context *)handle;
 
 	result->dst_gain = cxt->dst_gain;
-
+	cmr_u64 ae_time0 = systemTime(CLOCK_MONOTONIC);
 	rtn = cxt->lib_ops.alsc_calc(cxt->alsc_handle, param, result);
-
+	cmr_u64 ae_time1 = systemTime(CLOCK_MONOTONIC);
+	ISP_LOGV("SYSTEM_TEST -lsc_test  %dus ",(cmr_s32) ((ae_time1 - ae_time0) / 1000));
 	rtn = cxt->lib_ops.alsc_io_ctrl(cxt->alsc_handle, ALSC_GET_UPDATE_INFO, NULL, (void *)&update_info);
 	if(update_info.can_update_dest == 1){
 		memcpy(update_info.lsc_buffer_addr, result->dst_gain, dst_gain_size);
