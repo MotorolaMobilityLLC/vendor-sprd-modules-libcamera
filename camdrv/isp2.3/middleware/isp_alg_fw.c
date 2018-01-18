161c35824d5c38b90a6e04f2ef3fb77072ac8759
li1.xu <li1.xu@spreadtrum.com>
2018-01-18 15:31:36 +0800

    Bug #811962 revert workaround Skip the front cmaera frist aem stats is bypass
    [root cause  ] the front cmaera frist aem stats should is bypas
    [changes     ] revert the commit before
    [side effects] N/A
    [self test   ]:camera preview video
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:sp9832e_1h0
    [test case]:camera preview video
    [reviewers   ] qin.wang
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Iac3ebfba646f26ad26136cdc590b23f3b1a1913a

--- camdrv/isp2.3/middleware/isp_alg_fw.c
+++ camdrv/isp2.3/middleware/isp_alg_fw.c
@@ -1392,8 +1392,7 @@ static cmr_int ispalg_aem_stats_parser(cmr_handle isp_alg_handle, void *data)
 	if (ret) {
 		ISP_LOGE("fail to set statis buf");
 	}
-	if (statis_info->frame_id > 0 && cxt->camera_id == 1)
-		cxt->aem_is_update = 1;
+	cxt->aem_is_update = 1;
 	return ret;
 }
 
