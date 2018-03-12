21b4765de3db116733707fe6397ec421e57fe3a4
Prateek Gupta <prateek.gupta@spreadtrum.com>
2018-03-12 19:42:29 +0530

    Bug #840250 [isharkl2][8.1] Camera didnt open in GSI PAC
    [root cause  ] Could not load camera HAL as library libcampm.so
    not found. libcampm.so is present in systemimage but GSI PAC
    doesnt have sprd system image.
    [changes     ] Change path of libcampm.so from system/lib to
    vendor/lib.
    [side effects] none
    [self test   ]: camera sanity
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: isharkl2
    [test case]: preview,capture,video ok
    [reviewers   ] miller.song
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Id1f29fc20a978fa1b8e0e22151cdd30bfb17a6ce

--- camdrv/isp2.2/param_manager/Android.mk
+++ camdrv/isp2.2/param_manager/Android.mk
@@ -57,6 +57,10 @@ LOCAL_SHARED_LIBRARIES := libcutils libutils libdl libcamcommon
 
 LOCAL_SHARED_LIBRARIES += liblog
 
+ifeq (1, $(strip $(shell expr $(ANDROID_MAJOR_VER) \>= 8)))
+LOCAL_PROPRIETARY_MODULE := true
+endif
+
 include $(BUILD_SHARED_LIBRARY)
 
 include $(call first-makefiles-under,$(LOCAL_PATH))
