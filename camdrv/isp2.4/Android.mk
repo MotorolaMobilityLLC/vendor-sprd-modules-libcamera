85a990a1f459e50ea8108de2224f11f3e16781b3
hongcheng.li <hongcheng.li@spreadtrum.com>
2018-03-14 15:25:19 +0800

    Bug #785166 fix the issue that dynamic library loading speed is very slow
    [root cause  ] dlopen dynamic library use too much time
    [changes     ] link the libispalg.so and libjpeg_hw_sprd.so when compling libcamera
    [side effects] no
    [self test   ]:camera function test
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:pike2
    [test case]: camera preview/capture/video test
    [reviewers   ] miller.song
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I545018831b2a43db3c074cf32e87175ddf447cc1

--- camdrv/isp2.4/Android.mk
+++ camdrv/isp2.4/Android.mk
@@ -78,7 +78,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SHARED_LIBRARIES := libcutils libutils libdl liblog
 
-LOCAL_SHARED_LIBRARIES += libcamsensor libcambr libcamrt libcamcommon libcampm
+LOCAL_SHARED_LIBRARIES += libcamsensor libcambr libcamrt libcamcommon libcampm libispalg
 
 
 ifeq (1, $(strip $(shell expr $(ANDROID_MAJOR_VER) \>= 8)))
