b45853c6c82b94e885cad7ed0b45af978283ed0c
taochao.zhou <taochao.zhou@unisoc.com>
2018-08-03 19:06:14 +0800

    Bug #911475 [sharkl3] [androidp] fix androidp bringup bypass for camera module
    [root cause  ] androidp android bringup bypass camera
    [changes     ] fix androidp android bringup bypass camera
    [side effects] no
    [self test   ]:no
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:sp9863a_xxxx
    [test case]:no
    [reviewers   ] kane.li
    [change_type ] bringup
    [tag_product ] common
    
    Change-Id: Ida0a80f7a7b4eb5578502167407b29ef319b9edd

--- Android.mk
+++ Android.mk
@@ -1,10 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)),sp9863a)
+# sharkl3 bringup by pass camera
+else
+
 ifeq ($(strip $(TARGET_BOARD_CAMERA_FUNCTION_DUMMY)), true)
 include $(LOCAL_PATH)/hal3dummy/Android.mk
 else
 include $(LOCAL_PATH)/Camera.mk
 # include $(call all-subdir-makefiles)
 include $(call first-makefiles-under,$(LOCAL_PATH))
+endif
+
 endif
