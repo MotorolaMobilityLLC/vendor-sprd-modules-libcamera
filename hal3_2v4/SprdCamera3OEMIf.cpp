a2d89bf30146e7d9d7c2e3d4eefcb8609c803d1f
hongcheng.li <hongcheng.li@spreadtrum.com>
2018-09-04 20:10:59 +0800

    Bug #928865 android9.0 using new ion interface
    [root cause  ] using ion new interface
    [changes     ] using sync ion buffer interface
    [side effects] none
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:7731E
    [test case]:OK
    [reviewers   ] CCG,Miller
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: If11007dd4236cf59fbc475a97f76dc5980181b46

--- hal3_2v4/SprdCamera3OEMIf.cpp
+++ hal3_2v4/SprdCamera3OEMIf.cpp
@@ -5596,7 +5596,8 @@ int SprdCamera3OEMIf::flushIonBuffer(int buffer_fd, void *v_addr, void *p_addr,
     HAL_LOGD("E");
 
     int ret = 0;
-    ret = MemIon::Flush_ion_buffer(buffer_fd, v_addr, NULL, size);
+    //ret = MemIon::Flush_ion_buffer(buffer_fd, v_addr, NULL, size);
+    ret = MemIon::Sync_ion_buffer(buffer_fd);
     if (ret) {
         HAL_LOGE("Flush_ion_buffer failed, ret=%d", ret);
         goto exit;
