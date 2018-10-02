2dd7025fcb20b1762f0ab37aa095b57750d1fe40
Hao Yue (岳皓) <hao.yue@spreadtrum.com>
2018-10-02 11:30:39 +0800

    Bug #785516 9.0 AWB sync fov u32 to float
    [root cause  ] 785516 9.0 AWB sync fov u32 to float
    [changes     ] 785516 9.0 AWB sync fov u32 to float
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:9.0 trunk
    [test case]:yes
    [reviewers   ]
    [change_type ] feature
    [tag_product ] common
    
    Change-Id: Iaa95b6204cfdff3e0b66b9d6bb80e2775cfdc4af

--- ispalg/isp2.x/awb/sprd/inc/awb.h
+++ ispalg/isp2.x/awb/sprd/inc/awb.h
@@ -431,8 +431,8 @@ extern "C" {
 	struct awb_sync_info {
 		struct awb_stat_sync stat_master_info;
 		struct awb_stat_sync stat_slave_info;
-		cmr_u32 master_fov;
-		cmr_u32 slave_fov;
+		float master_fov;
+		float slave_fov;
 		cmr_u32 master_pix_cnt;
 		cmr_u32 slave_pix_cnt;
 		struct awb_ctrl_rgb_otp master_gldn_stat_info;
