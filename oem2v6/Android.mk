7663a8c90845c12d706c2177f5b7ec22af76d075
jun.wang1 <jun.wang1@spreadtrum.com>
2019-01-09 10:40:34 +0800

    Bug #977002 config sw 3dnr for roc1
    [root cause  ] roc1 need config sw 3dnr
    [changes     ] config sw 3dnr for roc1
    [side effects] no
    [self test   ]:
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]:roc1
    [test case]:3dnr capture
    [reviewers   ] kane.li
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I710ceb8ec993295d924e114cb2951dea13309a65

--- oem2v6/Android.mk
+++ oem2v6/Android.mk
@@ -87,7 +87,7 @@ LOCAL_SRC_FILES += src/cmr_uvdenoise.c
 endif
 
 ifeq ($(strip $(TARGET_BOARD_CAMERA_3DNR_CAPTURE)),true)
-ifeq ($(strip $(TARGET_BOARD_PLATFORM)),ums510)
+ifneq ($(filter $(strip $(TARGET_BOARD_PLATFORM)),ums510 ud710),)
 LOCAL_SRC_FILES += src/cmr_3dnr_sw.c
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../arithmetic/lib3dnr/blacksesame/inc
 LOCAL_SHARED_LIBRARIES += libtdnsTest libui libEGL libGLESv2
