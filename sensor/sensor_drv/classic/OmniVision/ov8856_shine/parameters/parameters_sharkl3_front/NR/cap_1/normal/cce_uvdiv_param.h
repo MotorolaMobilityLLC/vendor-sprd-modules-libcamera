bf4553498831c07543c02e1508f6ea4b511b42e0
enoch.zhang <enoch.zhang@unisoc.com>
2019-01-11 18:37:42 +0800

    Bug #992900 updata front camera NR param
    [root cause  ] NR param
    [changes     ] NR param
    [side effects]NA
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:sp9863a_go
    [test case]:yes
    [reviewers   ] zhappler.zhang
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Ie5075d1a4754b3457b3312cf49454fa9ff138e5d

--- sensor/sensor_drv/classic/OmniVision/ov8856_shine/parameters/parameters_sharkl3_front/NR/cap_1/normal/cce_uvdiv_param.h
+++ sensor/sensor_drv/classic/OmniVision/ov8856_shine/parameters/parameters_sharkl3_front/NR/cap_1/normal/cce_uvdiv_param.h
@@ -374,17 +374,17 @@
 	/*uvdiv_ratio*/
 	{
 		/*ratio_0*/
-		0x3E,
+		0x3A,
 		/*ratio_1*/
-		0x3E,
+		0x3A,
 		/*ratio*/
-		0x3E,
+		0x3A,
 		/*ratio_uv_min*/
-		0x3C,
+		0x3A,
 		/*ratio_y_min0*/
-		0x3C,
+		0x3A,
 		/*ratio_y_min1*/
-		0x3C,
+		0x3A,
 		/*reserved*/
 		{
 			0x00,0x00/*0-1*/
