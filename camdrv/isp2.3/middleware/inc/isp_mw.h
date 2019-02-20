be674554543dc98ec96dcab8510b9cc7c7918c38
Day.lu <day.lu@uniso.com>
2019-02-20 13:43:55 +0800

    Bug #982048 adding roi for bokeh
    [root cause ]:back ground and front ground is clear when touch
    [changes ] camdrv
    [side effects]:yes
    [self test ]:yes
    [download normally]:yes
    [power on/off normally]:on
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:s9863a
    [test case]:test bokeh
    [reviewers ]:qin.wang payne.zhang
    [change_type ]:bug
    [tag_product ]:common
    
    Change-Id: I9540c2d52343af64e846f5265cfc4417bbd1e402

--- camdrv/isp2.3/middleware/inc/isp_mw.h
+++ camdrv/isp2.3/middleware/inc/isp_mw.h
@@ -466,13 +466,21 @@ struct trim_info {
 	cmr_u32 trim_height;
 };
 
+struct isp_afctrl_roi {
+    cmr_u32 sx;
+    cmr_u32 sy;
+    cmr_u32 ex;
+    cmr_u32 ey;
+};
+
 struct isp_af_notice {
 	cmr_u32 mode;
 	cmr_u32 valid_win;
 	cmr_u32 focus_type;
 	cmr_u32 motor_pos;
 	cmr_u32 af_mode;
-	cmr_u32 reserved[8];;
+	struct isp_afctrl_roi af_roi;
+	cmr_u32 reserved[4];
 };
 
 enum isp_flash_type {
