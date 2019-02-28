25f69e4a128409b4939f18b0cba316aa908ee564
jun.wang1 <jun.wang1@spreadtrum.com>
2019-02-28 13:05:31 +0800

    Bug #1017097 af_ctrl_notice structure no switch
    [root cause  ] af_ctrl_notice structure no switch
    [changes     ] modify
    [side effects] no
    [self test   ]:
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]:sharkl5
    [test case]:bokeh
    [reviewers   ] kane.li
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I23aefcbe67f5999b97c264ac78d758bc1393f089

--- hal3_2v6/SprdCamera3OEMIf.cpp
+++ hal3_2v6/SprdCamera3OEMIf.cpp
@@ -4680,7 +4680,7 @@ void SprdCamera3OEMIf::HandleFocus(enum camera_cb_type cb, void *parm4) {
     ATRACE_BEGIN(__FUNCTION__);
 
     struct cmr_focus_status *focus_status;
-    struct af_ctrl_notice *af_ctrl;
+    struct isp_af_notice *af_ctrl;
 
     CONTROL_Tag controlInfo;
     mSetting->getCONTROLTag(&controlInfo);
@@ -4698,7 +4698,7 @@ void SprdCamera3OEMIf::HandleFocus(enum camera_cb_type cb, void *parm4) {
         HAL_LOGV("camera cb: autofocus succeeded.");
         {
             if (parm4 != NULL) {
-                af_ctrl = (af_ctrl_notice *)parm4;
+                af_ctrl = (isp_af_notice *)parm4;
                 if (af_ctrl->af_roi.sx != 0 || af_ctrl->af_roi.sy != 0 ||
                     af_ctrl->af_roi.ex != 0 || af_ctrl->af_roi.ey != 0) {
                     controlInfo.af_roi[0] = af_ctrl->af_roi.sx;
