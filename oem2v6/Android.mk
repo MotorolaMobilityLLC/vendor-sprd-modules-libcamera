f2e8fb884fa22205a2a4228c095368c28465ffe7
erick.chen <Erick.Chen@spreadtrum.com>
2019-02-28 15:41:26 +0800

    Bug #1002914 rename sharkl5 MKT ID ums510 to ums312
    [root cause  ]rename sharkl5 MKT ID ums510 to ums312
    [changes     ]rename sharkl5 MKT ID ums510 to ums312
    [side effects]NA
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:ums312
    [test case]:1\download success 2\normal boot
    [reviewers   ]huitao.yue
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I943746858891b25dd6de3fd85955cccdd6cabec7

--- oem2v6/Android.mk
+++ oem2v6/Android.mk
@@ -88,7 +88,7 @@ LOCAL_SRC_FILES += src/cmr_uvdenoise.c
 endif
 
 ifeq ($(strip $(TARGET_BOARD_CAMERA_3DNR_CAPTURE)),true)
-ifneq ($(filter $(strip $(TARGET_BOARD_PLATFORM)),ums510 ud710),)
+ifneq ($(filter $(strip $(TARGET_BOARD_PLATFORM)),ums312 ud710),)
 LOCAL_SRC_FILES += src/cmr_3dnr_sw.c
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../arithmetic/lib3dnr/blacksesame/inc
 LOCAL_SHARED_LIBRARIES += libtdnsTest libui libEGL libGLESv2
