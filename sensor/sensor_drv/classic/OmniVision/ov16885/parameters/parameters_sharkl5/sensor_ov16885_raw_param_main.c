8bf36676f6131ccd61cfe01aa2caaa237691fa0a
enoch.zhang <enoch.zhang@unisoc.com>
2019-03-04 21:14:30 +0800

    Bug #1009792 updata 4IN1 turning param20190304
    [root cause  ] cam param
    [changes     ] cam param
    [side effects]no
    [self test   ]:cap
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:sharkl5
    [test case]:camera cap
    [reviewers   ] zhappler.zhang
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I964db75b8bfff97ce990839ee21069892a0365a4

--- sensor/sensor_drv/classic/OmniVision/ov16885/parameters/parameters_sharkl5/sensor_ov16885_raw_param_main.c
+++ sensor/sensor_drv/classic/OmniVision/ov16885/parameters/parameters_sharkl5/sensor_ov16885_raw_param_main.c
@@ -34,7 +34,6 @@
 #include "sensor_ov16885_raw_param_prv_1.c"
 #include "sensor_ov16885_raw_param_cap_0.c"
 #include "sensor_ov16885_raw_param_cap_1.c"
-#include "sensor_ov16885_raw_param_cap_2.c"
 #include "sensor_ov16885_raw_param_video_0.c"
 #include "sensor_ov16885_raw_param_video_1.c"
 #include "sensor_ov16885_raw_param_video_2.c"
@@ -163,7 +162,7 @@ static struct sensor_raw_info s_ov16885_mipi_raw_info=
 		{NULL, 0},
 		{s_ov16885_tune_info_cap_0, sizeof(s_ov16885_tune_info_cap_0)},
 		{s_ov16885_tune_info_cap_1, sizeof(s_ov16885_tune_info_cap_1)},
-		{s_ov16885_tune_info_cap_2, sizeof(s_ov16885_tune_info_cap_2)},
+		{NULL, 0},
 		{NULL, 0},
 		{s_ov16885_tune_info_video_0, sizeof(s_ov16885_tune_info_video_0)},
 		{s_ov16885_tune_info_video_1, sizeof(s_ov16885_tune_info_video_1)},
@@ -181,7 +180,7 @@ static struct sensor_raw_info s_ov16885_mipi_raw_info=
 		NULL,
 		&s_ov16885_fix_info_cap_0,
 		&s_ov16885_fix_info_cap_1,
-		&s_ov16885_fix_info_cap_2,
+		NULL,
 		NULL,
 		&s_ov16885_fix_info_video_0,
 		&s_ov16885_fix_info_video_1,
@@ -196,7 +195,7 @@ static struct sensor_raw_info s_ov16885_mipi_raw_info=
 		{NULL, 0},
 		{s_ov16885_cap_0_tool_ui_input, sizeof(s_ov16885_cap_0_tool_ui_input)},
 		{s_ov16885_cap_1_tool_ui_input, sizeof(s_ov16885_cap_1_tool_ui_input)},
-		{s_ov16885_cap_2_tool_ui_input, sizeof(s_ov16885_cap_2_tool_ui_input)},
+		{NULL, 0},
 		{NULL, 0},
 		{s_ov16885_video_0_tool_ui_input, sizeof(s_ov16885_video_0_tool_ui_input)},
 		{s_ov16885_video_1_tool_ui_input, sizeof(s_ov16885_video_1_tool_ui_input)},
