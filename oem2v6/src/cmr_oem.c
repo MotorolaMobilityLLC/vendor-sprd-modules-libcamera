ac3afdaca8a74d838fcfce0c8fd744d8bd10b8c1
ares.wu <ares.wu@unisoc.com>
2019-03-12 11:15:02 +0800

    Bug #1022912 fix the problem that AI photo detect cannot change status in AE/AF lock mode
    [root cause  ] FD was swiched off by app in AE/AF lock mode
    [changes     ] libcamera
    [side effects] no
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: sharkl5
    [test case]: prev, AI scene detect
    [reviewers   ] kane.li
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Ic825518dc26ca416c5da725aeec59432d6048370

--- oem2v6/src/cmr_oem.c
+++ oem2v6/src/cmr_oem.c
@@ -7315,6 +7315,12 @@ cmr_int camera_isp_ioctl(cmr_handle oem_handle, cmr_uint cmd_type,
         ptr_flag = 1;
         isp_param_ptr = (void *)&param_ptr->threednr_param;
         break;
+    case COM_ISP_SET_AI_SET_FD_ON_OFF:
+        CMR_LOGI("set FD on/off %d", param_ptr->cmd_value);
+        isp_cmd = ISP_CTRL_AI_SET_FD_STATUS;
+        ptr_flag = 1;
+        isp_param_ptr = (void *)&param_ptr->cmd_value;
+        break;
 
     default:
         CMR_LOGE("don't support cmd %ld", cmd_type);
