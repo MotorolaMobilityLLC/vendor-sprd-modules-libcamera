9296f32da00310a628ccc22e4e3d742bc51d4b20
viola.han <viola.han@spreadtrum.com>
2019-04-10 15:34:56 +0800

    Bug #1016441 update camera HSV number from 24 to 32
    [root cause  ]increase camera PM HSV number from 24 to 32
    [changes     ] camera PM paramter
    [side effects] none
    [self test   ]: pass
    [download normally]:pass
    [power on/off normally]: ok
    [do common repository/branch inspection]:pass
    [is there dependence]:none
    [confirm dependent commit]: ok
    [board]:sharkl5 9.0
    [test case]: camera preview, capture, recording
    [reviewers   ] jerry.mao
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I69e3a30ff6b6b56fa5c6b00f5776f277263ceafd

--- ispalg/isp2.x/common/inc/sensor_raw_sharkl5.h
+++ ispalg/isp2.x/common/inc/sensor_raw_sharkl5.h
@@ -363,7 +363,7 @@ struct sensor_envi_detect_param {
 #define SENSOR_CCE_NUM 0x09
 #define SENSOR_CMC_NUM 16
 #define SENSOR_CTM_NUM 0x09
-#define SENSOR_HSV_NUM 24
+#define SENSOR_HSV_NUM 32
 #define SENSOR_LENS_NUM 0x09
 #define SENSOR_BLC_NUM 0x09
 #define SENSOR_MODE_NUM 0x02
