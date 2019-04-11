bf12689bb8abeea65a62f2d8e998e23b4a6be4e6
chunping.fan <chunping.fan@unisoc.com>
2019-04-11 11:03:42 +0800

    Bug #956533 copy mmdvfs ko to pac
    [root cause  ] add new feature
    [changes     ] add mmdvfs ko
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:no
    [board]:ud710
    [test case]:test mmdvfs
    [reviewers   ] joseph.cai hai.li
    [change_type ] feature
    [tag_product ] common
    
    Change-Id: I74f8a72da49812bdba5ef5a756878f53ca570c9c

--- kernel_module/camera_module.mk
+++ kernel_module/camera_module.mk
@@ -1,7 +1,7 @@
 PRODUCT_PACKAGES += sprd_sensor.ko \
     sprd_camera.ko \
     sprd_fd.ko \
-    sprd_cpp.ko
-
+    sprd_cpp.ko \
+    mmdvfs.ko
 PRODUCT_COPY_FILES += \
     vendor/sprd/modules/libcamera/kernel_module/camera.rc:vendor/etc/init/camera.rc
