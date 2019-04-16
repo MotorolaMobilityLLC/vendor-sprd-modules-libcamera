4009181700ad0839cb1412476523ec0323140ca2
aaron.yu <aaron.yu@unisoc.com>
2019-04-16 16:40:35 +0800

    Bug #983064 add 4in1 front camera for L5
    [root cause  ] add 4in1 ov16885
    [changes     ] add 4in1 ov16885
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:L5
    [test case]:preview
    [reviewers   ] miller.song
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I90055b6cdb960c9007341613fc61c103827f201a

--- sensor/sensor_drv/classic/OmniVision/ov16885/parameters/param_manager.c
+++ sensor/sensor_drv/classic/OmniVision/ov16885/parameters/param_manager.c
@@ -7,17 +7,11 @@ struct sensor_raw_info *ov16885_drv_init_raw_info(int sensor_id, int vendor_id,
 
 #ifdef _SENSOR_RAW_SHARKL3_H_
 #include "parameters_sharkl3/sensor_ov16885_raw_param_main.c"
-    return &s_ov16885_mipi_raw_info;
-#else
-    if (0 == sensor_id) {
+#elif defined(SHARKL5_SENSOR_OV16885)
 #include "parameters_sharkl5/sensor_ov16885_raw_param_main.c"
-        return &s_ov16885_mipi_raw_info;
-    }
-    if (1 == sensor_id) {
+#else
 #include "parameters_roc1/sensor_ov16885_raw_param_main.c"
-        return &s_ov16885_mipi_raw_info;
-    }
 #endif
 
-    return NULL;
+return &s_ov16885_mipi_raw_info;
 }
