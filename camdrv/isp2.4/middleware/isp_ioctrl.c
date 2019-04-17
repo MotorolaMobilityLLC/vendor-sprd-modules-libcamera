fc2496cb4da11fdb2b1bd16942ec361e608173b6
Ming Cheng <ming.cheng@spreadtrum.com>
2019-04-17 13:00:57 +0800

    Bug #1050531 add interface for oem getting flash skip count
    [root cause  ] add interface
    [changes     ] camdrv
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:sp7731e_1h10
    [test case]:flash capture
    [reviewers   ] langbiao.tan, ming.tan
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Ib75568bc712f53f8b3a3583f78ec0e8aef5625ba

--- camdrv/isp2.4/middleware/isp_ioctrl.c
+++ camdrv/isp2.4/middleware/isp_ioctrl.c
@@ -2559,6 +2559,22 @@ static cmr_int ispctl_auto_hdr(cmr_handle isp_alg_handle, void *param_ptr)
         return ret;
 }
 
+static cmr_int ispctl_get_flash_skip_num(cmr_handle isp_alg_handle, void *param_ptr)
+{
+    cmr_int ret = ISP_SUCCESS;
+    struct isp_alg_fw_context *cxt = (struct isp_alg_fw_context *)isp_alg_handle;
+    cmr_u32 skip_num = 0;
+    if (NULL == param_ptr) {
+            return ISP_PARAM_NULL;
+    }
+    if (cxt->ops.ae_ops.ioctrl)
+        ret = cxt->ops.ae_ops.ioctrl(cxt->ae_cxt.handle, AE_GET_FLASH_SKIP_FRAME_NUM,
+                                                NULL, (void *)&skip_num);
+    *(cmr_u32 *)param_ptr = skip_num;
+    ISP_LOGD("skip_num %d", skip_num);
+    return ret;
+}
+
 static struct isp_io_ctrl_fun s_isp_io_ctrl_fun_tab[] = {
 	{ISP_CTRL_AE_MEASURE_LUM, ispctl_ae_measure_lum},
 	{ISP_CTRL_EV, ispctl_ev},
@@ -2637,6 +2653,7 @@ static struct isp_io_ctrl_fun s_isp_io_ctrl_fun_tab[] = {
 	{ISP_CTRL_SET_APP_MODE, ispctl_set_app_mode},
 	{ISP_CTRL_GET_GLB_GAIN, ispctl_get_glb_gain},
 	{ISP_CTRL_AUTO_HDR_MODE, ispctl_auto_hdr},
+	{ISP_CTRL_GET_FLASH_SKIP_FRAME_NUM, ispctl_get_flash_skip_num},
 	{ISP_CTRL_MAX, NULL}
 };
 
