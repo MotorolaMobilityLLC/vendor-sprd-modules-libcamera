a09b8739cbde9b0cc2f76f2af46c1756ef8c3a33
shiyun.chen <shiyun.chen@unisoc.com>
2019-05-05 10:54:28 +0800

    Bug #1063650 add for motion photo function.
    [root cause  ] add for motion photo function.
    [changes     ] modify
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:sharkl5
    [test case]:capture/preview
    [reviewers   ] kane.li
    [change_type ] feature
    [tag_product ] common
    
    Change-Id: If1902a8a679a9665007ed47a83877e0275544c50

--- hal3_2v6/SprdCamera3OEMIf.cpp
+++ hal3_2v6/SprdCamera3OEMIf.cpp
@@ -4886,8 +4886,7 @@ void SprdCamera3OEMIf::HandleTakePicture(enum camera_cb_type cb, void *parm4) {
         mSetting->setLENSTag(lensInfo);
         if (checkPreviewStateForCapture() &&
             (mTakePictureMode == SNAPSHOT_NO_ZSL_MODE ||
-             mTakePictureMode == SNAPSHOT_DEFAULT_MODE ||
-             mTakePictureMode == SNAPSHOT_ZSL_MODE)) {
+             mTakePictureMode == SNAPSHOT_DEFAULT_MODE)) {
             receiveRawPicture((struct camera_frame_type *)parm4);
         } else {
             HAL_LOGW("drop current rawPicture");
