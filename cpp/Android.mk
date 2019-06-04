5d6337bb4549425687e76664edea921235a2c5ee
Xxin Li <xxin.li@spreadtrum.com>
2019-06-04 13:59:08 +0800

    Bug #1006934 vendor: libcamera: Resolve Sharkl3 Compiling Error
    [root cause  ] Include wrong Makefile in SharkL3
    [changes     ] vendor/sprd/modules/libcamera
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:sharkl5pro
    [test case]:Cpp unit test, compile sharkl3.
    [reviewers   ] wuyi.shen
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Modify Android.mk , resolve sharkl3 compile error.
    
    Change-Id: I9645d0d58e83e8d2c02034319850edea27673b59

--- cpp/Android.mk
+++ cpp/Android.mk
@@ -1,6 +1,9 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-$(warning $(CPP_DIR))
-
-include $(LOCAL_PATH)/../$(CPP_DIR)/Android.mk
+ifeq ($(TARGET_BOARD_CAMERA_CPP_MODULAR_KERNEL), lite_r5p0)
+include $(LOCAL_PATH)/lite_r5p0/Android.mk
+endif
+ifeq ($(TARGET_BOARD_CAMERA_CPP_MODULAR_KERNEL), lite_r6p0)
+include $(LOCAL_PATH)/lite_r6p0/Android.mk
+endif
