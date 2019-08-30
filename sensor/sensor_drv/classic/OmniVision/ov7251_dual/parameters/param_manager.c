49bf2b9a74dc75a978413e60e54ca0d5d3367a33
viola.han <viola.han@spreadtrum.com>
2019-08-30 13:53:06 +0800

    Bug #1149129 add AE3.0 sensor param on sharkl5Pro AndroidQ
    [root cause  ] request AE3.0 sensor param.
    [changes     ] add sensor param.
    [side effects] none
    [self test   ]: ok
    [download normally]: ok
    [power on/off normally]: ok
    [do common repository/branch inspection]: ok
    [is there dependence]: none
    [confirm dependent commit]: ok
    [board]: ums512
    [test case]: ok
    [reviewers   ] zhappler.zhang
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I0ec71d52f7ca0c6afd908f7318122a78a3b76a8f

--- sensor/sensor_drv/classic/OmniVision/ov7251_dual/parameters/param_manager.c
+++ sensor/sensor_drv/classic/OmniVision/ov7251_dual/parameters/param_manager.c
@@ -1,14 +1,16 @@
 #include "sensor_raw.h"
 
-struct sensor_raw_info *ov7251_dual_drv_init_raw_info(int sensor_id, int vendor_id,
+struct sensor_raw_info *ov7251_drv_init_raw_info(int sensor_id, int vendor_id,
                                                  int flash_ic_type,
                                                  int producer_id) {
     cmr_int rtn = SENSOR_SUCCESS;
 
 #ifdef _SENSOR_RAW_SHARKL5_H_
-#include "parameters_sharkl5/sensor_ov7251_dual_raw_param_main.c"
+#include "parameters_sharkl5/sensor_ov7251_dual_raw_param_main.c.c"
+#elif defined(_SENSOR_RAW_SHARKL5PRO_H_)
+#include "parameters_sharkl5pro/sensor_ov7251_dual_raw_param_main.c.c"
 #else
-#include "parameters_sharkl3/sensor_ov7251_dual_raw_param_main.c"
+#include "parameters_sharkl3/sensor_ov7251_dual_raw_param_main.c.c"
 #endif
     return &s_ov7251_dual_mipi_raw_info;
 }
