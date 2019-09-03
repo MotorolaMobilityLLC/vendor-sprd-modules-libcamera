2a7673edc617ffba54d1f89e8fb93a98bd29aa63
hongxing.yang <hongxing.yang@unisoc.com>
2019-09-03 19:48:08 +0800

    Bug #1150959 - add protection before using the saved exp/gain when mode change
    [root cause  ] exp gain is 0 will cause blck frames
    [changes     ] add protection before using the saved exp/gain when mode change
    [side effects] none
    [self test   ]:pass
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:none
    [board]:9.0
    [test case]:preview and capture OK
    [reviewers   ] xiaozhe.wang
    [change_type ] AOB --> sprd_adaptation
    [tag_product ] common
    
    Change-Id: I89b0bc619147f94f27f9872827eee65d648175bc

--- ispalg/isp2.x/ae/sprd/ae_sprd_adpt.c
+++ ispalg/isp2.x/ae/sprd/ae_sprd_adpt.c
@@ -4362,7 +4362,7 @@ static cmr_s32 ae_set_video_start(struct ae_ctrl_cxt *cxt, cmr_handle * param)
 				cxt->manual_level = s_ae_manual[cxt->camera_id].manual_level;
 			}
 			else if(0 != cxt->mode_switch[last_app_mode].gain){
-				if ((cxt->mode_switch[last_app_mode].ev_index > 0) && (CAMERA_MODE_MANUAL == last_app_mode)) {
+				if ((cxt->mode_switch[last_app_mode].ev_index > 0) && (CAMERA_MODE_MANUAL == last_app_mode) && (0 != cxt->mode_switch[cxt->app_mode].gain)) {
 					ISP_LOGD("0.ev_index:%d\n", cxt->mode_switch[last_app_mode].ev_index);
 					src_exp.target_offset = cxt->mode_switch[cxt->app_mode].target_offset;
 					src_exp.exp_line = cxt->mode_switch[cxt->app_mode].exp_line;
