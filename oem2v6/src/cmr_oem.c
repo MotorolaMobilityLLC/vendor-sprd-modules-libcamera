387d9b60c6009423a0c25d845025baa4046cf729
alwin.zou <alwin.zou@unisoc.com>
2019-10-11 19:25:49 +0800

    Bug #1177503,add MODE_MULTI_CAMERA to do fb
    [root cause  ] not do fb when multiCameraMode is MODE_MULTI_CAMERA
    [changes     ] libcamera
    [side effects]no
    [self test   ]:pass
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:sharkl5pro
    [test case]:camera
    [reviewers   ] victor.xu/hai.li
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I990b14846a0dfd7c7de7c3a71120d4241790affb

--- oem2v6/src/cmr_oem.c
+++ oem2v6/src/cmr_oem.c
@@ -5210,6 +5210,7 @@ cmr_int camera_start_encode(cmr_handle oem_handle, cmr_handle caller_handle,
 
         if ((cxt->is_multi_mode == MODE_SINGLE_CAMERA ||
              cxt->is_multi_mode == MODE_SELF_SHOT ||
+             cxt->is_multi_mode == MODE_MULTI_CAMERA ||
              (cxt->is_multi_mode == MODE_BLUR &&
               cxt->blur_facebeauty_flag == 1)) &&
             (filter_type == 0)) {
