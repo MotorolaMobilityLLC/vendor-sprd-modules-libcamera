11f56ec482373921b152c84a05be8f12f62daf4f
Zero Wang <zero.wang@unisoc.com>
2019-10-21 13:38:22 +0800

    Bug #1185038 Fix coverity issue, cid 133051
    [root cause  ] Exist memory leak risk
    [changes     ] Adjust the flow to avoid memory leak
    [side effects] none
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:sp9832e
    [test case]:coverity scan
    [reviewers   ] eddy.wei
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I400d5a2ee6ec1034d201a0762ac0517763102d25
    Signed-off-by: Zero Wang <zero.wang@unisoc.com>

--- camdrv/isp2.3/middleware/isp_dev_access.c
+++ camdrv/isp2.3/middleware/isp_dev_access.c
@@ -284,6 +284,10 @@ void isp_dev_statis_info_proc(cmr_handle isp_dev_handle, void *param_ptr)
 	struct isp_statis_info *statis_info = NULL;
 	struct isp_dev_access_context *cxt = (struct isp_dev_access_context *)isp_dev_handle;
 
+	if (!cxt->isp_event_cb) {
+		ISP_LOGE("No call back function!");
+		return;
+	}
 	statis_info = malloc(sizeof(*statis_info));
 
 	statis_info->phy_addr = irq_info->phy_addr;
@@ -305,37 +309,21 @@ void isp_dev_statis_info_proc(cmr_handle isp_dev_handle, void *param_ptr)
 		 statis_info->frame_id,
 		 statis_info->sec, statis_info->usec);
 	if (irq_info->irq_property == IRQ_AEM_STATIS) {
-		if (cxt->isp_event_cb) {
-			(*cxt->isp_event_cb) (ISP_CTRL_EVT_AE, statis_info, (void *)cxt->evt_alg_handle);
-		}
+		(*cxt->isp_event_cb) (ISP_CTRL_EVT_AE, statis_info, (void *)cxt->evt_alg_handle);
 	} else if (irq_info->irq_property == IRQ_AFM_STATIS) {
-		if (cxt->isp_event_cb) {
-			(*cxt->isp_event_cb) (ISP_CTRL_EVT_AF, statis_info, (void *)cxt->evt_alg_handle);
-		}
+		(*cxt->isp_event_cb) (ISP_CTRL_EVT_AF, statis_info, (void *)cxt->evt_alg_handle);
 	} else if (irq_info->irq_property == IRQ_DCAM_PULSE) {
-		if (cxt->isp_event_cb) {
-			(*cxt->isp_event_cb) (ISP_CTRL_EVT_AF, statis_info, (void *)cxt->evt_alg_handle);
-		}
+		(*cxt->isp_event_cb) (ISP_CTRL_EVT_AF, statis_info, (void *)cxt->evt_alg_handle);
 	} else if (irq_info->irq_property == IRQ_AFL_STATIS) {
-		if (cxt->isp_event_cb) {
-			(*cxt->isp_event_cb) (ISP_PROC_AFL_DONE, statis_info, (void *)cxt->evt_alg_handle);
-		}
+		(*cxt->isp_event_cb) (ISP_PROC_AFL_DONE, statis_info, (void *)cxt->evt_alg_handle);
 	} else if (irq_info->irq_property == IRQ_BINNING_STATIS) {
-		if (cxt->isp_event_cb) {
-			(*cxt->isp_event_cb) (ISP_CTRL_EVT_BINNING, statis_info, (void *)cxt->evt_alg_handle);
-		}
+		(*cxt->isp_event_cb) (ISP_CTRL_EVT_BINNING, statis_info, (void *)cxt->evt_alg_handle);
 	} else if (irq_info->irq_property == IRQ_PDAF_STATIS) {
-		if (cxt->isp_event_cb) {
-			(*cxt->isp_event_cb) (ISP_CTRL_EVT_PDAF, statis_info, (void *)cxt->evt_alg_handle);
-		}
+		(*cxt->isp_event_cb) (ISP_CTRL_EVT_PDAF, statis_info, (void *)cxt->evt_alg_handle);
 	} else if (irq_info->irq_property == IRQ_EBD_STATIS) {
-		if (cxt->isp_event_cb) {
-			(*cxt->isp_event_cb) (ISP_CTRL_EVT_EBD, statis_info, (void *)cxt->evt_alg_handle);
-		}
+		(*cxt->isp_event_cb) (ISP_CTRL_EVT_EBD, statis_info, (void *)cxt->evt_alg_handle);
 	} else if (irq_info->irq_property == IRQ_HIST_STATIS) {
-		if (cxt->isp_event_cb) {
-			(*cxt->isp_event_cb) (ISP_PROC_HIST_DONE, statis_info, (void *)cxt->evt_alg_handle);
-		}
+		(*cxt->isp_event_cb) (ISP_PROC_HIST_DONE, statis_info, (void *)cxt->evt_alg_handle);
 	} else {
 		free((void *)statis_info);
 		statis_info = NULL;
