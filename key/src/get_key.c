cd20a842a048f0610ea5be868b0d7287696bf6ca
yushu.wang <yushu.wang@unisoc.com>
2019-12-25 09:53:30 +0800

    Bug #1166924 customer key management
    [root cause  ] modify customer key management
    [changes     ] fit for customer with different board or chip
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:all
    [test case]:open camera
    [reviewers   ] changdou.sun
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: Ib532a356422935731dcb28537fa35ce7ab8d8a1a

--- key/src/get_key.c
+++
@@ -1,9 +0,0 @@
-#include "key.h"
-int get_input_key(char *input_key)
-{
-    if(strlen(INPUT_KEY)) {
-        memcpy(input_key, INPUT_KEY, sizeof(INPUT_KEY));
-        return 0;
-    } else
-        return 1;
-}
