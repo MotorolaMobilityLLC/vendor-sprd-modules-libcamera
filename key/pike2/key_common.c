41a14095937562a5211fdd36ae086bb547c56093
yushu.wang <yushu.wang@unisoc.com>
2019-12-27 10:44:07 +0800

    Bug #1166924 refine customer key management
    [root cause  ] customer key management
    [changes     ] add annotation
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:all
    [test case]:open camera
    [reviewers   ] changdou.sun
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I1fed35e727f9a10275a31c6e6d267f67b362b073

--- key/pike2/key_common.c
+++ key/pike2/key_common.c
@@ -1,19 +1,30 @@
 #include "key_common.h"
 
-struct key input_key[] = {
-    {"HDR",      "license"},
-    {"filter",   "license"},
-    {"FaceID",   "license"},
-    {"portrait", "license"},
+/*
+ * struct key input_key is the container of the name and the
+ * license of IP feature please add the name and license of 
+ * your IP feature in the following array of struct named 
+ * input_key as the input_key_ex shows.
+ * 
+ * static struct key input_key_ex[] = {
+ *      {"faceid",    "license"},
+ *      {"HDR",       "license"},
+ *      {"warp",      "license"},
+ * }
+ */
+
+static struct key input_key[] = {
+
 };
+
+
+/*
+ * Please do not modify the code below
+ */
 
 unsigned int get_key_len()
 {
-    if(sizeof(input_key))
-        return sizeof(input_key);
-    else
-        return -1;
-    return 0;
+    return sizeof(input_key);
 }
 
 struct key *retrieve_key()
