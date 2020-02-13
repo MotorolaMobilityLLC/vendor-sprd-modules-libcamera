a408b8626d20149fbda4ccfc29f23be56841f65b
Joseph Cai <joseph.cai@unisoc.com>
2020-02-13 12:11:43 +0800

    Bug #1250294 fix coverity bug
    [root cause  ] fix coverity bug
    [changes     ] fix coverity bug
    [side effects] no
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: yes
    [board]: SharkL5Pro
    [test case]: multi-camera
    [reviewers   ] victor.xu
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I78a269e3e46b3dbe36c1c3eafa698bf56aa86e67
    (cherry picked from commit 5da48ef294bb750eadc70be5ab1198416d3b9148)

--- hal_common/multiCamera/SprdCamera3MultiCamera.cpp
+++ hal_common/multiCamera/SprdCamera3MultiCamera.cpp
@@ -3,13 +3,16 @@
 #include <SprdCamera3Factory.h>
 #include "SprdCamera3MultiCamera.h"
 
+ANDROID_SINGLETON_STATIC_INSTANCE(sprdcamera::SprdCamera3MultiCamera);
+
 namespace sprdcamera {
 
 int SprdCamera3MultiCamera::get_camera_info(int camera_id,
-        struct camera_info *info)
-{
-    if (GetInstance()->mAuthorized) {
-        return GetInstance()->mFunc1(camera_id, info);
+                                            struct camera_info *info) {
+    auto &inst = SprdCamera3MultiCamera::getInstance();
+
+    if (inst.mAuthorized) {
+        return inst.mFuncGetCameraInfo(camera_id, info);
     } else {
         ALOGW("Fallback to id \"0\"");
         return SprdCamera3Factory::get_camera_info(0, info);
@@ -17,63 +20,53 @@ int SprdCamera3MultiCamera::get_camera_info(int camera_id,
 }
 
 int SprdCamera3MultiCamera::camera_device_open(const struct hw_module_t *module,
-        const char *id, struct hw_device_t **hw_device)
-{
-    if (GetInstance()->mAuthorized) {
-        return GetInstance()->mFunc2(module, id, hw_device);
+                                               const char *id,
+                                               struct hw_device_t **hw_device) {
+    auto &inst = SprdCamera3MultiCamera::getInstance();
+
+    if (inst.mAuthorized) {
+        return inst.mFuncOpen(module, id, hw_device);
     } else {
         ALOGW("Fallback to id \"0\"");
         return SprdCamera3Factory::mModuleMethods.open(module, "0", hw_device);
     }
 }
 
-SprdCamera3MultiCamera *SprdCamera3MultiCamera::sInstance = NULL;
-
-SprdCamera3MultiCamera *SprdCamera3MultiCamera::GetInstance() {
-    if (!sInstance) {
-        void *lib = dlopen("/vendor/lib/libmulticam.so", RTLD_LAZY);
-        if (!lib) {
-            ALOGE("Fail to load library: %s", dlerror());
-            sInstance = new SprdCamera3MultiCamera();
-            return sInstance;
-        }
-
-        FuncType1 f1 = (FuncType1)dlsym(lib, "get_camera_info");
-        if (!f1) {
-            ALOGE("Fail to load symbol: %s", dlerror());
-            sInstance = new SprdCamera3MultiCamera();
-            return sInstance;
-        }
-
-        FuncType2 f2 = (FuncType2)dlsym(lib, "camera_device_open");
-        if (!f2) {
-            ALOGE("Fail to load symbol: %s", dlerror());
-            sInstance = new SprdCamera3MultiCamera();
-            return sInstance;
-        }
-
-        sInstance = new SprdCamera3MultiCamera(lib, f1, f2);
+SprdCamera3MultiCamera::SprdCamera3MultiCamera()
+    : mAuthorized(false), mHandle(NULL), mFuncGetCameraInfo(NULL),
+      mFuncOpen(NULL) {
+    mHandle = dlopen("/vendor/lib/libmulticam.so", RTLD_LAZY);
+    if (!mHandle) {
+        ALOGE("Fail to load library: %s", dlerror());
+        goto exit;
     }
 
-ret:
-    return sInstance;
-}
+    mFuncGetCameraInfo =
+        (FuncType_GetCameraInfo)dlsym(mHandle, "get_camera_info");
+    if (!mFuncGetCameraInfo) {
+        ALOGE("Fail to load symbol: %s", dlerror());
+        dlclose(mHandle);
+        goto exit;
+    }
 
-SprdCamera3MultiCamera::SprdCamera3MultiCamera()
-    : mAuthorized(false), mHandle(NULL), mFunc1(NULL), mFunc2(NULL)
-{
-    ALOGI("Instantiate wrapper unauthorized!");
-}
+    mFuncOpen = (FuncType_Open)dlsym(mHandle, "camera_device_open");
+    if (!mFuncOpen) {
+        ALOGE("Fail to load symbol: %s", dlerror());
+        dlclose(mHandle);
+        goto exit;
+    }
 
-SprdCamera3MultiCamera::SprdCamera3MultiCamera(HandleType h, FuncType1 f1, FuncType2 f2)
-    : mAuthorized(true), mHandle(h), mFunc1(f1), mFunc2(f2)
-{
-    ALOGI("Instantiate wrapper");
+    mAuthorized = true;
+
+exit:
+    ALOGI("Instantiate wrapper %s!",
+          mAuthorized ? "authorized" : "unauthorized");
 }
 
 SprdCamera3MultiCamera::~SprdCamera3MultiCamera() {
-    dlclose(mHandle);
-}
+    if (mAuthorized)
+        dlclose(mHandle);
 
+    ALOGI("destroy wrapper");
 }
-
+}
