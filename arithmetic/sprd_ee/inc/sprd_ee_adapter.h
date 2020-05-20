4e2dcadb5ddcbf34bedede661b9dba5a55de1d08
jeff.wang <jeff.wang@unisoc.com>
2020-05-20 14:19:23 +0800

    Bug #1232179 ee intergration & tuning param adjust & param check protection
    [root cause  ] ee intergration & debug
    [changes     ] integration & adjust param & check
    [side effects] no
    [self test   ]:
    [download normally]:no
    [power on/off normally]:no
    [do common repository/branch inspection]:no
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:SharkL5 Pro
    [test case]:FDR
    [reviewers   ]
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I86f5bc45a89f9c0e57a40042bbda0022d605ef16

---
+++ arithmetic/sprd_ee/inc/sprd_ee_adapter.h
@@ -0,0 +1,48 @@
+#ifndef __SPRD_YUV_DENOISE_ADAPTER_HEADER_H__
+#define __SPRD_YUV_DENOISE_ADAPTER_HEADER_H__
+
+#include "sprd_camalg_adapter.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define JNIEXPORT  __attribute__ ((visibility ("default")))
+
+	typedef enum {
+		SPRD_EE_OPEN_CMD = 0,
+		SPRD_EE_CLOSE_CMD,
+		SPRD_EE_VERSION_CMD,
+		SPRD_EE_PROCESS_CMD,
+		SPRD_EE_MAX_CMD
+	} sprd_ee_cmd_t;
+
+	typedef struct {
+		void *ctx;
+		struct sprd_camalg_image imgIn;		/*input image buffer*/
+		struct sprd_camalg_image imgOut;	/*output image buffer*/
+		void *tuningParam;			/*tuning params*/
+		int tuningSize;				/*the size of tuningParam*/
+		int mode_idx;				/*mode index*/
+		int scene_idx;				/*scene index*/
+		int level_idx[2];			/*level index array*/
+		int level_weight[2];			/*level weight array*/
+		int level_num;				/*level number*/
+		void *scene_map_buffer;			/*scene map buffer*/
+		int width;				/*image width*/
+		int height;				/*image height*/
+		int crop_width;				/*image crop width*/
+		int crop_height;			/*image crop height*/
+	} sprd_ee_param_t;
+
+	/*
+	ee adapter cmd process interface
+	return value: 0 is ok, other value is failed
+	*/
+	JNIEXPORT int sprd_ee_adpt_ctrl(sprd_ee_cmd_t cmd, void *param);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
