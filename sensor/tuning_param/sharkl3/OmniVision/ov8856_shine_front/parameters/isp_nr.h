4270801f6a459518ca16ff768008a228cf7929c2
zoro.cai <Zoro.Cai@unisoc.com>
2020-08-07 20:11:33 +0800

    Bug #1384265 update ynrs block
    [root cause  ] there is no ynrs block
    [changes     ] add ynrs block for nr
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:9863
    [test case]:ok
    [reviewers   ] jerry.mao
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Icf831f6c0c8a5bd5624a094806b76ff290c5a700

--- sensor/tuning_param/sharkl3/OmniVision/ov8856_shine_front/parameters/isp_nr.h
+++ sensor/tuning_param/sharkl3/OmniVision/ov8856_shine_front/parameters/isp_nr.h
@@ -186,6 +186,12 @@ static struct sensor_nr_scene_map_param s_ov8856_front_nr_scene_map_param = {{
 #include "NR/video_2/normal/cnr_param.h"
 #endif
 
+#ifdef _NR_YNRS_PARAM_
+#include "NR/common/normal/ynrs_param.h"
+#include "NR/prv_0/normal/ynrs_param.h"
+#include "NR/prv_1/normal/ynrs_param.h"
+#include "NR/cap_1/normal/ynrs_param.h"
+#endif
 
 
 
