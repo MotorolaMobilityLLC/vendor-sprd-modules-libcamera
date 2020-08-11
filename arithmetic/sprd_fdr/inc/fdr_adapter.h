58429e956cd879dcf2c2da741f8200ba2b8a2f8d
xiaowei.liu <xiaowei.liu@unisoc.com>
2020-08-11 11:21:59 +0800

    Bug #1386357 fdr stage2 intergration
    [root cause  ] fdr stage2 dev
    [changes     ] fdr
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:l5pro
    [test case]:fdr
    [reviewers   ] shimin.he
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I7157a0aa81dd84a00b4433721d74a2f2fcd06716
    (cherry picked from commit b0216c1c538b3987d3edff8511f2ccdb1762a7a2)
    (cherry picked from commit c302f391b5c126d2250474c0e047e82bca46d5b7)

--- arithmetic/sprd_fdr/inc/fdr_adapter.h
+++ arithmetic/sprd_fdr/inc/fdr_adapter.h
@@ -17,6 +17,10 @@ typedef enum {
     FDR_CMD_ALIGN,
     FDR_CMD_MERGE,
     FDR_CMD_FUSION,
+    FDR_CMD_GET_VERSION,
+    FDR_CMD_GET_EXIF,
+    FDR_CMD_GET_FRAMENUM,
+    FDR_CMD_GET_MAX_FRAMENUM,
 } FDR_CMD;
 
 typedef struct {
@@ -43,6 +47,7 @@ typedef struct {
     void **ctx;
     sprd_camalg_image_t *image_array;
     fdr_merge_param *merge_param;
+    void *ae_param;
     fdr_merge_out_param *merge_out_param;
 } FDR_CMD_MERGE_PARAM_T;
 
@@ -51,7 +56,29 @@ typedef struct {
     sprd_camalg_image_t *image_in_array;
     sprd_camalg_image_t *image_out;
     expfusion_proc_param_t *param_proc;
+    void *ae_param;
 } FDR_CMD_FUSION_PARAM_T;
+
+typedef struct {
+    fdr_lib_version *lib_version;
+} FDR_CMD_GET_VERSION_PARAM_T;
+
+typedef struct {
+    void **ctx;
+    fdr_exif_t *exif_info;
+} FDR_CMD_GET_EXIF_PARAM_T;
+
+typedef struct {
+    fdr_calc_frame_param_in_t *param_in;
+    fdr_calc_frame_param_out_t *param_out;
+    fdr_calc_frame_status_t *calc_status;
+} FDR_CMD_GET_FRAMENUM_PARAM_T;
+
+typedef struct {
+    int *max_total_frame_num;
+    int *max_ref_frame_num;
+} FDR_CMD_GET_MAX_FRAMENUM_PARAM_T;
+
 
 #ifdef __cplusplus
 extern "C"
