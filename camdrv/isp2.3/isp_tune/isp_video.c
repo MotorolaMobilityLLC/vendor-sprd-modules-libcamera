67b3b2e4d33a0d7ac49986453a02ec5fa682bfce
yuanyuan.cai <yuanyuan.cai@unisoc.com>
2020-08-27 12:26:50 +0800

    Bug #1398893 Solve Coverity code scanning
    [root cause  ] Pointers are not initialized
    [changes     ] vendor/sprd/modules/libcamera
    [side effects] No
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: No
    [confirm dependent commit]: yes
    [board]: 9832e
    [test case]: Coverity Code Scan
    [reviewers   ] guifang.tang,jiaojiao.he,victor.xu
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I335f83f67a3c4f1068705201c4d21e95d4cda9d7

--- camdrv/isp2.3/isp_tune/isp_video.c
+++ camdrv/isp2.3/isp_tune/isp_video.c
@@ -1399,6 +1399,7 @@ cmr_s32 isp_dump_reg(cmr_handle handler, cmr_u8 * tx_buf, cmr_u32 len)
 	cmr_u32 num, i;
 	cmr_u32 tail_len;
 
+	memset(&param_info, 0, sizeof(struct isp_info));
 	if (NULL == tx_buf || NULL == handler) {
 		ISP_LOGE("fail to check param");
 		return ISP_PARAM_ERROR;
@@ -3326,7 +3327,7 @@ static cmr_s32 handle_isp_data(cmr_u8 * buf, cmr_u32 len)
 	case CMD_SFT_GET_POS:
 		{
 			ISP_LOGV("get pos");
-			cmr_u32 pos;
+			cmr_u32 pos = 0;
 			ret = isp_ioctl(isp_handler, ISP_CTRL_GET_AF_POS, &pos);	// set af info after auto focus
 			if (!ret) {
 				rsp_len += ispvideo_SetreTurnValue((cmr_u8 *) & eng_rsp_diag[rsp_len], ISP_CMD_SUCCESS);
@@ -4151,6 +4152,7 @@ static cmr_s32 handle_isp_data(cmr_u8 * buf, cmr_u32 len)
 	case CMD_WRTIE_SCENE_PARAM:
 		{
 			struct isptool_scene_param scene_info;
+			memset(&scene_info, 0, sizeof(struct isptool_scene_param));
 			ret = ispvideo_GetSceneInfo(buf, &scene_info);
 
 			//send response packet
@@ -4293,9 +4295,9 @@ void send_img_data(cmr_u32 format, cmr_u32 width, cmr_u32 height, char *imgptr, 
 		pthread_mutex_lock(&ispstream_lock);
 
 		char *img_ptr = (char *)preview_buf_ptr;
-		cmr_s32 img_len;
-		cmr_u32 img_w;
-		cmr_u32 img_h;
+		cmr_s32 img_len = 0;
+		cmr_u32 img_w = 0;
+		cmr_u32 img_h= 0;
 		/*if preview size more than vga that is subsample to less than vga for preview frame ratio */
 		ispvideo_Scale(format, width, height, imgptr, imagelen, &img_w, &img_h, &img_ptr, &img_len);
 
