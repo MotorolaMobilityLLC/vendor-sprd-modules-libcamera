3be8a1728e17742fe460bb4cca2ef95cad3da216
weiss.li <weiss.li@unisoc.com>
2020-09-16 20:36:13 +0800

    Bug #1407245 sharkl5 back master camera vcm bringup
    [root cause  ] sharkl5 back master camera vcm bringup
    [changes     ] add af driver dw9714v make file
    [side effects]NA
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yse
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:ums312_1h10
    [test case]:preview af, touch af, bokeh calibration
    [reviewers   ] hai.li
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] specific
    
    Change-Id: I3b4e8ddd270ec43ac593f9d9db5f22a527722521

--- sensor/af_drv/vcm_lib_cfg.mk
+++ sensor/af_drv/vcm_lib_cfg.mk
@@ -1,4 +1,5 @@
 PRODUCT_PACKAGES += libvcm_dw9714p \
+                    libvcm_dw9714v \
                     libvcm_lc898213 \
                     libvcm_dw9768v \
                     libvcm_dw9800 \
