40155a57ba69300febf78921e8ca6fa17eeb1a5b
jie.liu <jie.liu@unisoc.com>
2020-10-12 11:09:39 +0800

    Bug #1425438 [Camera Formats]测试时，Camera 1 :320*240页面下方有阴影
    [root cause  ] there is not preview and capture  1/4 bining LNC parameters
    [changes     ] add preview and capture 1/4 bining parameters and calibration LNC
    [side effects] none
    [self test   ]:pre ok
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:none
    [confirm dependent commit]:none
    [board]:sc9832e
    [test case]:3/3 pass
    [reviewers   ] jerry.mao
    [change_type ] feature_bugfix
    [tag_product ] specific
    
    Change-Id: If5719f06103034ac847737a03551e32cfd9e1eda

--- sensor/tuning_param/sharkle/OmniVision/ov5675_front_main/parameters/sensor_ov5675_raw_param_main.c
+++ sensor/tuning_param/sharkle/OmniVision/ov5675_front_main/parameters/sensor_ov5675_raw_param_main.c
@@ -31,10 +31,13 @@
 #include "sensor_ov5675_raw_param_common.c"
 #include "sensor_ov5675_raw_param_prv_0.c"
 #include "sensor_ov5675_raw_param_prv_1.c"
+#include "sensor_ov5675_raw_param_prv_2.c"
 #include "sensor_ov5675_raw_param_cap_0.c"
 #include "sensor_ov5675_raw_param_cap_1.c"
+#include "sensor_ov5675_raw_param_cap_2.c"
 #include "sensor_ov5675_raw_param_video_0.c"
 #include "sensor_ov5675_raw_param_video_1.c"
+#include "sensor_ov5675_raw_param_video_3.c"
 
 /* End Include */
 
@@ -48,7 +51,7 @@
 
 
 /* Capture Sizes:
-	1296x972,2592x1944
+	1296x972,2592x1944,648x486
 */
 
 
@@ -156,16 +159,16 @@ static struct sensor_raw_info s_ov5675_mipi_raw_info=
 		{s_ov5675_tune_info_common, sizeof(s_ov5675_tune_info_common)},
 		{s_ov5675_tune_info_prv_0, sizeof(s_ov5675_tune_info_prv_0)},
 		{s_ov5675_tune_info_prv_1, sizeof(s_ov5675_tune_info_prv_1)},
-		{NULL, 0},
+		{s_ov5675_tune_info_prv_2, sizeof(s_ov5675_tune_info_prv_2)},
 		{NULL, 0},
 		{s_ov5675_tune_info_cap_0, sizeof(s_ov5675_tune_info_cap_0)},
 		{s_ov5675_tune_info_cap_1, sizeof(s_ov5675_tune_info_cap_1)},
-		{NULL, 0},
+		{s_ov5675_tune_info_cap_2, sizeof(s_ov5675_tune_info_cap_2)},
 		{NULL, 0},
 		{s_ov5675_tune_info_video_0, sizeof(s_ov5675_tune_info_video_0)},
 		{s_ov5675_tune_info_video_1, sizeof(s_ov5675_tune_info_video_1)},
 		{NULL, 0},
-		{NULL, 0},
+		{s_ov5675_tune_info_video_3, sizeof(s_ov5675_tune_info_video_3)},
 	},
 	&s_ov5675_trim_info,
 	&s_ov5675_ioctrl,
@@ -174,31 +177,31 @@ static struct sensor_raw_info s_ov5675_mipi_raw_info=
 		&s_ov5675_fix_info_common,
 		&s_ov5675_fix_info_prv_0,
 		&s_ov5675_fix_info_prv_1,
-		NULL,
+		&s_ov5675_fix_info_prv_2,
 		NULL,
 		&s_ov5675_fix_info_cap_0,
 		&s_ov5675_fix_info_cap_1,
-		NULL,
+		&s_ov5675_fix_info_cap_2,
 		NULL,
 		&s_ov5675_fix_info_video_0,
 		&s_ov5675_fix_info_video_1,
 		NULL,
-		NULL,
+		&s_ov5675_fix_info_video_3,
 	},
 	{
 		{s_ov5675_common_tool_ui_input, sizeof(s_ov5675_common_tool_ui_input)},
 		{s_ov5675_prv_0_tool_ui_input, sizeof(s_ov5675_prv_0_tool_ui_input)},
 		{s_ov5675_prv_1_tool_ui_input, sizeof(s_ov5675_prv_1_tool_ui_input)},
-		{NULL, 0},
+		{s_ov5675_prv_2_tool_ui_input, sizeof(s_ov5675_prv_2_tool_ui_input)},
 		{NULL, 0},
 		{s_ov5675_cap_0_tool_ui_input, sizeof(s_ov5675_cap_0_tool_ui_input)},
 		{s_ov5675_cap_1_tool_ui_input, sizeof(s_ov5675_cap_1_tool_ui_input)},
-		{NULL, 0},
+		{s_ov5675_cap_2_tool_ui_input, sizeof(s_ov5675_cap_2_tool_ui_input)},
 		{NULL, 0},
 		{s_ov5675_video_0_tool_ui_input, sizeof(s_ov5675_video_0_tool_ui_input)},
 		{s_ov5675_video_1_tool_ui_input, sizeof(s_ov5675_video_1_tool_ui_input)},
 		{NULL, 0},
-		{NULL, 0},
+		{s_ov5675_video_3_tool_ui_input, sizeof(s_ov5675_video_3_tool_ui_input)},
 	},
 	{
 		&s_ov5675_nr_scene_map_param,
