95ff7702a16088ba602f9b958c1ff7e1a6e19e4a
minnie.li <minnie.li@unisoc.com>
2020-11-27 15:51:44 +0800

    Bug #1464826 correct the parameter of the function
    [root cause  ] mismatch of the formal parameter and transfer parameter
    [changes     ] correct the transfer parameter
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:all boards
    [test case]:camera
    [reviewers   ] hongcheng.li
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I45c1570be1af17a4342f54fb8dbcfb818ce7c6ea

--- oem2v4/src/cmr_preview.c
+++ oem2v4/src/cmr_preview.c
@@ -7904,25 +7904,7 @@ cmr_int prev_set_prev_param(struct prev_handle *handle, cmr_u32 camera_id,
     chn_param.frm_num = -1;
     chn_param.skip_num = sensor_info->mipi_cap_skip_num;
 
-    /* in slowmotion, we want do decimation in preview channel,
-    bug video buffer and preview buffer is in one request, so we cant
-    do decimation for now, otherwise the fps is low */
-
     chn_param.cap_inf_cfg.chn_deci_factor = 0;
-
-#ifdef SPRD_SLOWMOTION_OPTIMIZE
-    if (prev_cxt->prev_param.video_eb &&
-        prev_cxt->prev_param.video_slowmotion_eb) {
-        if (prev_cxt->prev_param.video_slowmotion_eb == 4) {
-            chn_param.cap_inf_cfg.chn_deci_factor = 3;
-            prev_cxt->prev_skip_num = 2;
-        } else if (prev_cxt->prev_param.video_slowmotion_eb == 3) {
-            chn_param.cap_inf_cfg.chn_deci_factor = 2;
-            prev_cxt->prev_skip_num = 2;
-        }
-    }
-#endif
-
     chn_param.cap_inf_cfg.frm_num = -1;
     chn_param.cap_inf_cfg.buffer_cfg_isp = 0;
     chn_param.cap_inf_cfg.cfg.need_binning = 0;
@@ -8003,7 +7985,8 @@ cmr_int prev_set_prev_param(struct prev_handle *handle, cmr_u32 camera_id,
     prev_cxt->prev_rect.height = chn_param.cap_inf_cfg.cfg.src_img_rect.height;
 
     /*get sensor interface info*/
-    ret = prev_get_sn_inf(handle, camera_id, chn_param.skip_num,
+    ret = prev_get_sn_inf(handle, camera_id,
+                          chn_param.cap_inf_cfg.chn_deci_factor,
                           &chn_param.sn_if);
     if (ret) {
         CMR_LOGE("get sn inf failed");
@@ -8021,6 +8004,22 @@ cmr_int prev_set_prev_param(struct prev_handle *handle, cmr_u32 camera_id,
         ret = CMR_CAMERA_FAIL;
         goto exit;
     }
+
+    /* in slowmotion, we want do decimation in preview channel,
+    bug video buffer and preview buffer is in one request, so we cant
+    do decimation for now, otherwise the fps is low */
+#ifdef SPRD_SLOWMOTION_OPTIMIZE
+        if (prev_cxt->prev_param.video_eb &&
+            prev_cxt->prev_param.video_slowmotion_eb) {
+            if (prev_cxt->prev_param.video_slowmotion_eb == 4) {
+                chn_param.cap_inf_cfg.chn_deci_factor = 3;
+                prev_cxt->prev_skip_num = 2;
+            } else if (prev_cxt->prev_param.video_slowmotion_eb == 3) {
+                chn_param.cap_inf_cfg.chn_deci_factor = 2;
+                prev_cxt->prev_skip_num = 2;
+            }
+        }
+#endif
 
     chn_param.cap_inf_cfg.cfg.flip_on = 0;
 
@@ -8405,7 +8404,8 @@ cmr_int prev_set_video_param(struct prev_handle *handle, cmr_u32 camera_id,
     prev_cxt->video_rect.height = chn_param.cap_inf_cfg.cfg.src_img_rect.height;
 
     /*get sensor interface info*/
-    ret = prev_get_sn_inf(handle, camera_id, chn_param.skip_num,
+    ret = prev_get_sn_inf(handle, camera_id,
+                          chn_param.cap_inf_cfg.chn_deci_factor,
                           &chn_param.sn_if);
     if (ret) {
         CMR_LOGE("get sn inf failed");
@@ -9152,7 +9152,8 @@ cmr_int prev_set_cap_param_raw(struct prev_handle *handle, cmr_u32 camera_id,
     }
 
     /*get sensor interface info*/
-    ret = prev_get_sn_inf(handle, camera_id, chn_param.skip_num,
+    ret = prev_get_sn_inf(handle, camera_id,
+                          chn_param.cap_inf_cfg.chn_deci_factor,
                           &chn_param.sn_if);
     if (ret) {
         CMR_LOGE("get sn inf failed");
@@ -9368,7 +9369,8 @@ cmr_int prev_set_dp_cap_param(struct prev_handle *handle, cmr_u32 camera_id,
     }
 
     /*get sensor interface info*/
-    ret = prev_get_sn_inf(handle, camera_id, chn_param.skip_num,
+    ret = prev_get_sn_inf(handle, camera_id,
+                          chn_param.cap_inf_cfg.chn_deci_factor,
                           &chn_param.sn_if);
     if (ret) {
         CMR_LOGE("get sn inf failed");
