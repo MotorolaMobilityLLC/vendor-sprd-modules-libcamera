8e563a366a4ab680c4413ed60e804e6c10a726fe
yushu.wang <yushu.wang@unisoc.com>
2020-12-24 14:29:04 +0800

    Bug #1478849 refine sensor driver to support FDR capture
    [root cause  ] 14bit is not configured for FDR on qogirl6
    [changes     ] add 14bit raw configuration
    [side effects]NA
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:all
    [test case]:camera
    [reviewers   ] dave.lee
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I3f4a37cc2d0cb8c465f8c947211f8693c9ed6550

--- sensor/sensor_drv/classic/OmniVision/ov8856_back/sensor_ov8856_back_mipi_raw_2lane.h
+++ sensor/sensor_drv/classic/OmniVision/ov8856_back/sensor_ov8856_back_mipi_raw_2lane.h
@@ -689,7 +689,7 @@ static struct sensor_module_info s_ov8856_module_info_tab[VENDOR_NUM] = {
                              .type = SENSOR_INTERFACE_TYPE_CSI2,
                              .bus_width = LANE_NUM,
                              .pixel_width = RAW_BITS,
-#ifdef _SENSOR_RAW_SHARKL5PRO_H_
+#if defined  _SENSOR_RAW_SHARKL5PRO_H_ || defined _SENSOR_RAW_SHARKL6_H
                              .is_loose = 2,
 #else
                              .is_loose = 0,
--- sensor/sensor_drv/classic/Sony/imx616/sensor_imx616_mipi_raw.h
+++ sensor/sensor_drv/classic/Sony/imx616/sensor_imx616_mipi_raw.h
@@ -943,11 +943,11 @@ static struct sensor_module_info s_imx616_module_info_tab[VENDOR_NUM] = {
                              .type = SENSOR_INTERFACE_TYPE_CSI2,
                              .bus_width = LANE_NUM,
                              .pixel_width = RAW_BITS,
-                             #ifdef _SENSOR_RAW_SHARKL5PRO_H_,
+#if defined  _SENSOR_RAW_SHARKL5PRO_H_ || defined _SENSOR_RAW_SHARKL6_H
                              .is_loose = 2,
-                             #else
+#else
                              .is_loose = 0,
-                             #endif
+#endif
                          },
                      .change_setting_skip_num = 1,
                      .horizontal_view_angle = 65,
