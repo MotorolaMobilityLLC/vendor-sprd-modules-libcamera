953c0a91619d1679f0462f87dac600de7a562ac5
xiaoxing.liang <xiaoxing.liang@unisoc.com>
2020-12-28 16:42:55 +0800

    Bug #1489002 commit imx586 tuning param
    [root cause  ] imx58 tuning param commit
    [changes     ] auto tuning param
    [side effects]none
    [self test   ]:pre ok
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:9230
    [test case]:pre ok
    [reviewers   ] jerry.mao
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Ifd059c75dfa21539eb1221e02b267fc0fbbce3e1

---
+++ sensor/tuning_param/qogirl6/Sony/imx586/parameters/isp_nr.h
@@ -0,0 +1,116 @@
+#ifdef _NR_MAP_PARAM_
+static struct sensor_nr_level_map_param s_imx586_nr_level_number_map_param = {{
+	218,249,154,127,243,47,0,8,168,18,47,22,32,167,47,22,
+	32,168,47,22,32,169,47,22,32,170,47,22,32,171,47,22
+}};
+
+static struct sensor_nr_level_map_param s_imx586_default_nr_level_map_param = {{
+	32,172,47,22,32,173,47,22,32,174,47,22,32,175,47,22,
+	32,176,47,22,32,177,47,22,32,178,47,22,32,179,47,22
+}};
+
+static struct sensor_nr_scene_map_param s_imx586_nr_scene_map_param = {{
+	0x00000001,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,
+	0x19191919,0x19191919,0x19191919,0x19191919,0x19191919,0x19191919,0x19191919,0x19191919,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000
+}};
+#endif
+
+#ifdef _NR_BAYER_NR_PARAM_
+#include "NR/common/normal/bayer_nr_param.h"
+#endif
+
+#ifdef _NR_VST_PARAM_
+#include "NR/common/normal/vst_param.h"
+#endif
+
+#ifdef _NR_IVST_PARAM_
+#include "NR/common/normal/ivst_param.h"
+#endif
+
+#ifdef _NR_RGB_DITHER_PARAM_
+#include "NR/common/normal/rgb_dither_param.h"
+#endif
+
+#ifdef _NR_BPC_PARAM_
+#include "NR/common/normal/bpc_param.h"
+#endif
+
+#ifdef _NR_CFAI_PARAM_
+#include "NR/common/normal/cfai_param.h"
+#endif
+
+#ifdef _NR_CCE_UVDIV_PARAM_
+#include "NR/common/normal/cce_uvdiv_param.h"
+#endif
+
+#ifdef _NR_YNR_PARAM_
+#include "NR/common/normal/ynr_param.h"
+#endif
+
+#ifdef _NR_EE_PARAM_
+#include "NR/common/normal/ee_param.h"
+#endif
+
+#ifdef _NR_3DNR_PARAM_
+#include "NR/common/normal/3dnr_param.h"
+#endif
+
+#ifdef _NR_PPE_PARAM_
+#include "NR/common/normal/ppe_param.h"
+#endif
+
+#ifdef _NR_YUV_NOISEFILTER_PARAM_
+#include "NR/common/normal/yuv_noisefilter_param.h"
+#endif
+
+#ifdef _NR_RGB_AFM_PARAM_
+#include "NR/common/normal/rgb_afm_param.h"
+#endif
+
+#ifdef _NR_IIRCNR_PARAM_
+#include "NR/common/normal/iircnr_param.h"
+#endif
+
+#ifdef _NR_YUV_PRECDN_PARAM_
+#include "NR/common/normal/yuv_precdn_param.h"
+#endif
+
+#ifdef _NR_UV_CDN_PARAM_
+#include "NR/common/normal/uv_cdn_param.h"
+#endif
+
+#ifdef _NR_UV_POSTCDN_PARAM_
+#include "NR/common/normal/uv_postcdn_param.h"
+#endif
+
+#ifdef _NR_CNR_PARAM_
+#include "NR/common/normal/cnr_param.h"
+#endif
+
+#ifdef _NR_IMBALANCE_PARAM_
+#include "NR/common/normal/imbalance_param.h"
+#endif
+
+#ifdef _NR_SW3DNR_PARAM_
+#include "NR/common/normal/sw3dnr_param.h"
+#endif
+
+#ifdef _NR_BWU_BWD_PARAM_
+#include "NR/common/normal/bwud_param.h"
+#endif
+
+#ifdef _NR_YNRS_PARAM_
+#include "NR/common/normal/ynrs_param.h"
+#endif
+
+#ifdef _NR_CNR3_PARAM_
+#include "NR/common/normal/cnr3_param.h"
+#endif
+
+#ifdef _NR_MFNR_PARAM_
+#include "NR/common/normal/mfnr_param.h"
+#endif
+
+#ifdef _NR_POST_EE_PARAM_
+#include "NR/common/normal/post_ee_param.h"
+#endif
