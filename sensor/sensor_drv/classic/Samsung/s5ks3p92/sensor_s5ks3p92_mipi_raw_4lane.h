74db7fd22a35ccecaf29f07cec949a3a6b76c496
minnie.li <minnie.li@unisoc.com>
2021-01-01 13:52:36 +0800

    Bug #1492351 fix fov problem on SharkL5 Pro camera id 1
    [root cause  ] after configuring the standard setup, camera id 1 test failed
    [changes     ] update the focal length value corresponding with the measured fov
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:ums512
    [test case]: fov test
    [reviewers   ]
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I263a07195e4bee3b1296e3070efedb314178ae99

--- sensor/sensor_drv/classic/Samsung/s5ks3p92/sensor_s5ks3p92_mipi_raw_4lane.h
+++ sensor/sensor_drv/classic/Samsung/s5ks3p92/sensor_s5ks3p92_mipi_raw_4lane.h
@@ -2204,7 +2204,7 @@ static SENSOR_STATIC_INFO_T s_s5ks3p92_static_info[VENDOR_NUM] = {
                      .exp_valid_frame_num = 1,
                      .clamp_level = 64,
                      .adgain_valid_frame_num = 1,
-                     .fov_info = {{4.614f, 3.444f}, 3.856f}}}
+                     .fov_info = {{4.614f, 3.444f}, 3.708f}}}
     /*If there are multiple modules,please add here*/
 };
 
