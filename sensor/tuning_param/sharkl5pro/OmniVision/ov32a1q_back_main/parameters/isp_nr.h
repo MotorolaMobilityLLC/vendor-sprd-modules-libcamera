d4b154551a8392983b93beb864ece54e6faad0cc
Luke.Li <Luke.Li@unisoc.com>
2021-01-22 19:57:53 +0800

    Bug #1511272 add hdr bokeh nr param for sharkl5pro 0122
    [root cause  ] lack of hdr bokeh nr param
    [changes     ] add hdr bokeh nr param
    [side effects] none
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:sharkl5pro
    [test case]:preview and capture
    [reviewers   ] jerry.mao
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I19b2f7943a58b181f2cedb9f4fefeadc78f9b858

--- sensor/tuning_param/sharkl5pro/OmniVision/ov32a1q_back_main/parameters/isp_nr.h
+++ sensor/tuning_param/sharkl5pro/OmniVision/ov32a1q_back_main/parameters/isp_nr.h
@@ -10,7 +10,7 @@ static struct sensor_nr_level_map_param s_ov32a1q_default_nr_level_map_param = {
 }};
 
 static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
-	0x00000003,0x00000000,0x00000001,0x00000000,0x00000000,0x00000001,0x00000003,0x00000000,
+	0x000000C3,0x00000000,0x00000001,0x00000000,0x00000000,0x00000001,0x00000003,0x00000000,
 	0x00000000,0x00000000,0x00000000,0x00000001,0x00000000,0x00000001,0x00000000,0x00000000
 }};
 #endif
@@ -18,6 +18,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_BAYER_NR_PARAM_
 #include "NR/common/normal/bayer_nr_param.h"
 #include "NR/common/night/bayer_nr_param.h"
+#include "NR/common/hdr/bayer_nr_param.h"
+#include "NR/common/bokeh/bayer_nr_param.h"
 #include "NR/prv_1/normal/bayer_nr_param.h"
 #include "NR/cap_0/normal/bayer_nr_param.h"
 #include "NR/cap_1/normal/bayer_nr_param.h"
@@ -29,6 +31,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_VST_PARAM_
 #include "NR/common/normal/vst_param.h"
 #include "NR/common/night/vst_param.h"
+#include "NR/common/hdr/vst_param.h"
+#include "NR/common/bokeh/vst_param.h"
 #include "NR/prv_1/normal/vst_param.h"
 #include "NR/cap_0/normal/vst_param.h"
 #include "NR/cap_1/normal/vst_param.h"
@@ -40,6 +44,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_IVST_PARAM_
 #include "NR/common/normal/ivst_param.h"
 #include "NR/common/night/ivst_param.h"
+#include "NR/common/hdr/ivst_param.h"
+#include "NR/common/bokeh/ivst_param.h"
 #include "NR/prv_1/normal/ivst_param.h"
 #include "NR/cap_0/normal/ivst_param.h"
 #include "NR/cap_1/normal/ivst_param.h"
@@ -51,6 +57,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_RGB_DITHER_PARAM_
 #include "NR/common/normal/rgb_dither_param.h"
 #include "NR/common/night/rgb_dither_param.h"
+#include "NR/common/hdr/rgb_dither_param.h"
+#include "NR/common/bokeh/rgb_dither_param.h"
 #include "NR/prv_1/normal/rgb_dither_param.h"
 #include "NR/cap_0/normal/rgb_dither_param.h"
 #include "NR/cap_1/normal/rgb_dither_param.h"
@@ -62,6 +70,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_BPC_PARAM_
 #include "NR/common/normal/bpc_param.h"
 #include "NR/common/night/bpc_param.h"
+#include "NR/common/hdr/bpc_param.h"
+#include "NR/common/bokeh/bpc_param.h"
 #include "NR/prv_1/normal/bpc_param.h"
 #include "NR/cap_0/normal/bpc_param.h"
 #include "NR/cap_1/normal/bpc_param.h"
@@ -73,6 +83,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_GRGB_PARAM_
 #include "NR/common/normal/grgb_param.h"
 #include "NR/common/night/grgb_param.h"
+#include "NR/common/hdr/grgb_param.h"
+#include "NR/common/bokeh/grgb_param.h"
 #include "NR/prv_1/normal/grgb_param.h"
 #include "NR/cap_0/normal/grgb_param.h"
 #include "NR/cap_1/normal/grgb_param.h"
@@ -84,6 +96,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_CFAI_PARAM_
 #include "NR/common/normal/cfai_param.h"
 #include "NR/common/night/cfai_param.h"
+#include "NR/common/hdr/cfai_param.h"
+#include "NR/common/bokeh/cfai_param.h"
 #include "NR/prv_1/normal/cfai_param.h"
 #include "NR/cap_0/normal/cfai_param.h"
 #include "NR/cap_1/normal/cfai_param.h"
@@ -95,6 +109,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_CCE_UVDIV_PARAM_
 #include "NR/common/normal/cce_uvdiv_param.h"
 #include "NR/common/night/cce_uvdiv_param.h"
+#include "NR/common/hdr/cce_uvdiv_param.h"
+#include "NR/common/bokeh/cce_uvdiv_param.h"
 #include "NR/prv_1/normal/cce_uvdiv_param.h"
 #include "NR/cap_0/normal/cce_uvdiv_param.h"
 #include "NR/cap_1/normal/cce_uvdiv_param.h"
@@ -106,6 +122,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_YNR_PARAM_
 #include "NR/common/normal/ynr_param.h"
 #include "NR/common/night/ynr_param.h"
+#include "NR/common/hdr/ynr_param.h"
+#include "NR/common/bokeh/ynr_param.h"
 #include "NR/prv_1/normal/ynr_param.h"
 #include "NR/cap_0/normal/ynr_param.h"
 #include "NR/cap_1/normal/ynr_param.h"
@@ -117,6 +135,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_EE_PARAM_
 #include "NR/common/normal/ee_param.h"
 #include "NR/common/night/ee_param.h"
+#include "NR/common/hdr/ee_param.h"
+#include "NR/common/bokeh/ee_param.h"
 #include "NR/prv_1/normal/ee_param.h"
 #include "NR/cap_0/normal/ee_param.h"
 #include "NR/cap_1/normal/ee_param.h"
@@ -128,6 +148,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_3DNR_PARAM_
 #include "NR/common/normal/3dnr_param.h"
 #include "NR/common/night/3dnr_param.h"
+#include "NR/common/hdr/3dnr_param.h"
+#include "NR/common/bokeh/3dnr_param.h"
 #include "NR/prv_1/normal/3dnr_param.h"
 #include "NR/cap_0/normal/3dnr_param.h"
 #include "NR/cap_1/normal/3dnr_param.h"
@@ -139,6 +161,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_PPE_PARAM_
 #include "NR/common/normal/ppe_param.h"
 #include "NR/common/night/ppe_param.h"
+#include "NR/common/hdr/ppe_param.h"
+#include "NR/common/bokeh/ppe_param.h"
 #include "NR/prv_1/normal/ppe_param.h"
 #include "NR/cap_0/normal/ppe_param.h"
 #include "NR/cap_1/normal/ppe_param.h"
@@ -150,6 +174,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_YUV_NOISEFILTER_PARAM_
 #include "NR/common/normal/yuv_noisefilter_param.h"
 #include "NR/common/night/yuv_noisefilter_param.h"
+#include "NR/common/hdr/yuv_noisefilter_param.h"
+#include "NR/common/bokeh/yuv_noisefilter_param.h"
 #include "NR/prv_1/normal/yuv_noisefilter_param.h"
 #include "NR/cap_0/normal/yuv_noisefilter_param.h"
 #include "NR/cap_1/normal/yuv_noisefilter_param.h"
@@ -161,6 +187,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_RGB_AFM_PARAM_
 #include "NR/common/normal/rgb_afm_param.h"
 #include "NR/common/night/rgb_afm_param.h"
+#include "NR/common/hdr/rgb_afm_param.h"
+#include "NR/common/bokeh/rgb_afm_param.h"
 #include "NR/prv_1/normal/rgb_afm_param.h"
 #include "NR/cap_0/normal/rgb_afm_param.h"
 #include "NR/cap_1/normal/rgb_afm_param.h"
@@ -172,6 +200,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_IIRCNR_PARAM_
 #include "NR/common/normal/iircnr_param.h"
 #include "NR/common/night/iircnr_param.h"
+#include "NR/common/hdr/iircnr_param.h"
+#include "NR/common/bokeh/iircnr_param.h"
 #include "NR/prv_1/normal/iircnr_param.h"
 #include "NR/cap_0/normal/iircnr_param.h"
 #include "NR/cap_1/normal/iircnr_param.h"
@@ -183,6 +213,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_YUV_PRECDN_PARAM_
 #include "NR/common/normal/yuv_precdn_param.h"
 #include "NR/common/night/yuv_precdn_param.h"
+#include "NR/common/hdr/yuv_precdn_param.h"
+#include "NR/common/bokeh/yuv_precdn_param.h"
 #include "NR/prv_1/normal/yuv_precdn_param.h"
 #include "NR/cap_0/normal/yuv_precdn_param.h"
 #include "NR/cap_1/normal/yuv_precdn_param.h"
@@ -194,6 +226,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_UV_CDN_PARAM_
 #include "NR/common/normal/uv_cdn_param.h"
 #include "NR/common/night/uv_cdn_param.h"
+#include "NR/common/hdr/uv_cdn_param.h"
+#include "NR/common/bokeh/uv_cdn_param.h"
 #include "NR/prv_1/normal/uv_cdn_param.h"
 #include "NR/cap_0/normal/uv_cdn_param.h"
 #include "NR/cap_1/normal/uv_cdn_param.h"
@@ -205,6 +239,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_UV_POSTCDN_PARAM_
 #include "NR/common/normal/uv_postcdn_param.h"
 #include "NR/common/night/uv_postcdn_param.h"
+#include "NR/common/hdr/uv_postcdn_param.h"
+#include "NR/common/bokeh/uv_postcdn_param.h"
 #include "NR/prv_1/normal/uv_postcdn_param.h"
 #include "NR/cap_0/normal/uv_postcdn_param.h"
 #include "NR/cap_1/normal/uv_postcdn_param.h"
@@ -216,6 +252,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_CNR_PARAM_
 #include "NR/common/normal/cnr_param.h"
 #include "NR/common/night/cnr_param.h"
+#include "NR/common/hdr/cnr_param.h"
+#include "NR/common/bokeh/cnr_param.h"
 #include "NR/prv_1/normal/cnr_param.h"
 #include "NR/cap_0/normal/cnr_param.h"
 #include "NR/cap_1/normal/cnr_param.h"
@@ -227,6 +265,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_IMBALANCE_PARAM_
 #include "NR/common/normal/imbalance_param.h"
 #include "NR/common/night/imbalance_param.h"
+#include "NR/common/hdr/imbalance_param.h"
+#include "NR/common/bokeh/imbalance_param.h"
 #include "NR/prv_1/normal/imbalance_param.h"
 #include "NR/cap_0/normal/imbalance_param.h"
 #include "NR/cap_1/normal/imbalance_param.h"
@@ -238,6 +278,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_SW3DNR_PARAM_
 #include "NR/common/normal/sw3dnr_param.h"
 #include "NR/common/night/sw3dnr_param.h"
+#include "NR/common/hdr/sw3dnr_param.h"
+#include "NR/common/bokeh/sw3dnr_param.h"
 #include "NR/prv_1/normal/sw3dnr_param.h"
 #include "NR/cap_0/normal/sw3dnr_param.h"
 #include "NR/cap_1/normal/sw3dnr_param.h"
@@ -249,6 +291,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_BWU_BWD_PARAM_
 #include "NR/common/normal/bwud_param.h"
 #include "NR/common/night/bwud_param.h"
+#include "NR/common/hdr/bwud_param.h"
+#include "NR/common/bokeh/bwud_param.h"
 #include "NR/prv_1/normal/bwud_param.h"
 #include "NR/cap_0/normal/bwud_param.h"
 #include "NR/cap_1/normal/bwud_param.h"
@@ -260,6 +304,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_YNRS_PARAM_
 #include "NR/common/normal/ynrs_param.h"
 #include "NR/common/night/ynrs_param.h"
+#include "NR/common/hdr/ynrs_param.h"
+#include "NR/common/bokeh/ynrs_param.h"
 #include "NR/prv_1/normal/ynrs_param.h"
 #include "NR/cap_0/normal/ynrs_param.h"
 #include "NR/cap_1/normal/ynrs_param.h"
@@ -271,6 +317,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_CNR3_PARAM_
 #include "NR/common/normal/cnr3_param.h"
 #include "NR/common/night/cnr3_param.h"
+#include "NR/common/hdr/cnr3_param.h"
+#include "NR/common/bokeh/cnr3_param.h"
 #include "NR/prv_1/normal/cnr3_param.h"
 #include "NR/cap_0/normal/cnr3_param.h"
 #include "NR/cap_1/normal/cnr3_param.h"
@@ -282,6 +330,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_MFNR_PARAM_
 #include "NR/common/normal/mfnr_param.h"
 #include "NR/common/night/mfnr_param.h"
+#include "NR/common/hdr/mfnr_param.h"
+#include "NR/common/bokeh/mfnr_param.h"
 #include "NR/prv_1/normal/mfnr_param.h"
 #include "NR/cap_0/normal/mfnr_param.h"
 #include "NR/cap_1/normal/mfnr_param.h"
@@ -293,6 +343,8 @@ static struct sensor_nr_scene_map_param s_ov32a1q_nr_scene_map_param = {{
 #ifdef _NR_POST_EE_PARAM_
 #include "NR/common/normal/post_ee_param.h"
 #include "NR/common/night/post_ee_param.h"
+#include "NR/common/hdr/post_ee_param.h"
+#include "NR/common/bokeh/post_ee_param.h"
 #include "NR/prv_1/normal/post_ee_param.h"
 #include "NR/cap_0/normal/post_ee_param.h"
 #include "NR/cap_1/normal/post_ee_param.h"
