20fe8bc0f2d4093a69117447eb3d6483fc5d1a31
taotao.luo <taotao.luo@unisoc.com>
2021-02-09 18:49:22 +0800

    Bug #1526511 HDR tuning parameter control
    [root cause  ] hdr develop
    [changes     ] new add
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:sharkl5 pro
    [test case]:sprd_easy_hdr
    [reviewers   ]xiaowei.liu shimin.he
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I04c8a76d3031312087486b238c56d1361d193921

--- arithmetic/sprd_easy_hdr/src/sprd_hdr_adapter.cpp
+++ arithmetic/sprd_easy_hdr/src/sprd_hdr_adapter.cpp
@@ -20,15 +20,25 @@ static enum camalg_run_type g_run_type = SPRD_CAMALG_RUN_TYPE_CPU;
 
 void *sprd_hdr_adpt_init(int max_width, int max_height, void *param)
 {
-	void *handle = 0;
+    void *handle = 0;
 #ifdef CONFIG_SPRD_HDR_LIB_VERSION_2
-	hdr_config_t cfg;
-	sprd_hdr_config_default(&cfg);
-	cfg.max_width = max_width;
-	cfg.max_height = max_height;
-	cfg.img_width = max_width;
-	cfg.img_height = max_height;
-	cfg.img_stride = max_width;
+    hdr_config_t cfg;
+    sprd_hdr_config_default(&cfg);
+    HDR_LOGI("param: %p\n", param);
+    if(param != 0) {
+        sprd_hdr_init_param_t *adapt_init_param = (sprd_hdr_init_param_t *)param;
+        cfg.tuning_param_size = adapt_init_param->tuning_param_size;
+        cfg.tuning_param = adapt_init_param->tuning_param;
+        cfg.malloc = adapt_init_param->malloc;
+        cfg.free = adapt_init_param->free;
+        HDR_LOGI("tuning_param: %p ,tuning_param_size: %d, malloc: %p, free:%p \n",cfg.tuning_param,cfg.tuning_param_size, cfg.malloc, cfg.free);
+    }
+
+    cfg.max_width = max_width;
+    cfg.max_height = max_height;
+    cfg.img_width = max_width;
+    cfg.img_height = max_height;
+    cfg.img_stride = max_width;
 
     char strRunType[256];
     if (g_run_type == SPRD_CAMALG_RUN_TYPE_VDSP && !sprd_caa_vdsp_check_supported())
@@ -42,133 +52,135 @@ void *sprd_hdr_adpt_init(int max_width, int max_height, void *param)
 
     HDR_LOGI("current run type: %d\n", g_run_type);
 
-	if (g_run_type == SPRD_CAMALG_RUN_TYPE_CPU)
-		sprd_hdr_open(&handle, &cfg);
-	else if (g_run_type == SPRD_CAMALG_RUN_TYPE_VDSP)
-		sprd_hdr_vdsp_open(&handle, &cfg);
+    if (g_run_type == SPRD_CAMALG_RUN_TYPE_CPU)
+        sprd_hdr_open(&handle, &cfg);
+    else if (g_run_type == SPRD_CAMALG_RUN_TYPE_VDSP)
+        sprd_hdr_vdsp_open(&handle, &cfg);
 #endif
 
 #ifdef CONFIG_SPRD_HDR_LIB
-	if (sprd_hdr_pool_init())
-		return 0;
-	handle = (void *)-1;
+     if (sprd_hdr_pool_init())
+        return 0;
+    handle = (void *)-1;
 #endif
-	return handle;
+    return handle;
 }
 
 int sprd_hdr_adpt_deinit(void *handle)
 {
-	int ret = 0;
+    int ret = 0;
 #ifdef CONFIG_SPRD_HDR_LIB_VERSION_2
-	if (g_run_type == SPRD_CAMALG_RUN_TYPE_CPU)
-		ret = sprd_hdr_close(handle);
-	else if (g_run_type == SPRD_CAMALG_RUN_TYPE_VDSP)
-		ret = sprd_hdr_vdsp_close(handle);
+    if (g_run_type == SPRD_CAMALG_RUN_TYPE_CPU)
+        ret = sprd_hdr_close(handle);
+    else if (g_run_type == SPRD_CAMALG_RUN_TYPE_VDSP)
+        ret = sprd_hdr_vdsp_close(handle);
 #endif
 
 #ifdef CONFIG_SPRD_HDR_LIB
-	ret = sprd_hdr_pool_destroy();
+    ret = sprd_hdr_pool_destroy();
 #endif
-	return ret;
+    return ret;
 }
 
 int sprd_hdr_adpt_ctrl(void *handle, sprd_hdr_cmd_t cmd, void *param)
 {
-	int ret = 0;
-	switch (cmd) {
-	case SPRD_HDR_GET_VERSION_CMD: {
+    int ret = 0;
+    switch (cmd) {
+    case SPRD_HDR_GET_VERSION_CMD: {
 #ifdef CONFIG_SPRD_HDR_LIB_VERSION_2
-		hdr_version_t *version = (hdr_version_t *)param;
-		ret = sprd_hdr_version(version);
+    hdr_version_t *version = (hdr_version_t *)param;
+    ret = sprd_hdr_version(version);
 #endif
-		break;
-	}
-	case SPRD_HDR_PROCESS_CMD: {
-		sprd_hdr_param_t *hdr_param = (sprd_hdr_param_t *)param;
-		int i = 0;
+    break;
+    }
+    case SPRD_HDR_PROCESS_CMD: {
+        sprd_hdr_param_t *hdr_param = (sprd_hdr_param_t *)param;
+        int i = 0;
 #ifdef CONFIG_SPRD_HDR_LIB_VERSION_2
-		ldr_image_t input[2];
-		if (g_run_type == SPRD_CAMALG_RUN_TYPE_CPU) {
-			for (i = 0; i < 2; i++) {
-				input[i].data = (uint8_t *)hdr_param->input[i].addr[0];
-				input[i].width = hdr_param->input[i].width;
-				input[i].height = hdr_param->input[i].height;
-				input[i].stride = hdr_param->input[i].stride;
-				input[i].ev = hdr_param->ev[i];
-			}
+        ldr_image_t input[2];
+        if (g_run_type == SPRD_CAMALG_RUN_TYPE_CPU) {
+        for (i = 0; i < 2; i++) {
+            input[i].data = (uint8_t *)hdr_param->input[i].addr[0];
+            input[i].width = hdr_param->input[i].width;
+            input[i].height = hdr_param->input[i].height;
+            input[i].stride = hdr_param->input[i].stride;
+            input[i].ev = hdr_param->ev[i];
+            input[i].tuning_param_index = hdr_param->tuning_param_index;
+        }
 
-			uint8_t *output = (uint8_t *)hdr_param->output.addr[0];
+        uint8_t *output = (uint8_t *)hdr_param->output.addr[0];
 
-			ret = sprd_hdr_process(handle, input, output);
-		} else if (g_run_type == SPRD_CAMALG_RUN_TYPE_VDSP) {
-			ldr_image_vdsp_t input_vdsp[2], output_vdsp;
-			for (i = 0; i < 2; i++) {
-				input_vdsp[i].image.data = (uint8_t *)hdr_param->input[i].addr[0];
-				input_vdsp[i].image.width = hdr_param->input[i].width;
-				input_vdsp[i].image.height = hdr_param->input[i].height;
-				input_vdsp[i].image.stride = hdr_param->input[i].stride;
-				input_vdsp[i].image.ev = hdr_param->ev[i];
-				input_vdsp[i].fd = hdr_param->input[i].ion_fd;
-			}
+        ret = sprd_hdr_process(handle, input, output);
+        } else if (g_run_type == SPRD_CAMALG_RUN_TYPE_VDSP) {
+        ldr_image_vdsp_t input_vdsp[2], output_vdsp;
+        for (i = 0; i < 2; i++) {
+            input_vdsp[i].image.data = (uint8_t *)hdr_param->input[i].addr[0];
+            input_vdsp[i].image.width = hdr_param->input[i].width;
+            input_vdsp[i].image.height = hdr_param->input[i].height;
+            input_vdsp[i].image.stride = hdr_param->input[i].stride;
+            input_vdsp[i].image.ev = hdr_param->ev[i];
+            input_vdsp[i].image.tuning_param_index = hdr_param->tuning_param_index;
+            input_vdsp[i].fd = hdr_param->input[i].ion_fd;
+        }
 
-			output_vdsp.image.data = (uint8_t *)hdr_param->output.addr[0];
-			output_vdsp.image.width = hdr_param->output.width;
-			output_vdsp.image.height = hdr_param->output.height;
-			output_vdsp.image.stride = hdr_param->output.stride;
-			output_vdsp.fd = hdr_param->output.ion_fd;
+        output_vdsp.image.data = (uint8_t *)hdr_param->output.addr[0];
+        output_vdsp.image.width = hdr_param->output.width;
+        output_vdsp.image.height = hdr_param->output.height;
+        output_vdsp.image.stride = hdr_param->output.stride;
+        output_vdsp.fd = hdr_param->output.ion_fd;
 
-			ret = sprd_hdr_vdsp_process(handle, input_vdsp, &output_vdsp);
-		}
+        ret = sprd_hdr_vdsp_process(handle, input_vdsp, &output_vdsp);
+       }
 #endif
 
 #ifdef CONFIG_SPRD_HDR_LIB
-		BYTE *Y0 = (BYTE *)hdr_param->input[0].addr[0];
-		BYTE *Y1 = (BYTE *)hdr_param->input[1].addr[0];
-		BYTE *Y2 = (BYTE *)hdr_param->input[2].addr[0];
-		BYTE *output = (BYTE *)hdr_param->output.addr[0];
-		int height = hdr_param->input[0].height;
-		int width = hdr_param->input[0].width;
-		ret = HDR_Function(Y0, Y1, Y2, output, height, width, "YVU420_SEMIPLANAR");
+        BYTE *Y0 = (BYTE *)hdr_param->input[0].addr[0];
+        BYTE *Y1 = (BYTE *)hdr_param->input[1].addr[0];
+        BYTE *Y2 = (BYTE *)hdr_param->input[2].addr[0];
+        BYTE *output = (BYTE *)hdr_param->output.addr[0];
+        int height = hdr_param->input[0].height;
+        int width = hdr_param->input[0].width;
+        ret = HDR_Function(Y0, Y1, Y2, output, height, width, "YVU420_SEMIPLANAR");
 #endif
-		break;
-	}
-	case SPRD_HDR_FAST_STOP_CMD: {
+        break;
+    }
+    case SPRD_HDR_FAST_STOP_CMD: {
 #ifdef CONFIG_SPRD_HDR_LIB_VERSION_2
-		ret = sprd_hdr_fast_stop(handle);
+        ret = sprd_hdr_fast_stop(handle);
 #endif
 
 #ifdef CONFIG_SPRD_HDR_LIB
-		sprd_hdr_set_stop_flag(HDR_STOP);
+        sprd_hdr_set_stop_flag(HDR_STOP);
 #endif
-		break;
-	}
-	default:
-		HDR_LOGW("unknown cmd: %d\n", cmd);
-		break;
-	}
-	return ret;
+        break;
+    }
+    default:
+        HDR_LOGW("unknown cmd: %d\n", cmd);
+        break;
+    }
+    return ret;
 }
 
 int sprd_hdr_get_devicetype(enum camalg_run_type *type)
 {
-	if (!type)
-		return 1;
+    if (!type)
+        return 1;
 #ifdef CONFIG_SPRD_HDR_LIB_VERSION_2
-	*type = g_run_type;
+    *type = g_run_type;
 #endif
 
 #ifdef CONFIG_SPRD_HDR_LIB
-	*type = SPRD_CAMALG_RUN_TYPE_CPU;
+    *type = SPRD_CAMALG_RUN_TYPE_CPU;
 #endif
 
-	return 0;
+    return 0;
 }
 
 int sprd_hdr_set_devicetype(enum camalg_run_type type)
 {
-	if (type < SPRD_CAMALG_RUN_TYPE_CPU || type >= SPRD_CAMALG_RUN_TYPE_MAX)
-		return 1;
-	g_run_type = type;
+    if (type < SPRD_CAMALG_RUN_TYPE_CPU || type >= SPRD_CAMALG_RUN_TYPE_MAX)
+        return 1;
+    g_run_type = type;
 
-	return 0;
+    return 0;
 }
--- ispalg/ae/sprd/ae3.x/hdr/inc/sprd_hdr_api.h
+++ ispalg/ae/sprd/ae3.x/hdr/inc/sprd_hdr_api.h
@@ -1,7 +1,8 @@
 #ifndef _SPRD_HDR_API_H_
 #define _SPRD_HDR_API_H_
 
-#include "stdint.h"
+#include <stdint.h>
+#include <stddef.h>
 
 #ifdef __cplusplus
 extern   "C"
@@ -11,134 +12,145 @@ extern   "C"
 /*! version information */
 typedef struct _tag_hdr_version_t
 {
-	uint8_t		major;              /*!< API major version */
-	uint8_t		minor;              /*!< API minor version */
-	uint8_t		micro;              /*!< API micro version */
-	uint8_t		nano;               /*!< API nano version */
-	char		built_date[0x20];   /*!< API built date */
-	char		built_time[0x20];   /*!< API built time */
-	char		built_rev[0x100];	/*!< API built version, linked with vcs resivion> */
+    uint8_t    major;              /*!< API major version */
+    uint8_t    minor;              /*!< API minor version */
+    uint8_t    micro;              /*!< API micro version */
+    uint8_t    nano;               /*!< API nano version */
+    char    built_date[0x20];    /*!< API built date */
+    char    built_time[0x20];    /*!< API built time */
+    char    built_rev[0x100];    /*!< API built version, linked with vcs resivion> */
 } hdr_version_t;
 
 /*! hdr statistic parameters */
 typedef struct _tag_hdr_stat_t
-{   
-	uint32_t*   hist256;            /*!< histogram buffer pointer */
-	uint8_t*    img;                /*!< gray image buffer pointer */
-	int         w;                  /*!< image width */
-	int         h;                  /*!< image height */
-	int         s;                  /*!< image buffer stride */
+{
+    uint32_t*   hist256;            /*!< histogram buffer pointer */
+    uint8_t*    img;                /*!< gray image buffer pointer */
+    int         w;                  /*!< image width */
+    int         h;                  /*!< image height */
+    int         s;                  /*!< image buffer stride */
 } hdr_stat_t;
 
 /*! hdr_detect parameters */
 typedef struct _tag_hdr_detect_t
 {
-	uint8_t     thres_dark;         //[0, 40]    
-	uint8_t     thres_bright;       //[180, 255]          
+    uint8_t     thres_dark;         //[0, 40]
+    uint8_t     thres_bright;       //[180, 255]
+    int         bv;
+    uint8_t     face_num;
+    uint16_t    abl_weight;
+    uint32_t    iso;
+    void*       tuning_param;
+    int         tuning_param_size;
+    int         tuning_param_index;
 } hdr_detect_t;
 
 /*! LDR image data structure */
 typedef struct _tag_ldr_image_t
 {
-	uint8_t*    data;
-	int         width;
-	int         height;
-	int         stride;
-	float       ev;
+    uint8_t*    data;
+    int         width;
+    int         height;
+    int         stride;
+    float       ev;
+    int         tuning_param_index;
 } ldr_image_t;
 
 typedef struct
 {
-	ldr_image_t image;
-	int			fd;
+    ldr_image_t image;
+    int    fd;
 } ldr_image_vdsp_t;
 
 /*! mf_hdr align parameters */
 typedef struct _tag_hdr_align_t
 {
-	uint8_t     align_layer;        //[6, 8]
-	uint8_t     align_noiseT;       //[4,20]
-	float       align_thres[2];     //[0.3, 0.7]
+    uint8_t     align_layer;        //[6, 8]
+    uint8_t     align_noiseT;       //[4,20]
+    float       align_thres[2];     //[0.3, 0.7]
 } hdr_align_t;
 
 /*! mf_hdr fusion parameters */
-typedef struct 
+typedef struct
 {
-	uint8_t     deghost_en;         //[0,  1]
-	uint8_t     layer_minus;        //[0,  4]
-	uint8_t     thres_oe;           //[0,255]
-	uint8_t     thres_mv;           //[0,255]
-	uint8_t     radius_e;           //[1,  5]
-	uint16_t    dist_thres;         //[50,255]
-	uint16_t    mv_num_thres;       //[0, 300]
-	uint8_t     mv_border;          //[0, 50]
-	uint8_t     mv_dilate_thres;    //[0,255]
-	uint8_t     thres_oe2;			//[0, 255]
-	uint8_t     w_yoe_m;            //[0,255]
-	float       w_yoe_s;            //[0.0, 1.0]
-	uint8_t     w_uv_m;             //[0,255]
-	float       w_uv_s;             //[0.0, 1.0]
-	float       sat_strength;       //[1.0, 5.0]
+    uint8_t     deghost_en;         //[0,  1]
+    uint8_t     layer_minus;        //[0,  4]
+    uint8_t     thres_oe;           //[0,255]
+    uint8_t     thres_mv;           //[0,255]
+    uint8_t     radius_e;           //[1,  5]
+    uint16_t    dist_thres;         //[50,255]
+    uint16_t    mv_num_thres;       //[0, 300]
+    uint8_t     mv_border;          //[0, 50]
+    uint8_t     mv_dilate_thres;    //[0,255]
+    uint8_t     thres_oe2;          //[0, 255]
+    uint8_t     w_yoe_m;            //[0,255]
+    float       w_yoe_s;            //[0.0, 1.0]
+    uint8_t     w_uv_m;             //[0,255]
+    float       w_uv_s;             //[0.0, 1.0]
+    float       sat_strength;       //[1.0, 5.0]
 } hdr_fusion_t;
 
 /*! mf_hdr ltm parameters */
 typedef struct _tag_hdr_ltm_t
 {
-	uint8_t		contrast_en;		 //[0,  1]
-	uint8_t		strength;            //[1,  7] 
-	uint8_t		texture_counter_en;  //[0,  1]
-	float		text_point_alpha;    //[0,  1]
-	uint8_t		text_point_thres;    //[1, 31]
-	uint8_t		text_prop_thres;     //[1, 31]
+    uint8_t    contrast_en;        //[0,  1]
+    uint8_t    strength;           //[1,  7]
+    uint8_t    texture_counter_en; //[0,  1]
+    float      text_point_alpha;   //[0,  1]
+    uint8_t    text_point_thres;   //[1, 31]
+    uint8_t    text_prop_thres;    //[1, 31]
 } hdr_ltm_t;
 
 typedef struct _tag_hdr_config_t
-{   
-	int			max_width;
-	int			max_height;
-    
-	int			img_width;
-	int			img_height;
-	int			img_stride;
-	int			img_num;
-	char*		core_str;
-
-	uint8_t		detect_bypass;  //[0,  1]
-	uint8_t		align_bypass;   //[0, 1]
-
-	hdr_detect_t	scene_param;
-	hdr_align_t		align_param;
-	hdr_fusion_t	fusion_param;
-	hdr_ltm_t		hdr_ltm_param;
+{
+    int    max_width;
+    int    max_height;
+    int    img_width;
+    int    img_height;
+    int    img_stride;
+    int    img_num;
+    char*  core_str;
+    uint8_t    detect_bypass;  //[0,  1]
+    uint8_t    align_bypass;   //[0, 1]
+    hdr_detect_t    scene_param;
+    hdr_align_t    align_param;
+    hdr_fusion_t    fusion_param;
+    hdr_ltm_t    hdr_ltm_param;
+    void*    tuning_param;
+    int      tuning_param_size;
+    void*    (*malloc)(size_t size, char* type);
+    void     (*free)(void* addr);
 } hdr_config_t;
 
 typedef struct {
-	void *dl_handle;
-	int (*sprd_caa_vdsp_open)(void **h_vdsp);
-	int (*sprd_caa_vdsp_close)(void *h_vdsp);
-	int (*sprd_caa_vdsp_send)(void *h_vdsp, const char *nsid, int priority, void **h_ionmem_list, uint32_t h_ionmem_num);
-	int (*sprd_caa_cadence_vdsp_load_library)(void *h_vdsp, const char *nsid);
-	int (*sprd_caa_vdsp_Send)(const char *nsid, int priority, void **h_ionmem_list, uint32_t h_ionmem_num);
-	void *(*sprd_caa_ionmem_alloc)(uint32_t size, bool iscache);
-	int (*sprd_caa_ionmem_free)(void *h_ionmem);
-	void *(*sprd_caa_ionmem_get_vaddr)(void *h_ionmem);
-	int (*sprd_caa_ionmem_get_fd)(void *h_ionmem);
-	void (*ProcessState_initWithDriver)(const char *driver);
-	void (*ProcessState_startThreadPool)();
-	void (*IPCThreadState_joinThreadPool)(bool isMain);
-	void (*IPCThreadState_stopProcess)(bool immediate);
+    void *dl_handle;
+    int (*sprd_caa_vdsp_open)(void **h_vdsp);
+    int (*sprd_caa_vdsp_close)(void *h_vdsp);
+    int (*sprd_caa_vdsp_send)(void *h_vdsp, const char *nsid, int priority, void **h_ionmem_list, uint32_t h_ionmem_num);
+    int (*sprd_caa_cadence_vdsp_load_library)(void *h_vdsp, const char *nsid);
+    int (*sprd_caa_vdsp_Send)(const char *nsid, int priority, void **h_ionmem_list, uint32_t h_ionmem_num);
+    int (*sprd_caa_vdsp_maxfreq_lock)(void *h_vdsp);
+    int (*sprd_caa_vdsp_maxfreq_unlock)(void *h_vdsp);
+    void *(*sprd_caa_ionmem_alloc)(uint32_t size, bool iscache);
+    int (*sprd_caa_ionmem_free)(void *h_ionmem);
+    void *(*sprd_caa_ionmem_get_vaddr)(void *h_ionmem);
+    int (*sprd_caa_ionmem_get_fd)(void *h_ionmem);
+    void (*ProcessState_initWithDriver)(const char *driver);
+    void (*ProcessState_startThreadPool)();
+    void (*IPCThreadState_joinThreadPool)(bool isMain);
+    void (*IPCThreadState_stopProcess)(bool immediate);
 } camalg_assist_lib_api_t;
 
 typedef void * hdr_inst_t;
 
 #ifdef _USRDLL
-	#ifdef SPRD_ISP_EXPORTS
-		#define SPRD_ISP_API    __declspec(dllexport)
-	#else
-		#define SPRD_ISP_API    __declspec(dllimport)
-	#endif
+    #ifdef SPRD_ISP_EXPORTS
+        #define SPRD_ISP_API    __declspec(dllexport)
+    #else
+        #define SPRD_ISP_API    __declspec(dllimport)
+    #endif
 #else
-	#define SPRD_ISP_API
+    #define SPRD_ISP_API
 #endif
 
 SPRD_ISP_API int sprd_hdr_scndet_multi_inst(hdr_detect_t* scndet, hdr_stat_t* stat, float* ev, int *p_smooth_flag, int *p_frameID);
@@ -165,4 +177,3 @@ SPRD_ISP_API int sprd_hdr_load_api(camalg_assist_lib_api_t *libapi);
 #endif
 
 #endif
-
