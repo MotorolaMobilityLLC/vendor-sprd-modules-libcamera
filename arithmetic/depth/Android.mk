55128ce62370cf386e5dd8ec641de047add9bcfb
xin.guo <xin.guo@unisoc.com>
2021-02-16 15:40:45 +0800

    Bug #1524716 depth6.5.0.1 and adapter
    [root cause  ] depth adapter
    [changes     ] depth adapter
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:sharkl3
    [test case]:bokeh
    [reviewers   ]
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Iad380aaa06c2f4f4f2bf91f102f34900fedaf759

--- arithmetic/depth/Android.mk
+++ arithmetic/depth/Android.mk
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+ifeq ($(strip $(TARGET_BOARD_BOKEH_MODE_SUPPORT)),true)
 ifneq ($(PLATFORM_VERSION),4.4.4)
-#ifeq ($(strip $(TARGET_BOARD_BOKEH_MODE_SUPPORT)),true)
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_ARCH), $(filter $(TARGET_ARCH), arm arm64))
@@ -22,6 +22,39 @@ LIB_PATH := lib
 else ifeq ($(TARGET_ARCH), $(filter $(TARGET_ARCH), x86 x86_64))
 LIB_PATH := x86_lib
 endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libceres_online
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := both
+LOCAL_MODULE_STEM_32 := $(LOCAL_MODULE).so
+LOCAL_MODULE_STEM_64 := $(LOCAL_MODULE).so
+LOCAL_SRC_FILES_32 := $(LIB_PATH)/libceres_online.so
+LOCAL_SRC_FILES_64 := $(LIB_PATH)64/libceres_online.so
+LOCAL_CHECK_ELF_FILES := false
+ifeq (1, $(strip $(shell expr $(ANDROID_MAJOR_VER) \>= 8)))
+LOCAL_PROPRIETARY_MODULE := true
+endif
+
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libDualCam_OnliCalib
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := both
+LOCAL_MODULE_STEM_32 := $(LOCAL_MODULE).so
+LOCAL_MODULE_STEM_64 := $(LOCAL_MODULE).so
+LOCAL_SRC_FILES_32 := $(LIB_PATH)/libDualCam_OnliCalib.so
+LOCAL_SRC_FILES_64 := $(LIB_PATH)64/libDualCam_OnliCalib.so
+LOCAL_SHARED_LIBRARIES := libceres_online
+LOCAL_CHECK_ELF_FILES := false
+ifeq (1, $(strip $(shell expr $(ANDROID_MAJOR_VER) \>= 8)))
+LOCAL_PROPRIETARY_MODULE := true
+endif
+
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libsprddepth
@@ -32,11 +65,46 @@ LOCAL_MODULE_STEM_32 := $(LOCAL_MODULE).so
 LOCAL_MODULE_STEM_64 := $(LOCAL_MODULE).so
 LOCAL_SRC_FILES_32 := $(LIB_PATH)/libsprddepth.so
 LOCAL_SRC_FILES_64 := $(LIB_PATH)64/libsprddepth.so
-LOCAL_SHARED_LIBRARIES := libc libdl liblog libm libSegLite libTfliteWrapper
-
-ifeq (1, 1) #(strip $(shell expr $(ANDROID_MAJOR_VER) \>= 8)))
+LOCAL_SHARED_LIBRARIES := libc libdl liblog libm
+LOCAL_CHECK_ELF_FILES := false
+ifeq (1, $(strip $(shell expr $(ANDROID_MAJOR_VER) \>= 8)))
 LOCAL_PROPRIETARY_MODULE := true
 endif
 
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := src/sprd_depth_adapter.cpp
+LOCAL_MODULE := libsprddepthadapter
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS := -O3 -fno-strict-aliasing -fPIC -fvisibility=hidden -Wno-error=unused-parameter
+LOCAL_SHARED_LIBRARIES := libcutils liblog
+LOCAL_SHARED_LIBRARIES += libsprddepth
+
+LOCAL_C_INCLUDES := \
+        $(LOCAL_PATH)/inc \
+        $(LOCAL_PATH)/../inc \
+        $(TOP)/system/core/include/cutils/ \
+        $(TOP)/system/core/include/ \
+        $(TOP)/libnativehelper/include_jni
+        
+LOCAL_CHECK_ELF_FILES := false
+ifeq (1, $(strip $(shell expr $(ANDROID_MAJOR_VER) \>= 8)))
+LOCAL_PROPRIETARY_MODULE := true
 endif
+
+ifneq ($(filter $(TARGET_BOARD_PLATFORM), ud710), )
+LOCAL_CFLAGS += -DDEFAULT_RUNTYPE_NPU
+else ifneq ($(filter $(TARGET_BOARD_PLATFORM), ums512), )
+LOCAL_CFLAGS += -DDEFAULT_RUNTYPE_VDSP
+else 
+LOCAL_CFLAGS += -DDEFAULT_RUNTYPE_CPU
+endif
+
+include $(BUILD_SHARED_LIBRARY)
+include $(call all-makefiles-under, $(LOCAL_PATH))
+
+endif
+endif
+
+
--- libcam_device.mk
+++ libcam_device.mk
@@ -14,6 +14,9 @@ SPRD_LIB += libfcell
 SPRD_LIB += libsprd_fcell
 SPRD_LIB += libcamcalitest
 #endif
+
+SPRD_LIB += libunnengine
+
 PRODUCT_PACKAGES += $(SPRD_LIB)
 
 PRODUCT_COPY_FILES += vendor/sprd/modules/libcamera/arithmetic/sprd_easy_hdr/param/sprd_hdr_tuning.param:vendor/etc/sprd_hdr_tuning.param
@@ -23,9 +26,15 @@ PRODUCT_COPY_FILES += vendor/sprd/modules/libcamera/arithmetic/sprd_fdr/firmware
                       vendor/sprd/modules/libcamera/arithmetic/sprd_easy_hdr/firmware/hdr_cadence.bin:vendor/firmware/hdr_cadence.bin \
                       vendor/sprd/modules/libcamera/arithmetic/sprd_warp/firmware/warp_cadence.bin:vendor/firmware/warp_cadence.bin \
                       vendor/sprd/modules/libcamera/arithmetic/facebeauty/firmware/facebeauty_cadence.bin:vendor/firmware/facebeauty_cadence.bin \
-					  vendor/sprd/modules/libcamera/arithmetic/libmfnr/firmware/mfnr_cadence.bin:vendor/firmware/mfnr_cadence.bin\
-		      vendor/sprd/modules/libcamera/arithmetic/sprd_portrait_scene/firmware/portraitseg_cadence.bin:vendor/firmware/portraitseg_cadence.bin \
-		      vendor/sprd/modules/libcamera/arithmetic/sprd_portrait_scene/firmware/portraitseg_network.bin:vendor/firmware/portraitseg_network.bin
+                      vendor/sprd/modules/libcamera/arithmetic/libmfnr/firmware/mfnr_cadence.bin:vendor/firmware/mfnr_cadence.bin\
+                      vendor/sprd/modules/libcamera/arithmetic/sprd_portrait_scene/firmware/portraitseg_cadence.bin:vendor/firmware/portraitseg_cadence.bin \
+                      vendor/sprd/modules/libcamera/arithmetic/sprd_portrait_scene/firmware/portraitseg_network.bin:vendor/firmware/portraitseg_network.bin \
+                      vendor/sprd/modules/libcamera/arithmetic/depth/firmware/PortraitSegHP_cadence.bin:vendor/firmware/PortraitSegHP_cadence.bin \
+                      vendor/sprd/modules/libcamera/arithmetic/depth/firmware/PortraitSegHP_network.bin:vendor/firmware/PortraitSegHP_network.bin \
+                      vendor/sprd/modules/libcamera/arithmetic/depth/firmware/PortraitSegLP_cadence.bin:vendor/firmware/PortraitSegLP_cadence.bin \
+                      vendor/sprd/modules/libcamera/arithmetic/depth/firmware/PortraitSegLP_network.bin:vendor/firmware/PortraitSegLP_network.bin \
+                      vendor/sprd/modules/libcamera/arithmetic/depth/firmware/PortraitSegMP_cadence.bin:vendor/firmware/PortraitSegMP_cadence.bin \
+                      vendor/sprd/modules/libcamera/arithmetic/depth/firmware/PortraitSegMP_network.bin:vendor/firmware/PortraitSegMP_network.bin 
 
 COM_IMG_PATH :=$(LOCAL_PATH)/arithmetic/sprd_portrait_scene/image/common
 img_files := $(shell ls $(COM_IMG_PATH))
@@ -42,4 +51,9 @@ TF_MODEL_PATH := vendor/sprd/modules/libcamera/arithmetic/tf_models
 model_files := $(shell ls $(TF_MODEL_PATH))
 PRODUCT_COPY_FILES += $(foreach file, $(model_files), \
          $(TF_MODEL_PATH)/$(file):vendor/etc/tf_models/$(file))
+         
+UNN_TF_MODEL_PATH := vendor/sprd/modules/libcamera/arithmetic/tf_models
+unn_model_files := $(shell ls $(UNN_TF_MODEL_PATH))
+PRODUCT_COPY_FILES += $(foreach file, $(unn_model_files), \
+         $(UNN_TF_MODEL_PATH)/$(file):vendor/firmware/$(file))
 
