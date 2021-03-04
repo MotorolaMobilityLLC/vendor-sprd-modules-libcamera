57446d57b4d7025a93b0bb3a3b04fb50b0aa17c1
y <y>
2021-03-04 09:21:56 +0000

    Bug #1533892 To test the L6 video eis effect, the eis parameter must be calibrated
    [root cause  ]:add the eis calibrated parameter in L6
    [changes     ]:test the L6 phone eis effect
    [side effects]:no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:ums9230
    [test case]:video eis
    [reviewers   ] hongcheng.li
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Ia7ecc7d2d6cb4e4a8f8208d34a911b90a08bf6d3

--- arithmetic/eis/inc/sprd_eis.h
+++ arithmetic/eis/inc/sprd_eis.h
@@ -52,10 +52,11 @@ typedef struct sprd_eis_init_info {
 
 typedef struct sprd_eis_multi_init_info {
 	char board_name[36];
-    int camera_id;
+	char sensor_name[36];
 	double f;
 	double td;
 	double ts;
+	double fov_loss;
 } sprd_eis_multi_init_info_t;
 
 typedef struct eis_info {
@@ -107,11 +108,14 @@ const sprd_eis_init_info_t eis_init_info_tab[] = {
 };
 
 const sprd_eis_multi_init_info eis_multi_init_info_tab[] = {
-    {"sp9863a-1", 0, 0.7766f, 0.0397f, 0.0224f},
-    {"sp9863a-1", 3, 0.4701f, 0.0397f, 0.0219f},
-    {"ums512-1", 0, 0.7432f, 0.0289f, 0.0208f},
-    {"ums512-1", 2, 0.4468f, 0.0297f, 0.0204f},
-    {"ums512-1", 3, 1.5442f, 0.0308f, 0.0222f},
+    {"sp9863a-1", "imx351", 0.7766f, 0.0397f, 0.0224f, 0.25f},
+    {"sp9863a-1", "ov5675_dual", 0.4701f, 0.0397f, 0.0219f, 0.25f},
+    {"ums512-1", "ov32a1q", 0.7432f, 0.0289f, 0.0208f, 0.25f},
+    {"ums512-1", "ov16885_normal", 0.4468f, 0.0297f, 0.0204f, 0.25f},
+    {"ums512-1", "ov8856_shine", 1.5442f, 0.0308f, 0.0222f, 0.25f},
+    {"ums512-1", "imx586", 0.7415f, 0.0232f, 0.0242f, 0.25f},
+    {"ums512-1", "ov8856_back", 0.4735f, 0.0193f, 0.0231f, 0.25f},
+    {"ums512-1", "ov08a10_back", 2.2842f, 0.0167f, 0.0207f, 0.25f},
 };
 
 #ifdef __cplusplus
