1ffea4db65d3441cb7231f3402debf97955930e5
yushu.wang <yushu.wang@unisoc.com>
2021-03-08 16:16:20 +0800

    Bug #1535868 add imx616 2lane setting for qogirn6Pro
    [root cause  ] imx616 need to run at 2lane mode
    [changes     ] add imx616 2lane driver
    [side effects]NA
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:yes
    [confirm dependent commit]:all
    [board]:qogirn6pro
    [test case]:camera
    [reviewers   ] hai.li
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I447e8726b7603d8a074bef3b25b7c06a2d87f95e

--- sensor/sensor_drv/sensor_lib_cfg.mk
+++ sensor/sensor_drv/sensor_lib_cfg.mk
@@ -51,6 +51,7 @@ PRODUCT_PACKAGES += libsensor_imx351 \
     libsensor_ov8856_front \
     libsensor_ov8856_back \
     libsensor_imx616 \
+    libsensor_imx616_2lane \
     libsensor_ov02a10 \
     libsensor_s5kgw1sp03 \
     libsensor_ov64b40 \
