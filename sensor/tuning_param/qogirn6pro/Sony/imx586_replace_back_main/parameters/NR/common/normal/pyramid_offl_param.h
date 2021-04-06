d2cc6bf9faae636a5b2c6e700104477453a0d65f
Luke.Li <Luke.Li@unisoc.com>
2021-04-06 16:39:02 +0800

    Bug #1527200 add imx586 param for N6pro 0406
    [root cause  ] sensor bringup
    [changes     ] add imx586 param
    [side effects] none
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:N6pro
    [test case]:preview and capture
    [reviewers   ] yougang.wang
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I244608ed2b982f8560776c42791d3468db8b25fc

---
+++ sensor/tuning_param/qogirn6pro/Sony/imx586_replace_back_main/parameters/NR/common/normal/pyramid_offl_param.h
@@ -0,0 +1,327 @@
+/*versionid=0x000D0010*/
+/*maxGain=0.00*/
+/*param0.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x06,
+	/*reserved*/
+	0x00,
+}
+,
+/*param1.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param2.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param3.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param4.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param5.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param6.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param7.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param8.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param9.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param10.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param11.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param12.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param13.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param14.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param15.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param16.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param17.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param18.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param19.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param20.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param21.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param22.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param23.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
+/*param24.&BasePoint=1&*/
+/*n6pro_sensor_pyramid_offl_level*/
+{
+	/*dec_offline_bypass*/
+	0x00,
+	/*reconstruction_byass*/
+	0x00,
+	/*offline_layer_num*/
+	0x00,
+	/*reserved*/
+	0x00,
+}
+,
