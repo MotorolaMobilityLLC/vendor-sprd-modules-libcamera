a8eb603c67949ba3c6282379bc0282b9230410e9
linger.wang <linger.wang@unisoc.com>
2021-04-07 11:17:34 +0000

    Bug #1611738 face ae not work in slowmotion
    [root cause  ] enable face ae in slowmotion
    [changes     ] enable face ae in slowmotion
    [side effects] none
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:9863
    [test case]:video slowmotion
    [reviewers   ] hongcheng.li,jinyun.yu
    [change_type ] AOB --> sprd_adaptation
    [tag_product ] common
    
    Change-Id: Id1df878645e2a46f3ecb56ff07c96b019c005e4f
    (cherry picked from commit d7bae80ebbfbe9bba8efa00d425623c947484778)
    (cherry picked from commit a75e16343b81fe56b5988244b72cfba2642bdbe3)

--- hal3_2v6/SprdCamera3OEMIf.cpp
+++ hal3_2v6/SprdCamera3OEMIf.cpp
@@ -1365,9 +1365,6 @@ status_t SprdCamera3OEMIf::faceDectect(bool enable) {
         HAL_LOGE("oem is null or oem ops is null");
         return UNKNOWN_ERROR;
     }
-    sprddefInfo = mSetting->getSPRDDEFTagPTR();
-    if (sprddefInfo->slowmotion > 1)
-        return ret;
 
     if (enable) {
         mHalOem->ops->camera_fd_start(mCameraHandle, 1);
