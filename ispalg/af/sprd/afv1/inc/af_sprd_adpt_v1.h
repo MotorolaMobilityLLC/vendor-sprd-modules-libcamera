82f65c87454cf094dea3f12734eedbd52ca15dfe
sam.zhang <sam.zhang@example.com>
2021-04-12 15:36:18 +0800

    Bug #1394088 AR core代码合入
    [root cause ]:cts test fail
    [changes ]:af adpt & af ctrl
    [side effects]:none
    [self test ]:cts test
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:none
    [confirm dependent commit]:none
    [board]:ums512
    [test case]:cts test
    [reviewers ]:wenbin.guo
    [change_type ]feature
    [tag_product ] specific
    
    Change-Id: I8f1d3850fe11dfa919e73e65c0442292382d3b9e

--- ispalg/af/sprd/afv1/inc/af_sprd_adpt_v1.h
+++ ispalg/af/sprd/afv1/inc/af_sprd_adpt_v1.h
@@ -274,6 +274,21 @@ typedef struct _mlog_AFtime {
 	char *AF_type;
 } mlog_AFtime;
 
+typedef struct _cts_params {
+	float focus_distance;
+	cmr_s32 frame_num;
+	cmr_u32 set_cts_params_flag;
+	cmr_u32 min_real_focus_distance;
+	cmr_u32 reserved[10];
+} cts_params;
+
+typedef struct _af_squeue {
+	cmr_u32 size;
+	cmr_s32 front;
+	cmr_s32 rear;
+	cts_params af_params[6];
+} af_squeue;
+
 typedef struct _af_ctrl {
 	void *af_alg_cxt;	// AF_Data fv;
 	cmr_u32 af_dump_info_len;
@@ -364,6 +379,9 @@ typedef struct _af_ctrl {
 	cmr_u32 slave_focus_cnt;
 	cmr_u32 pdaf_support;
 	cmr_u32 video_stop_af_mode;
+	af_squeue queue;
+	cmr_u16 focus_distance_cnt;
+	cmr_u32 focus_distance_result[3];
 } af_ctrl_t;
 
 cmr_handle sprd_afv1_init(void *in, void *out);
