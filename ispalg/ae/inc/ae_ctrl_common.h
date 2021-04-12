bc65e238de75436dda9b842629ece823891df11b
lihong.zhang <lihong.zhang@unisoc.com>
2021-04-12 13:32:38 +0800

    Bug #1555692    for manual sensor rtrunk
    [root cause  ]  for manual sensor rtrunk
    [changes     ]  CAM_ALGO_ISP_3A_AE_3.X_H22_20210412_1328 fix ae inc version
    [side effects]none
    [self test   ]:pass
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:rtrunk
    [test case]:normal camera test
    [reviewers   ] jiawei.he
    [change_type ] bugfix
    [tag_product ] common
    
    Change-Id: Ib61a5f391fc1ead7e51b1dab3e5c6c713bb4586f

--- ispalg/ae/inc/ae_ctrl_common.h
+++ ispalg/ae/inc/ae_ctrl_common.h
@@ -28,6 +28,11 @@ struct ae_callback_param {
 	cmr_u32 exp_line;
 	cmr_u64 exp_time;
 	cmr_u32 ae_stable;
+	cmr_u8 flash_fired;
+	cmr_u32 cur_effect_sensitivity;
+	cmr_u32 cur_effect_exp_time;
+	cmr_s32 frame_number;
+	float cur_effect_fps;
 };
 
 #endif
