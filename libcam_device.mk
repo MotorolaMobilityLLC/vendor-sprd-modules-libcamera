402a041c07f3156ba25a1abc09be8232c18a75da
xiaowei.liu <xiaowei.liu@unisoc.com>
2021-04-23 17:49:59 +0800

    Bug #1585436 update fdr for n6pro
    [root cause  ] update fdr for n6pro
    [changes     ] fdr config
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:n6pro
    [test case]:fdr
    [reviewers   ] shimin.he
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I8fc20108d89052ab33e832eda13968bd7fb6481c

--- libcam_device.mk
+++ libcam_device.mk
@@ -21,11 +21,11 @@ PRODUCT_PACKAGES += $(SPRD_LIB)
 
 PRODUCT_COPY_FILES += vendor/sprd/modules/libcamera/arithmetic/sprd_easy_hdr/param/sprd_hdr_tuning.param:vendor/etc/sprd_hdr_tuning.param
 
+$(call inherit-product-if-exists, vendor/sprd/modules/libcamera/arithmetic/sprd_fdr/product.mk)
 $(call inherit-product-if-exists, vendor/sprd/modules/libcamera/arithmetic/sprd_portrait_scene/product.mk)
 
 ifneq ($(filter $(TARGET_BOARD_PLATFORM), ums512), )
-PRODUCT_COPY_FILES += vendor/sprd/modules/libcamera/arithmetic/sprd_fdr/firmware/fdr_cadence.bin:vendor/firmware/fdr_cadence.bin \
-                      vendor/sprd/modules/libcamera/arithmetic/sprd_easy_hdr/firmware/hdr_cadence.bin:vendor/firmware/hdr_cadence.bin \
+PRODUCT_COPY_FILES += vendor/sprd/modules/libcamera/arithmetic/sprd_easy_hdr/firmware/hdr_cadence.bin:vendor/firmware/hdr_cadence.bin \
                       vendor/sprd/modules/libcamera/arithmetic/sprd_warp/firmware/warp_cadence.bin:vendor/firmware/warp_cadence.bin \
                       vendor/sprd/modules/libcamera/arithmetic/facebeauty/firmware/facebeauty_cadence.bin:vendor/firmware/facebeauty_cadence.bin \
                       vendor/sprd/modules/libcamera/arithmetic/libmfnr/firmware/mfnr_cadence.bin:vendor/firmware/mfnr_cadence.bin\
