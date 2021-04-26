9f9bbf0d84872b1716177550826ce44523e5d62c
wangtingting <tingting.wang@chino-e.com>
2021-04-26 20:22:33 +0800

    moto_e20-26[moto_e20]:S0601 camera tuning parameter update
    Root cause:
    Tuning parameter
    
    Solution:
    Tuning parameter
    
    Test Cases:
    Ontim camera test list
    
    Change-Id: I4fdeef43c05ea72a3024179cf3804654c61ef058
    St-result:PASS

--- sensor/tuning_param/qogirl6/Galaxycore/gc5035_arb/parameters/sensor_gc5035_arb_raw_param_main.c
+++ sensor/tuning_param/qogirl6/Galaxycore/gc5035_arb/parameters/sensor_gc5035_arb_raw_param_main.c
@@ -22,7 +22,6 @@
 
 #include "sensor_raw.h"
 
-
 #define _NR_MAP_PARAM_
 #include "isp_nr.h"
 #undef _NR_MAP_PARAM_
@@ -45,7 +44,7 @@
 /************************************************************************/
 
 
-/* IspToolVersion=R1.17.0501 */
+/* IspToolVersion=R1.21.1601 */
 
 
 /* Capture Sizes:
@@ -99,13 +98,7 @@ static struct sensor_raw_ioctrl s_gc5035_arb_ioctrl=
 };
 
 
-/********************************************************************************
- * static struct sensor_version_info s_****_version_info, **** is the sensor name .
- * Param[2]/ Param[3] are ASCII values of the sensor name string ****.
- * Please modify the sensor name by using rename sensor function of the ISP TOOL, 
- * then the Param[2]/ Param[3] are changed accordingly.
- * NO modifying manually.
-********************************************************************************/
+/************************************************************************/
 
 
 static struct sensor_version_info s_gc5035_arb_version_info=
