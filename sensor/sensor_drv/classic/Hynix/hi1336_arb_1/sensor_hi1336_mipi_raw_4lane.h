a02390504a38407f91d974f430e50b5866e08b6a
taoshuo <shuo.tao@chino-e.com>
2021-05-26 11:18:55 +0800

    ARUBA-172:modify sensor_drv "is_loose" to solve Night Vision thumbnail Huaping
    St-result:PASS
    
    Night Vision thumbnail Huaping
    
    Change-Id: I01f34b5364acf59bcc789d8933bec4b0079ee66c

--- sensor/sensor_drv/classic/Hynix/hi1336_arb_1/sensor_hi1336_mipi_raw_4lane.h
+++ sensor/sensor_drv/classic/Hynix/hi1336_arb_1/sensor_hi1336_mipi_raw_4lane.h
@@ -1449,7 +1449,7 @@ static struct sensor_module_info s_hi1336_module_info_tab[VENDOR_NUM] = {
              .type = SENSOR_INTERFACE_TYPE_CSI2,
              .bus_width = LANE_NUM,
              .pixel_width = RAW_BITS,
-             .is_loose = 0,
+             .is_loose = 2,
          },
      .change_setting_skip_num = 1,
      .horizontal_view_angle = 65,
