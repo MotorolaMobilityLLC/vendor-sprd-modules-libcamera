28ff72e9512c2bb2d062009183f155322d11a497
wenhong <hong.wen@chino-e.com>
2021-05-29 14:11:06 +0800

    EKARUBA-2109:S0601 the main camera tuning parameter update  Root cause:Tuning parameter  Solution:Tuning parameter  Test Cases:Ontim camera test list  St-result:PASS
    Change-Id: Ib69b152fffa06ebee15568340fa35c36ae6084f9

--- sensor/tuning_param/qogirl6/Hynix/hi1336_arb_1/parameters/isp_nr.h
+++ sensor/tuning_param/qogirl6/Hynix/hi1336_arb_1/parameters/isp_nr.h
@@ -10,8 +10,8 @@ static struct sensor_nr_level_map_param s_hi1336_arb_1_default_nr_level_map_para
 }};
 
 static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
-	0x00000001,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,
-	0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,
+	0x000000E3,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,
+	0x00000000,0x00000000,0x00000001,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,
 	0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,
 	0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000
 }};
@@ -19,100 +19,225 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 
 #ifdef _NR_BAYER_NR_PARAM_
 #include "NR/common/normal/bayer_nr_param.h"
+#include "NR/common/night/bayer_nr_param.h"
+#include "NR/common/panorama/bayer_nr_param.h"
+#include "NR/common/hdr/bayer_nr_param.h"
+#include "NR/common/bokeh/bayer_nr_param.h"
+#include "NR/video_1/normal/bayer_nr_param.h"
 #endif
 
 #ifdef _NR_VST_PARAM_
 #include "NR/common/normal/vst_param.h"
+#include "NR/common/night/vst_param.h"
+#include "NR/common/panorama/vst_param.h"
+#include "NR/common/hdr/vst_param.h"
+#include "NR/common/bokeh/vst_param.h"
+#include "NR/video_1/normal/vst_param.h"
 #endif
 
 #ifdef _NR_IVST_PARAM_
 #include "NR/common/normal/ivst_param.h"
+#include "NR/common/night/ivst_param.h"
+#include "NR/common/panorama/ivst_param.h"
+#include "NR/common/hdr/ivst_param.h"
+#include "NR/common/bokeh/ivst_param.h"
+#include "NR/video_1/normal/ivst_param.h"
 #endif
 
 #ifdef _NR_RGB_DITHER_PARAM_
 #include "NR/common/normal/rgb_dither_param.h"
+#include "NR/common/night/rgb_dither_param.h"
+#include "NR/common/panorama/rgb_dither_param.h"
+#include "NR/common/hdr/rgb_dither_param.h"
+#include "NR/common/bokeh/rgb_dither_param.h"
+#include "NR/video_1/normal/rgb_dither_param.h"
 #endif
 
 #ifdef _NR_BPC_PARAM_
 #include "NR/common/normal/bpc_param.h"
+#include "NR/common/night/bpc_param.h"
+#include "NR/common/panorama/bpc_param.h"
+#include "NR/common/hdr/bpc_param.h"
+#include "NR/common/bokeh/bpc_param.h"
+#include "NR/video_1/normal/bpc_param.h"
 #endif
 
 #ifdef _NR_CFAI_PARAM_
 #include "NR/common/normal/cfai_param.h"
+#include "NR/common/night/cfai_param.h"
+#include "NR/common/panorama/cfai_param.h"
+#include "NR/common/hdr/cfai_param.h"
+#include "NR/common/bokeh/cfai_param.h"
+#include "NR/video_1/normal/cfai_param.h"
 #endif
 
 #ifdef _NR_CCE_UVDIV_PARAM_
 #include "NR/common/normal/cce_uvdiv_param.h"
+#include "NR/common/night/cce_uvdiv_param.h"
+#include "NR/common/panorama/cce_uvdiv_param.h"
+#include "NR/common/hdr/cce_uvdiv_param.h"
+#include "NR/common/bokeh/cce_uvdiv_param.h"
+#include "NR/video_1/normal/cce_uvdiv_param.h"
 #endif
 
 #ifdef _NR_YNR_PARAM_
 #include "NR/common/normal/ynr_param.h"
+#include "NR/common/night/ynr_param.h"
+#include "NR/common/panorama/ynr_param.h"
+#include "NR/common/hdr/ynr_param.h"
+#include "NR/common/bokeh/ynr_param.h"
+#include "NR/video_1/normal/ynr_param.h"
 #endif
 
 #ifdef _NR_EE_PARAM_
 #include "NR/common/normal/ee_param.h"
+#include "NR/common/night/ee_param.h"
+#include "NR/common/panorama/ee_param.h"
+#include "NR/common/hdr/ee_param.h"
+#include "NR/common/bokeh/ee_param.h"
+#include "NR/video_1/normal/ee_param.h"
 #endif
 
 #ifdef _NR_3DNR_PARAM_
 #include "NR/common/normal/3dnr_param.h"
+#include "NR/common/night/3dnr_param.h"
+#include "NR/common/panorama/3dnr_param.h"
+#include "NR/common/hdr/3dnr_param.h"
+#include "NR/common/bokeh/3dnr_param.h"
+#include "NR/video_1/normal/3dnr_param.h"
 #endif
 
 #ifdef _NR_PPE_PARAM_
 #include "NR/common/normal/ppe_param.h"
+#include "NR/common/night/ppe_param.h"
+#include "NR/common/panorama/ppe_param.h"
+#include "NR/common/hdr/ppe_param.h"
+#include "NR/common/bokeh/ppe_param.h"
+#include "NR/video_1/normal/ppe_param.h"
 #endif
 
 #ifdef _NR_YUV_NOISEFILTER_PARAM_
 #include "NR/common/normal/yuv_noisefilter_param.h"
+#include "NR/common/night/yuv_noisefilter_param.h"
+#include "NR/common/panorama/yuv_noisefilter_param.h"
+#include "NR/common/hdr/yuv_noisefilter_param.h"
+#include "NR/common/bokeh/yuv_noisefilter_param.h"
+#include "NR/video_1/normal/yuv_noisefilter_param.h"
 #endif
 
 #ifdef _NR_RGB_AFM_PARAM_
 #include "NR/common/normal/rgb_afm_param.h"
+#include "NR/common/night/rgb_afm_param.h"
+#include "NR/common/panorama/rgb_afm_param.h"
+#include "NR/common/hdr/rgb_afm_param.h"
+#include "NR/common/bokeh/rgb_afm_param.h"
+#include "NR/video_1/normal/rgb_afm_param.h"
 #endif
 
 #ifdef _NR_IIRCNR_PARAM_
 #include "NR/common/normal/iircnr_param.h"
+#include "NR/common/night/iircnr_param.h"
+#include "NR/common/panorama/iircnr_param.h"
+#include "NR/common/hdr/iircnr_param.h"
+#include "NR/common/bokeh/iircnr_param.h"
+#include "NR/video_1/normal/iircnr_param.h"
 #endif
 
 #ifdef _NR_YUV_PRECDN_PARAM_
 #include "NR/common/normal/yuv_precdn_param.h"
+#include "NR/common/night/yuv_precdn_param.h"
+#include "NR/common/panorama/yuv_precdn_param.h"
+#include "NR/common/hdr/yuv_precdn_param.h"
+#include "NR/common/bokeh/yuv_precdn_param.h"
+#include "NR/video_1/normal/yuv_precdn_param.h"
 #endif
 
 #ifdef _NR_UV_CDN_PARAM_
 #include "NR/common/normal/uv_cdn_param.h"
+#include "NR/common/night/uv_cdn_param.h"
+#include "NR/common/panorama/uv_cdn_param.h"
+#include "NR/common/hdr/uv_cdn_param.h"
+#include "NR/common/bokeh/uv_cdn_param.h"
+#include "NR/video_1/normal/uv_cdn_param.h"
 #endif
 
 #ifdef _NR_UV_POSTCDN_PARAM_
 #include "NR/common/normal/uv_postcdn_param.h"
+#include "NR/common/night/uv_postcdn_param.h"
+#include "NR/common/panorama/uv_postcdn_param.h"
+#include "NR/common/hdr/uv_postcdn_param.h"
+#include "NR/common/bokeh/uv_postcdn_param.h"
+#include "NR/video_1/normal/uv_postcdn_param.h"
 #endif
 
 #ifdef _NR_CNR_PARAM_
 #include "NR/common/normal/cnr_param.h"
+#include "NR/common/night/cnr_param.h"
+#include "NR/common/panorama/cnr_param.h"
+#include "NR/common/hdr/cnr_param.h"
+#include "NR/common/bokeh/cnr_param.h"
+#include "NR/video_1/normal/cnr_param.h"
 #endif
 
 #ifdef _NR_IMBALANCE_PARAM_
 #include "NR/common/normal/imbalance_param.h"
+#include "NR/common/night/imbalance_param.h"
+#include "NR/common/panorama/imbalance_param.h"
+#include "NR/common/hdr/imbalance_param.h"
+#include "NR/common/bokeh/imbalance_param.h"
+#include "NR/video_1/normal/imbalance_param.h"
 #endif
 
 #ifdef _NR_SW3DNR_PARAM_
 #include "NR/common/normal/sw3dnr_param.h"
+#include "NR/common/night/sw3dnr_param.h"
+#include "NR/common/panorama/sw3dnr_param.h"
+#include "NR/common/hdr/sw3dnr_param.h"
+#include "NR/common/bokeh/sw3dnr_param.h"
+#include "NR/video_1/normal/sw3dnr_param.h"
 #endif
 
 #ifdef _NR_BWU_BWD_PARAM_
 #include "NR/common/normal/bwud_param.h"
+#include "NR/common/night/bwud_param.h"
+#include "NR/common/panorama/bwud_param.h"
+#include "NR/common/hdr/bwud_param.h"
+#include "NR/common/bokeh/bwud_param.h"
+#include "NR/video_1/normal/bwud_param.h"
 #endif
 
 #ifdef _NR_YNRS_PARAM_
 #include "NR/common/normal/ynrs_param.h"
+#include "NR/common/night/ynrs_param.h"
+#include "NR/common/panorama/ynrs_param.h"
+#include "NR/common/hdr/ynrs_param.h"
+#include "NR/common/bokeh/ynrs_param.h"
+#include "NR/video_1/normal/ynrs_param.h"
 #endif
 
 #ifdef _NR_CNR3_PARAM_
 #include "NR/common/normal/cnr3_param.h"
+#include "NR/common/night/cnr3_param.h"
+#include "NR/common/panorama/cnr3_param.h"
+#include "NR/common/hdr/cnr3_param.h"
+#include "NR/common/bokeh/cnr3_param.h"
+#include "NR/video_1/normal/cnr3_param.h"
 #endif
 
 #ifdef _NR_MFNR_PARAM_
 #include "NR/common/normal/mfnr_param.h"
+#include "NR/common/night/mfnr_param.h"
+#include "NR/common/panorama/mfnr_param.h"
+#include "NR/common/hdr/mfnr_param.h"
+#include "NR/common/bokeh/mfnr_param.h"
+#include "NR/video_1/normal/mfnr_param.h"
 #endif
 
 #ifdef _NR_POST_EE_PARAM_
 #include "NR/common/normal/post_ee_param.h"
+#include "NR/common/night/post_ee_param.h"
+#include "NR/common/panorama/post_ee_param.h"
+#include "NR/common/hdr/post_ee_param.h"
+#include "NR/common/bokeh/post_ee_param.h"
+#include "NR/video_1/normal/post_ee_param.h"
 #endif
