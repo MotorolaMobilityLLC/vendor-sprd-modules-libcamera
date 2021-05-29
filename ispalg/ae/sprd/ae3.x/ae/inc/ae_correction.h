5093e0e53d6d6f4af597a739effe99dff1479174
jiawei.he <jiawei.he@unisoc.com>
2021-05-29 15:50:48 +0800

    Revert "Revert "Bug #1033099-optimize ae code for power""
    This reverts commit fdfce2f4abe42c29fe69cdc4979cb7db000fa395.
    [change_type ] feature_bugfix
    [tag_product ] common
    [self test] camera normal test
    
    Change-Id: I0a4449e133334be6dfe31a91b1fdd2bd226fe6fc

--- ispalg/ae/sprd/ae3.x/ae/inc/ae_correction.h
+++ ispalg/ae/sprd/ae3.x/ae/inc/ae_correction.h
@@ -43,6 +43,15 @@ enum ae_sync_type {
 	AE_SYNC_1,/*for brigness sync: for zoom*/
 };
 
+enum {
+	AEC_SINGLE = 0,
+	AEC_DUAL,
+	AEC_BLUR,
+	AEC_TRIBLE_W_T_UW,
+	AEC_TRIBLE_W_T_UW_SYNC,
+	AEC_CAMERA_MAX,
+};
+
 struct tar_lum_range{//added by feifan.wang
 	cmr_u32 target_lum_range_in_bak;
 	cmr_u32 target_lum_range_out_bak;
