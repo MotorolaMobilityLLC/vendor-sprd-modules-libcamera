8a9fe52783c021a26320e6fcbd5a48b28b0e1d86
wenhong <hong.wen@chino-e.com>
2021-06-04 21:04:34 +0800

    EKmoto_e20-2109:S0601 the main camera tuning parameter update  Root cause:Tuning parameter  Solution:Tuning parameter  Test Cases:Ontim camera test list  St-result:PASS
    Change-Id: Ib504b23eab9024a31e09eb8afc36ba30d7a5acb8

--- sensor/tuning_param/qogirl6/Hynix/hi1336_arb_1/parameters/isp_nr.h
+++ sensor/tuning_param/qogirl6/Hynix/hi1336_arb_1/parameters/isp_nr.h
@@ -11,7 +11,7 @@ static struct sensor_nr_level_map_param s_hi1336_arb_1_default_nr_level_map_para
 
 static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 	0x000000E3,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,
-	0x00000000,0x00000000,0x00000001,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,
+	0x00000000,0x00000000,0x00000001,0x00000000,0x00000000,0x00000001,0x00000000,0x00000000,
 	0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,
 	0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000
 }};
@@ -24,6 +24,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/bayer_nr_param.h"
 #include "NR/common/bokeh/bayer_nr_param.h"
 #include "NR/video_1/normal/bayer_nr_param.h"
+#include "NR/fdr_cap0/normal/bayer_nr_param.h"
 #endif
 
 #ifdef _NR_VST_PARAM_
@@ -33,6 +34,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/vst_param.h"
 #include "NR/common/bokeh/vst_param.h"
 #include "NR/video_1/normal/vst_param.h"
+#include "NR/fdr_cap0/normal/vst_param.h"
 #endif
 
 #ifdef _NR_IVST_PARAM_
@@ -42,6 +44,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/ivst_param.h"
 #include "NR/common/bokeh/ivst_param.h"
 #include "NR/video_1/normal/ivst_param.h"
+#include "NR/fdr_cap0/normal/ivst_param.h"
 #endif
 
 #ifdef _NR_RGB_DITHER_PARAM_
@@ -51,6 +54,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/rgb_dither_param.h"
 #include "NR/common/bokeh/rgb_dither_param.h"
 #include "NR/video_1/normal/rgb_dither_param.h"
+#include "NR/fdr_cap0/normal/rgb_dither_param.h"
 #endif
 
 #ifdef _NR_BPC_PARAM_
@@ -60,6 +64,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/bpc_param.h"
 #include "NR/common/bokeh/bpc_param.h"
 #include "NR/video_1/normal/bpc_param.h"
+#include "NR/fdr_cap0/normal/bpc_param.h"
 #endif
 
 #ifdef _NR_CFAI_PARAM_
@@ -69,6 +74,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/cfai_param.h"
 #include "NR/common/bokeh/cfai_param.h"
 #include "NR/video_1/normal/cfai_param.h"
+#include "NR/fdr_cap0/normal/cfai_param.h"
 #endif
 
 #ifdef _NR_CCE_UVDIV_PARAM_
@@ -78,6 +84,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/cce_uvdiv_param.h"
 #include "NR/common/bokeh/cce_uvdiv_param.h"
 #include "NR/video_1/normal/cce_uvdiv_param.h"
+#include "NR/fdr_cap0/normal/cce_uvdiv_param.h"
 #endif
 
 #ifdef _NR_YNR_PARAM_
@@ -87,6 +94,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/ynr_param.h"
 #include "NR/common/bokeh/ynr_param.h"
 #include "NR/video_1/normal/ynr_param.h"
+#include "NR/fdr_cap0/normal/ynr_param.h"
 #endif
 
 #ifdef _NR_EE_PARAM_
@@ -96,6 +104,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/ee_param.h"
 #include "NR/common/bokeh/ee_param.h"
 #include "NR/video_1/normal/ee_param.h"
+#include "NR/fdr_cap0/normal/ee_param.h"
 #endif
 
 #ifdef _NR_3DNR_PARAM_
@@ -105,6 +114,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/3dnr_param.h"
 #include "NR/common/bokeh/3dnr_param.h"
 #include "NR/video_1/normal/3dnr_param.h"
+#include "NR/fdr_cap0/normal/3dnr_param.h"
 #endif
 
 #ifdef _NR_PPE_PARAM_
@@ -114,6 +124,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/ppe_param.h"
 #include "NR/common/bokeh/ppe_param.h"
 #include "NR/video_1/normal/ppe_param.h"
+#include "NR/fdr_cap0/normal/ppe_param.h"
 #endif
 
 #ifdef _NR_YUV_NOISEFILTER_PARAM_
@@ -123,6 +134,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/yuv_noisefilter_param.h"
 #include "NR/common/bokeh/yuv_noisefilter_param.h"
 #include "NR/video_1/normal/yuv_noisefilter_param.h"
+#include "NR/fdr_cap0/normal/yuv_noisefilter_param.h"
 #endif
 
 #ifdef _NR_RGB_AFM_PARAM_
@@ -132,6 +144,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/rgb_afm_param.h"
 #include "NR/common/bokeh/rgb_afm_param.h"
 #include "NR/video_1/normal/rgb_afm_param.h"
+#include "NR/fdr_cap0/normal/rgb_afm_param.h"
 #endif
 
 #ifdef _NR_IIRCNR_PARAM_
@@ -141,6 +154,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/iircnr_param.h"
 #include "NR/common/bokeh/iircnr_param.h"
 #include "NR/video_1/normal/iircnr_param.h"
+#include "NR/fdr_cap0/normal/iircnr_param.h"
 #endif
 
 #ifdef _NR_YUV_PRECDN_PARAM_
@@ -150,6 +164,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/yuv_precdn_param.h"
 #include "NR/common/bokeh/yuv_precdn_param.h"
 #include "NR/video_1/normal/yuv_precdn_param.h"
+#include "NR/fdr_cap0/normal/yuv_precdn_param.h"
 #endif
 
 #ifdef _NR_UV_CDN_PARAM_
@@ -159,6 +174,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/uv_cdn_param.h"
 #include "NR/common/bokeh/uv_cdn_param.h"
 #include "NR/video_1/normal/uv_cdn_param.h"
+#include "NR/fdr_cap0/normal/uv_cdn_param.h"
 #endif
 
 #ifdef _NR_UV_POSTCDN_PARAM_
@@ -168,6 +184,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/uv_postcdn_param.h"
 #include "NR/common/bokeh/uv_postcdn_param.h"
 #include "NR/video_1/normal/uv_postcdn_param.h"
+#include "NR/fdr_cap0/normal/uv_postcdn_param.h"
 #endif
 
 #ifdef _NR_CNR_PARAM_
@@ -177,6 +194,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/cnr_param.h"
 #include "NR/common/bokeh/cnr_param.h"
 #include "NR/video_1/normal/cnr_param.h"
+#include "NR/fdr_cap0/normal/cnr_param.h"
 #endif
 
 #ifdef _NR_IMBALANCE_PARAM_
@@ -186,6 +204,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/imbalance_param.h"
 #include "NR/common/bokeh/imbalance_param.h"
 #include "NR/video_1/normal/imbalance_param.h"
+#include "NR/fdr_cap0/normal/imbalance_param.h"
 #endif
 
 #ifdef _NR_SW3DNR_PARAM_
@@ -195,6 +214,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/sw3dnr_param.h"
 #include "NR/common/bokeh/sw3dnr_param.h"
 #include "NR/video_1/normal/sw3dnr_param.h"
+#include "NR/fdr_cap0/normal/sw3dnr_param.h"
 #endif
 
 #ifdef _NR_BWU_BWD_PARAM_
@@ -204,6 +224,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/bwud_param.h"
 #include "NR/common/bokeh/bwud_param.h"
 #include "NR/video_1/normal/bwud_param.h"
+#include "NR/fdr_cap0/normal/bwud_param.h"
 #endif
 
 #ifdef _NR_YNRS_PARAM_
@@ -213,6 +234,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/ynrs_param.h"
 #include "NR/common/bokeh/ynrs_param.h"
 #include "NR/video_1/normal/ynrs_param.h"
+#include "NR/fdr_cap0/normal/ynrs_param.h"
 #endif
 
 #ifdef _NR_CNR3_PARAM_
@@ -222,6 +244,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/cnr3_param.h"
 #include "NR/common/bokeh/cnr3_param.h"
 #include "NR/video_1/normal/cnr3_param.h"
+#include "NR/fdr_cap0/normal/cnr3_param.h"
 #endif
 
 #ifdef _NR_MFNR_PARAM_
@@ -231,6 +254,7 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/mfnr_param.h"
 #include "NR/common/bokeh/mfnr_param.h"
 #include "NR/video_1/normal/mfnr_param.h"
+#include "NR/fdr_cap0/normal/mfnr_param.h"
 #endif
 
 #ifdef _NR_POST_EE_PARAM_
@@ -240,4 +264,5 @@ static struct sensor_nr_scene_map_param s_hi1336_arb_1_nr_scene_map_param = {{
 #include "NR/common/hdr/post_ee_param.h"
 #include "NR/common/bokeh/post_ee_param.h"
 #include "NR/video_1/normal/post_ee_param.h"
+#include "NR/fdr_cap0/normal/post_ee_param.h"
 #endif
