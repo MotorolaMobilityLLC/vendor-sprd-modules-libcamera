b5c0e61ac25ba5c88f9d8b7db8712fed9d797a58
wenhong <hong.wen@chino-e.com>
2021-06-09 19:38:18 +0800

    s0601-moto_e20-2417:S0601 2 camera FDR  tuning parameter update             Root cause:Tuning parameter             Solution:Tuning parameter             Test Cases:Ontim camera test list             St-result:PASS
    Change-Id: I4dc8611b8c3e23abc365778fec4abcef5f820297

--- sensor/tuning_param/qogirl6/Hynix/hi1336_arb_2/parameters/NR/fdr_cap0/normal/yuv_precdn_param.h
+++ sensor/tuning_param/qogirl6/Hynix/hi1336_arb_2/parameters/NR/fdr_cap0/normal/yuv_precdn_param.h
@@ -422,7 +422,7 @@
 	/*reserved0*/
 	0x00,
 	/*bypass*/
-	0x00000001,
+	0x00000000,
 }
 ,
 /*param5.&BasePoint=1&*/
@@ -507,7 +507,7 @@
 	/*reserved0*/
 	0x00,
 	/*bypass*/
-	0x00000001,
+	0x00000000,
 }
 ,
 /*param6.&BasePoint=1&*/
@@ -592,7 +592,7 @@
 	/*reserved0*/
 	0x00,
 	/*bypass*/
-	0x00000001,
+	0x00000000,
 }
 ,
 /*param7.&BasePoint=1&*/
@@ -677,7 +677,7 @@
 	/*reserved0*/
 	0x00,
 	/*bypass*/
-	0x00000001,
+	0x00000000,
 }
 ,
 /*param8.&BasePoint=1&*/
