7698664a4e09e8211b084bc17a1da70aba74446d
jie.liu <jie.liu@unisoc.com>
2021-06-14 17:49:25 +0800

    Bug #1631034 后摄第一轮超级夜景Pro主观测试存在伪影明显、画面朦、融合不自然、人脸不自然、人脸偏暗、亮噪明显且颗粒较大等效果问题
    [root cause  ] fdr param not suitable
    [changes     ] change fdr aectrl param and drc param
    [side effects] none
    [self test   ]:pre ok
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:ums9230
    [test case]:3/3
    [reviewers   ] yougang.wang
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I6e27359bb2027444628fd45ec7f2365f79f319d2

--- sensor/tuning_param/qogirl6/Sony/imx586/parameters/sensor_imx586_raw_param_main.c
+++ sensor/tuning_param/qogirl6/Sony/imx586/parameters/sensor_imx586_raw_param_main.c
@@ -37,7 +37,7 @@
 #include "sensor_imx586_raw_param_video_0.c"
 #include "sensor_imx586_raw_param_video_1.c"
 #include "sensor_imx586_raw_param_video_2.c"
-#include "sensor_imx586_raw_param_fdr_cap0.c"
+#include "sensor_imx586_raw_param_fdr_cap1.c"
 
 /* End Include */
 
@@ -175,8 +175,8 @@ static struct sensor_raw_info s_imx586_mipi_raw_info=
 		{s_imx586_tune_info_video_1, sizeof(s_imx586_tune_info_video_1)},
 		{s_imx586_tune_info_video_2, sizeof(s_imx586_tune_info_video_2)},
 		{NULL, 0},
-		{s_imx586_tune_info_fdr_cap0, sizeof(s_imx586_tune_info_fdr_cap0)},
 		{NULL, 0},
+		{s_imx586_tune_info_fdr_cap1, sizeof(s_imx586_tune_info_fdr_cap1)},
 		{NULL, 0},
 		{NULL, 0},
 	},
@@ -197,8 +197,8 @@ static struct sensor_raw_info s_imx586_mipi_raw_info=
 		&s_imx586_fix_info_video_1,
 		&s_imx586_fix_info_video_2,
 		NULL,
-		&s_imx586_fix_info_fdr_cap0,
 		NULL,
+		&s_imx586_fix_info_fdr_cap1,
 		NULL,
 		NULL,
 	},
@@ -216,8 +216,8 @@ static struct sensor_raw_info s_imx586_mipi_raw_info=
 		{s_imx586_video_1_tool_ui_input, sizeof(s_imx586_video_1_tool_ui_input)},
 		{s_imx586_video_2_tool_ui_input, sizeof(s_imx586_video_2_tool_ui_input)},
 		{NULL, 0},
-		{s_imx586_fdr_cap0_tool_ui_input, sizeof(s_imx586_fdr_cap0_tool_ui_input)},
 		{NULL, 0},
+		{s_imx586_fdr_cap1_tool_ui_input, sizeof(s_imx586_fdr_cap1_tool_ui_input)},
 		{NULL, 0},
 		{NULL, 0},
 	},
