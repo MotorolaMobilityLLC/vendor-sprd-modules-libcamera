cfddbc2b3c0890dd0064dc3961314e502396bdf3
guozuoyu <zuoyu.guo@chino-e.com>
2021-06-21 18:38:02 +0800

    moto_e40-7::bring up add mono infor for main2 ov02b10
    St-result:PASS
    
    add mono infor for main2
    
    Change-Id: I6827964c49405ff3a99738772b4f4c3136118740

--- sensor/sensor_drv/classic/OmniVision/ov02b1b_syp_1/sensor_ov02b1b_syp_mipi_raw.h
+++ sensor/sensor_drv/classic/OmniVision/ov02b1b_syp_1/sensor_ov02b1b_syp_mipi_raw.h
@@ -20,7 +20,7 @@
 
 
 #define VENDOR_NUM           1
-#define SENSOR_NAME          "ov02b1b_syp_mipi_raw"
+#define SENSOR_NAME          "ov02b1b_syp_1"
 
 #define I2C_SLAVE_ADDR       0x7A     /*0x20*//* 8bit slave address*/
 
@@ -383,7 +383,8 @@ static SENSOR_STATIC_INFO_T s_ov02b1b_syp_static_info[VENDOR_NUM] = {
         .exp_valid_frame_num = 1,
         .clamp_level = 64,
         .adgain_valid_frame_num = 1,
-        .fov_info = {{4.614f, 3.444f}, 4.222f}}
+        .fov_info = {{4.614f, 3.444f}, 4.222f},
+        .mono_sensor = 1}
     }
     /*If there are multiple modules,please add here*/
 };
