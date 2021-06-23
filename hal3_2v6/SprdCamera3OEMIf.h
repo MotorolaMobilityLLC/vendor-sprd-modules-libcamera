58bba5fd866544d11fc2a9198aba0cf1f556816c
chenguoping <guoping.chen@ontim.cn>
2021-06-23 10:14:46 +0800

    EKARUBA-2177[Aruba][System Test][Camera][8/10][Critical]Turn on the flash to block the camera, and the camera's shooting interface is black
    Root Cause:
    Photo AF status stuck
    
    Solution:
    Join unisoc patch
    
    TestCase:
    Turn on the camera> select taking pictures> turn on the flash light> block the camera> take pictures
    
    St-result : PASS
    
    Change-Id: I9473ff088c6c8146333730b701827648884158ee

--- hal3_2v6/SprdCamera3OEMIf.h
+++ hal3_2v6/SprdCamera3OEMIf.h
@@ -1062,6 +1062,8 @@ class SprdCamera3OEMIf : public virtual RefBase {
     uint32_t mSkipNum;
     bool EisErr;
     uint32_t mNeed_share_buf;
+    bool mIsCapturing;
+    bool mNeedCancelAutoFocus;
 };
 
 }; // namespace sprdcamera
