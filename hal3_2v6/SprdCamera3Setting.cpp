4b07f5a6fa6fe9cd5218461e17c77e56bdfcafe6
xinglili <lili.xing@chino-e.com>
2021-07-12 11:50:57 +0800

    EKCYPRUS-1674 JOT test Camera fail SUMMARY:edit ae state for hal St-result:PASS
    Change-Id: I4e6df4a7884da4c1d90e662e5cb4b2c3403200ca

--- hal3_2v6/SprdCamera3Setting.cpp
+++ hal3_2v6/SprdCamera3Setting.cpp
@@ -6713,9 +6713,15 @@ camera_metadata_t *SprdCamera3Setting::reportMetadataToFramework
         }
         if (camMetadata->exists(ANDROID_CONTROL_AE_STATE)) {
             valueU8 = camMetadata->find(ANDROID_CONTROL_AE_STATE).data.u8[0];
-            if(valueU8 != s_setting[mCameraId].controlInfo.ae_state)
-                    camMetadata->update(ANDROID_CONTROL_AE_STATE,
-                                       &(s_setting[mCameraId].resultInfo.ae_state), 1);
+            //if(valueU8 != s_setting[mCameraId].controlInfo.ae_state)
+            //        camMetadata->update(ANDROID_CONTROL_AE_STATE,
+             HAL_LOGD("camMetadata-> ANDROID_CONTROL_AE_STATE %d,controlInfo.ae_state %d", valueU8,s_setting[mCameraId].controlInfo.ae_state);
+
+            if(valueU8 != s_setting[mCameraId].controlInfo.ae_state){
+               camMetadata->update(ANDROID_CONTROL_AE_STATE, &(s_setting[mCameraId].resultInfo.ae_state), 1);
+            }else{
+                  camMetadata->update(ANDROID_CONTROL_AE_STATE, &(s_setting[mCameraId].resultInfo.ae_state), 1);
+            }
         }
     }
 
