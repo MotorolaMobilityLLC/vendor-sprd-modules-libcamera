72fed7b567a241455e0e2624110df13110f400b8
fuchun.lin <fuchun.lin@unisoc.com>
2021-08-05 16:25:22 +0800

    Bug #1671518 (SPCSS00882478) - [UMS512][ONTIM][moto_e40][11493]moto_e40 在暗光与逆光下无法识别到人脸
    [root cause  ] update fdv3 for 20a_moto
    [changes     ] update fdv3 for 20a_moto
    [side effects]update fdv3 for 20a_moto
    [self test   ]:update fdv3 for 20a_moto
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:ums512
    [test case]:update fdv3 for 20a_moto
    [reviewers   ] wenguo.li
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I7f5cdfb4b782bdb0901f50c7dc677ef7ff63bf88

--- arithmetic/sprdface/Android.mk
+++ arithmetic/sprdface/Android.mk
@@ -35,7 +35,10 @@ LOCAL_MODULE_TAGS := optional
 ifeq (1, 1) #(strip $(shell expr $(ANDROID_MAJOR_VER) \>= 8)))
 LOCAL_PROPRIETARY_MODULE := true
 endif
+
+
 include $(BUILD_PREBUILT)
+
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libFaceDetectV3
@@ -50,81 +53,7 @@ LOCAL_MODULE_TAGS := optional
 ifeq (1, 1) #(strip $(shell expr $(ANDROID_MAJOR_VER) \>= 8)))
 LOCAL_PROPRIETARY_MODULE := true
 endif
-include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libFaceChecker
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MULTILIB := both
-LOCAL_MODULE_STEM_32 := libFaceChecker.so
-LOCAL_MODULE_STEM_64 := libFaceChecker.so
-LOCAL_SRC_FILES_32 := $(LIB_PATH)/libFaceChecker.so
-LOCAL_SRC_FILES_64 := $(LIB_PATH)64/libFaceChecker.so
-LOCAL_SHARED_LIBRARIES := libumnn libc libdl liblog libm
-LOCAL_MODULE_TAGS := optional
-ifeq (1, 1) #(strip $(shell expr $(ANDROID_MAJOR_VER) \>= 8)))
-LOCAL_PROPRIETARY_MODULE := true
-endif
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libFaceTrackerFA
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MULTILIB := both
-LOCAL_MODULE_STEM_32 := libFaceTrackerFA.so
-LOCAL_MODULE_STEM_64 := libFaceTrackerFA.so
-LOCAL_SRC_FILES_32 := $(LIB_PATH)/libFaceTrackerFA.so
-LOCAL_SRC_FILES_64 := $(LIB_PATH)64/libFaceTrackerFA.so
-LOCAL_SHARED_LIBRARIES := libc libdl liblog libm
-LOCAL_MODULE_TAGS := optional
-ifeq (1, 1) #(strip $(shell expr $(ANDROID_MAJOR_VER) \>= 8)))
-LOCAL_PROPRIETARY_MODULE := true
-endif
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libFaceDetect
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MULTILIB := both
-LOCAL_MODULE_STEM_32 := libFaceDetect.so
-LOCAL_MODULE_STEM_64 := libFaceDetect.so
-LOCAL_SRC_FILES_32 := $(LIB_PATH)/libFaceDetect.so
-LOCAL_SRC_FILES_64 := $(LIB_PATH)64/libFaceDetect.so
-LOCAL_SHARED_LIBRARIES := libFaceDetectV3 libFaceChecker libFaceTrackerFA libc libdl liblog libm
-LOCAL_MODULE_TAGS := optional
-ifeq (1, 1) #(strip $(shell expr $(ANDROID_MAJOR_VER) \>= 8)))
-LOCAL_PROPRIETARY_MODULE := true
-endif
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libFaceDetectHW
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MULTILIB := both
-LOCAL_MODULE_STEM_32 := libFaceDetectHW.so
-LOCAL_MODULE_STEM_64 := libFaceDetectHW.so
-LOCAL_SRC_FILES_32 := $(LIB_PATH)/libFaceDetectHW.so
-LOCAL_SRC_FILES_64 := $(LIB_PATH)64/libFaceDetectHW.so
-LOCAL_SHARED_LIBRARIES := libc libdl liblog libm libFaceChecker libFaceTrackerFA libsprdfd_hw
-LOCAL_MODULE_TAGS := optional
-ifeq (1, 1) #(strip $(shell expr $(ANDROID_MAJOR_VER) \>= 8)))
-LOCAL_PROPRIETARY_MODULE := true
-endif
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libFaceDetectL
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MULTILIB := both
-LOCAL_MODULE_STEM_32 := libFaceDetectL.so
-LOCAL_MODULE_STEM_64 := libFaceDetectL.so
-LOCAL_SRC_FILES_32 := $(LIB_PATH)/libFaceDetectL.so
-LOCAL_SRC_FILES_64 := $(LIB_PATH)64/libFaceDetectL.so
-LOCAL_SHARED_LIBRARIES := libc libdl liblog libm libFaceTrackerFA
-LOCAL_MODULE_TAGS := optional
-ifeq (1, 1) #(strip $(shell expr $(ANDROID_MAJOR_VER) \>= 8)))
-LOCAL_PROPRIETARY_MODULE := true
-endif
 include $(BUILD_PREBUILT)
 
 
@@ -138,7 +67,7 @@ LOCAL_MODULE_STEM_32 := libsprdfd.so
 LOCAL_MODULE_STEM_64 := libsprdfd.so
 LOCAL_SRC_FILES_32 := $(LIB_PATH)/libsprdfd.so
 LOCAL_SRC_FILES_64 := $(LIB_PATH)64/libsprdfd.so
-LOCAL_SHARED_LIBRARIES := libFaceDetect libFaceDetectHW libFaceDetectL libFaceDetectV3 libc libdl liblog libm libsprdfd_hw
+LOCAL_SHARED_LIBRARIES := libsprdfd_hw libFaceDetectV3 libumnn libc libdl liblog libm 
 LOCAL_MODULE_TAGS := optional
 ifeq (1, 1) #(strip $(shell expr $(ANDROID_MAJOR_VER) \>= 8)))
 LOCAL_PROPRIETARY_MODULE := true
